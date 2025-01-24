fab55b09ee2812c86400d63a3d703c79086fd020
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-24 15:50:19 +0100

    add arrays explanations

--- mustang.tcl
+++ mustang.tcl
@@ -2,4 +2,17 @@
 # City:       🅿🅰🅻🅴🆁🅼🅾, 🆂🅸🅲🅸🅻🆈
 # Country:    🅸🆃🅰🅻🆈
 
+## ARRAYS
+#
+# ::ACCENTS --> It's an array that contains all the Mustang accents colors.
+# ::ADDRESS --> It's an array that contains addresses organized in various ways.
+# ::CURRENT --> It's an array that contains all the Mustang widgets current options values.
+# ::DATA    --> It's an array that contains all the Mustang variables that doesn't fit anywhere else.
+# ::DEFAULT --> It's an array that contains all the Mustang widgets default options values.
+# ::FONT    --> It's an array that contains all the Mustang fonts data.
+# ::STYLE   --> It's an array that contains every style name known by each theme available.
+# ::SVG     --> It's an array that contains all the Mustang svgs icons.
+# ::TABLE   --> It's an array that contains all the Mustang tables.
+# ::TEMP    --> It's an array that contains all the Mustang temporary variables.
+
 #*EOF*
