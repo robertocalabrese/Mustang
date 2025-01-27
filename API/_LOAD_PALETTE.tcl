0c427610a05ba7172ccb291348fcfdbfc4b42c8c
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-27 08:48:56 +0100

    read the entire palette file

--- API/_LOAD_PALETTE.tcl
+++ API/_LOAD_PALETTE.tcl
@@ -11,4 +11,14 @@ proc ::_LOAD_PALETTE { filepath } {
     # Get the palette name.
     set palette [file rootname [file tail $filepath]]
 
+    # Load the palette file.
+    try {
+        open [file nativename $filepath] r
+    } on error {} {
+        chan puts stdout "Unable to load the '$palette' palette. Ignoring."
+    } on ok { channel } {
+        # Read the entire file.
+        set file_content [split [chan read $channel] "\n"]
+        chan close $channel
+    }
 }
