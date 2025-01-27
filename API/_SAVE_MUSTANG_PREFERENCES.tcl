02c43a58d7ea56a119b1d3498f995c1d3e0d6776
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-27 16:25:34 +0100

    open the mustang configuration file for writing

--- API/_SAVE_MUSTANG_PREFERENCES.tcl
+++ API/_SAVE_MUSTANG_PREFERENCES.tcl
@@ -3,4 +3,10 @@
 # Save the Mustang configuration file.
 #
 # Return the word 'success' or a text string describing what went wrong.
-proc ::_SAVE_MUSTANG_PREFERENCES {} {}
+proc ::_SAVE_MUSTANG_PREFERENCES {} {
+    try {
+        open [file join $::CONFIG_DIR mustang mustang.conf] w
+    } on error { errortext errorcode } {
+        return $errortext
+    } on ok { channel } {}
+}
