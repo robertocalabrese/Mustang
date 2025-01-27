c761440b8fb79bacc646ba416a64786bf54784db
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-27 16:28:54 +0100

    save the current biggest font family and size

--- API/_SAVE_MUSTANG_PREFERENCES.tcl
+++ API/_SAVE_MUSTANG_PREFERENCES.tcl
@@ -21,5 +21,14 @@ proc ::_SAVE_MUSTANG_PREFERENCES {} {
         chan puts $channel "# \['blue', 'cyan', 'green', 'orange', 'purple', 'red', 'yellow' or 'custom'\]"
         chan puts $channel "ACCENT: $::ACCENT"
         chan puts $channel ""
+
+        chan puts $channel "# BIGGEST_FONT"
+        chan puts $channel "#"
+        chan puts $channel "# It's a list that specifies the biggest font to use (both family and size)."
+        chan puts $channel "# If the size argument is a positive number, it is interpreted as a size in points."
+        chan puts $channel "# If size is a negative number, its absolute value is interpreted as a size in pixels."
+        chan puts $channel "# If a font cannot be displayed at the specified size, a nearby size will be chosen."
+        chan puts $channel "BIGGEST_FONT: $::FONT(Biggest,family) $::FONT(Biggest,size)"
+        chan puts $channel ""
     }
 }
