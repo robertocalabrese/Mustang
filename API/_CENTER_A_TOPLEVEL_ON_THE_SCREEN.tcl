2529ef435de8231cccdab374b109e84d016d4aac
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-27 11:06:00 +0100

    add initial _CENTER_A_TOPLEVEL_ON_THE_SCREEN API

---
+++ API/_CENTER_A_TOPLEVEL_ON_THE_SCREEN.tcl
@@ -0,0 +1,10 @@
+## _CENTER_A_TOPLEVEL_ON_THE_SCREEN
+#
+# Center a toplevel address 'w' on the screen.
+#
+# Where:
+#
+# w   Should be the toplevel address to be centered.
+#
+# It doesn't return anything.
+proc ::_CENTER_A_TOPLEVEL_ON_THE_SCREEN { w } {}
