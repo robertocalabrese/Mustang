56b2f7132575054aa7bffb19638bcb66dcf1bca8
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-27 11:07:31 +0100

    check that w is a toplevel

--- API/_CENTER_A_TOPLEVEL_ON_THE_SCREEN.tcl
+++ API/_CENTER_A_TOPLEVEL_ON_THE_SCREEN.tcl
@@ -7,4 +7,11 @@
 # w   Should be the toplevel address to be centered.
 #
 # It doesn't return anything.
-proc ::_CENTER_A_TOPLEVEL_ON_THE_SCREEN { w } {}
+proc ::_CENTER_A_TOPLEVEL_ON_THE_SCREEN { w } {
+    # Check that w is a toplevel.
+    if { $w ni $::ADDRESS(classtype,TOPLEVEL) } {
+        return
+    } else {
+        update
+    }
+}
