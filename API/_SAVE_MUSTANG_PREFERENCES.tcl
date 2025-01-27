6e8c55a8e34f33ea4b96e3ed7d55806a998b6b3b
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-27 16:24:10 +0100

    add initial _SAVE_MUSTANG_PREFERENCES API

---
+++ API/_SAVE_MUSTANG_PREFERENCES.tcl
@@ -0,0 +1,6 @@
+# _SAVE_MUSTANG_PREFERENCES
+#
+# Save the Mustang configuration file.
+#
+# Return the word 'success' or a text string describing what went wrong.
+proc ::_SAVE_MUSTANG_PREFERENCES {} {}
