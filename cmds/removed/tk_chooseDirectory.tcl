d2bd994c04269bf5c5423da948074682bdca7b41
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-28 16:49:40 +0100

    fix the new 'dialog directory' command info

--- cmds/removed/tk_chooseDirectory.tcl
+++ cmds/removed/tk_chooseDirectory.tcl
@@ -12,7 +12,7 @@ rename tk_chooseDirectory _tk_chooseDirectory
 
 proc tk_chooseDirectory { args } {
     chan puts stdout "The 'tk_chooseDirectory' command has been removed from Tk by Mustang. Ignoring."
-    chan puts stdout "Please use 'dialog directory' instead."
+    chan puts stdout "Please use the 'dialog directory' command instead."
 
     return ""
 }
