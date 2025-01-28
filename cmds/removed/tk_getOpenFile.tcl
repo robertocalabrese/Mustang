c634e443504c1a506831c17f43bbb28d3b21c893
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-28 16:48:39 +0100

    fix the new 'dialog openFile' command info

--- cmds/removed/tk_getOpenFile.tcl
+++ cmds/removed/tk_getOpenFile.tcl
@@ -12,7 +12,7 @@ rename tk_getOpenFile _tk_getOpenFile
 
 proc tk_getOpenFile { args } {
     chan puts stdout "The 'tk_getOpenFile' command has been removed from Tk by Mustang. Ignoring."
-    chan puts stdout "Please use 'dialog openFile' instead."
+    chan puts stdout "Please use the 'dialog openFile' command instead."
 
     return ""
 }
