8c66f881daf083d5858ea47e7dc72915695df2d9
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-28 16:39:35 +0100

    remove the 'tk_SetPalette' command from Tk

---
+++ cmds/removed/tk_SetPalette.tcl
@@ -0,0 +1,17 @@
+# Developer:  🆁🅾🅱🅴🆁🆃🅾  🅲🅰🅻🅰🅱🆁🅴🆂🅴
+# City:       🅿🅰🅻🅴🆁🅼🅾, 🆂🅸🅲🅸🅻🆈
+# Country:    🅸🆃🅰🅻🆈
+
+############################
+##                        ##
+##     COMMAND REMOVED    ##
+##                        ##
+############################
+
+proc tk_SetPalette { args } {
+    chan puts stdout "The 'tk_SetPalette' command has been removed from Tk by Mustang. Ignoring."
+
+    return ""
+}
+
+#*EOF*
