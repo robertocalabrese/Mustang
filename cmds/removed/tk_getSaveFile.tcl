f8d7b79d5f7ee7b2d87d67e4e674638295d32d1a
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-28 16:48:10 +0100

    fix the new 'dialog saveFile' command info

--- cmds/removed/tk_getSaveFile.tcl
+++ cmds/removed/tk_getSaveFile.tcl
@@ -12,7 +12,7 @@ rename tk_getSaveFile _tk_getSaveFile
 
 proc tk_getSaveFile { args } {
     chan puts stdout "The 'tk_getSaveFile' command has been removed from Tk by Mustang. Ignoring."
-    chan puts stdout "Please use 'dialog saveFile' instead."
+    chan puts stdout "Please use the 'dialog saveFile' command instead."
 
     return ""
 }
