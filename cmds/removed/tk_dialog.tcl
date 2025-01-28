ab70cb732a39a3146c19f8aab4425e87cbef07e2
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-28 16:49:08 +0100

    fix the new 'dialog' command info

--- cmds/removed/tk_dialog.tcl
+++ cmds/removed/tk_dialog.tcl
@@ -10,7 +10,7 @@
 
 proc tk_dialog {} {
     chan puts stdout "The 'tk_dialog' command has been removed from Tk by Mustang. Ignoring."
-    chan puts stdout "Please use 'dialog' instead."
+    chan puts stdout "Please use the 'dialog' command instead."
 
     return ""
 }
