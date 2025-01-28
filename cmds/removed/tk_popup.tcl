56cba188222c41c2c47b85f61ec100ceb56581b8
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-28 16:45:36 +0100

    fix the new 'contextmenu' command info

--- cmds/removed/tk_popup.tcl
+++ cmds/removed/tk_popup.tcl
@@ -12,7 +12,7 @@ rename tk_popup _tk_popup
 
 proc tk_popup { args } {
     chan puts stdout "The 'tk_popup' command has been removed from Tk by Mustang. Ignoring."
-    chan puts stdout "Please use 'contextmenu' instead."
+    chan puts stdout "Please use the 'contextmenu' command instead."
 
     return ""
 }
