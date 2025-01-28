2886c83c9c52f247eeda85e68c5487a7da4d25af
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-28 18:42:53 +0100

    fix: use 'chan puts' instead of '::_PUTS'

--- cmds/removed/tk_textCut.tcl
+++ cmds/removed/tk_textCut.tcl
@@ -11,8 +11,8 @@
 rename tk_textCut _tk_textCut
 
 proc tk_textCut { window } {
-    {*}$::_PUTS stdout "The 'tk_textCut' command has been removed from Tk by Mustang. Ignoring."
-    {*}$::_PUTS stdout "Please use the 'cut' command instead."
+    chan puts stdout "The 'tk_textCut' command has been removed from Tk by Mustang. Ignoring."
+    chan puts stdout "Please use the 'cut' command instead."
 
     return ""
 }
