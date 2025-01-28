91cea47b5c6312f0d9c603010e0f4ae51a71dcd9
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-28 18:42:20 +0100

    fix: use 'chan puts' instead of '::_PUTS'

--- cmds/removed/tk_textPaste.tcl
+++ cmds/removed/tk_textPaste.tcl
@@ -11,8 +11,8 @@
 rename tk_textPaste _tk_textPaste
 
 proc tk_textPaste { window } {
-    {*}$::_PUTS stdout "The 'tk_textPaste' command has been removed from Tk by Mustang. Ignoring."
-    {*}$::_PUTS stdout "Please use the 'paste' command instead."
+    chan puts stdout "The 'tk_textPaste' command has been removed from Tk by Mustang. Ignoring."
+    chan puts stdout "Please use the 'paste' command instead."
 
     return ""
 }
