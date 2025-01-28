ae7f8a3040d5f4e1310d7eae05d04de35cc510b9
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-28 16:25:08 +0100

    remove the 'tk_getOpenFile' command from Tk

---
+++ cmds/removed/tk_getOpenFile.tcl
@@ -0,0 +1,20 @@
+# Developer:  🆁🅾🅱🅴🆁🆃🅾  🅲🅰🅻🅰🅱🆁🅴🆂🅴
+# City:       🅿🅰🅻🅴🆁🅼🅾, 🆂🅸🅲🅸🅻🆈
+# Country:    🅸🆃🅰🅻🆈
+
+############################
+##                        ##
+##     COMMAND REMOVED    ##
+##                        ##
+############################
+
+rename tk_getOpenFile _tk_getOpenFile
+
+proc tk_getOpenFile { args } {
+    chan puts stdout "The 'tk_getOpenFile' command has been removed from Tk by Mustang. Ignoring."
+    chan puts stdout "Please use 'dialog openFile' instead."
+
+    return ""
+}
+
+#*EOF*
