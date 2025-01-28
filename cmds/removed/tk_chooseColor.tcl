b298598a5840c0fc62c4bf19a53c6a70857084d7
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-28 16:02:50 +0100

    remove the 'tk_chooseColor' command from Tk

---
+++ cmds/removed/tk_chooseColor.tcl
@@ -0,0 +1,20 @@
+# Developer:  🆁🅾🅱🅴🆁🆃🅾  🅲🅰🅻🅰🅱🆁🅴🆂🅴
+# City:       🅿🅰🅻🅴🆁🅼🅾, 🆂🅸🅲🅸🅻🆈
+# Country:    🅸🆃🅰🅻🆈
+
+############################
+##                        ##
+##     COMMAND REMOVED    ##
+##                        ##
+############################
+
+rename tk_chooseColor _tk_chooseColor
+
+proc tk_chooseColor { args } {
+    chan puts stdout "The 'tk_chooseColor' command has been removed from Tk by Mustang. Ignoring."
+    chan puts stdout "Please use 'dialog color' instead."
+
+    return ""
+}
+
+#*EOF*
