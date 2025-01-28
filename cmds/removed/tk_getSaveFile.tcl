6e9e95de985c3b8e596f4d46f5b02ea4768ff201
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-28 16:26:17 +0100

    remove the 'tk_getSaveFile' command from Tk

---
+++ cmds/removed/tk_getSaveFile.tcl
@@ -0,0 +1,20 @@
+# Developer:  🆁🅾🅱🅴🆁🆃🅾  🅲🅰🅻🅰🅱🆁🅴🆂🅴
+# City:       🅿🅰🅻🅴🆁🅼🅾, 🆂🅸🅲🅸🅻🆈
+# Country:    🅸🆃🅰🅻🆈
+
+############################
+##                        ##
+##     COMMAND REMOVED    ##
+##                        ##
+############################
+
+rename tk_getSaveFile _tk_getSaveFile
+
+proc tk_getSaveFile { args } {
+    chan puts stdout "The 'tk_getSaveFile' command has been removed from Tk by Mustang. Ignoring."
+    chan puts stdout "Please use 'dialog saveFile' instead."
+
+    return ""
+}
+
+#*EOF*
