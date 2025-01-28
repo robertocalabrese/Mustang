8753ecb1b18dc030e607f2639740128fbef27d15
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-28 16:01:37 +0100

    remove the 'message' command from Tk

---
+++ cmds/removed/message.tcl
@@ -0,0 +1,20 @@
+# Developer:  🆁🅾🅱🅴🆁🆃🅾  🅲🅰🅻🅰🅱🆁🅴🆂🅴
+# City:       🅿🅰🅻🅴🆁🅼🅾, 🆂🅸🅲🅸🅻🆈
+# Country:    🅸🆃🅰🅻🆈
+
+############################
+##                        ##
+##     COMMAND REMOVED    ##
+##                        ##
+############################
+
+rename message _message
+
+proc message { args } {
+    chan puts stdout "The 'message' command has been removed from Tk by Mustang. Ignoring."
+    chan puts stdout "Please use 'dialog messagebox' instead."
+
+    return ""
+}
+
+#*EOF*
