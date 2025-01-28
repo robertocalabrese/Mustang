3c8c478d35ea9b161793cfd6564a093d5e5e430a
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-01-28 16:46:29 +0100

    fix the new 'dialog message' command info

--- cmds/removed/tk_messageBox.tcl
+++ cmds/removed/tk_messageBox.tcl
@@ -12,7 +12,7 @@ rename tk_messageBox _tk_messageBox
 
 proc tk_messageBox { args } {
     chan puts stdout "The 'tk_messageBox' command has been removed from Tk by Mustang. Ignoring."
-    chan puts stdout "Please use 'dialog message' instead."
+    chan puts stdout "Please use the 'dialog message' command instead."
 
     return ""
 }
