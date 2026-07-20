36cf2f2b12b9183e218df69492f473e364dc07f7
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-07-20 09:22:16 +0200

    grid: add 'grid bbox'

--- cmds/grid.tcl
+++ cmds/grid.tcl
@@ -114,7 +114,33 @@ proc ::ms::grid::Command { args } {
                 default { ::ms::Error "Invalid number of arguments." $caller_info }
             }
         }
-        bbox {}
+        bbox {
+            switch -- [llength $args] {
+                1   -
+                3   -
+                5   {
+                    set container [lindex  $args 0]
+                    set args      [lremove $args 0]
+
+                    # Get the 'container' real address.
+                    set result [::ms::Check_Pathname $container invalid]
+                    switch -- $result {
+                        invalid { ::ms::Error "Invalid address, '$container'." $caller_info }
+                        default { set w [lindex $result 0] }
+                    }
+
+                    # Execute the command.
+                    try {
+                        _grid bbox $w {*}$args
+                    } on error { errortext errorcode } {
+                        ::ms::Error "$errortext" $caller_info
+                    } on ok { result } {
+                        return $result
+                    }
+                }
+                default { ::ms::Error "Invalid number of arguments." $caller_info }
+            }
+        }
         columnconfigure -
         rowconfigure    {}
         configure {}
