fc4bbe761445dca0a6218be9c4f6cbcc19571975
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-07-20 09:37:19 +0200

    pack: add 'pack propagate'

--- cmds/pack.tcl
+++ cmds/pack.tcl
@@ -371,7 +371,32 @@ proc ::ms::pack::Command { args } {
                 default { ::ms::Error "Invalid number of arguments." $caller_info }
             }
         }
-        propagate {}
+        propagate {
+            switch -- [llength $args] {
+                1   -
+                2   {
+                    set container [lindex  $args 0]
+                    set args      [lremove $args 0]
+
+                    # Get the 'container' real address.
+                    set result [::ms::Check_Pathname $container invalid]
+                    switch -- $result {
+                        invalid { ::ms::Error "Invalid address, '$container'." $caller_info }
+                        default { set w [lindex $result 0] }
+                    }
+
+                    # Execute the command.
+                    try {
+                        _pack propagate $w {*}$args
+                    } on error { errortext errorcode } {
+                        ::ms::Error "$errortext" $caller_info
+                    } on ok { result } {
+                        return $result
+                    }
+                }
+                default { ::ms::Error "Invalid number of arguments." $caller_info }
+            }
+        }
         default {}
     }
 }
