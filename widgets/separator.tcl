13b57fb55793583c254401683969a2cd113ae73b
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-07-20 17:48:53 +0200

    separator: add the initial draft of the mustang 'separator' widget

---
+++ widgets/separator.tcl
@@ -0,0 +1,153 @@
+# Copyright: 2025 Roberto Calabrese a.k.a. Kicka
+#
+# This file is part of "Mustang", a GUI toolkit for Tcl/Tk 9.0 and later
+# (https://codeberg.org/kicka/Mustang).
+#
+# The author hereby grant permission to use, copy, modify, distribute,
+# and license this software and its documentation for any purpose, provided
+# that existing copyright notices are retained in all copies and that this
+# notice is included verbatim in any distributions. No written agreement,
+# license, or royalty fee is required for any of the authorized uses.
+# Modifications to this software may be copyrighted by their authors
+# and need not follow the licensing terms described here, provided that
+# the new terms are clearly indicated on the first page of each file where
+# they apply.
+#
+# IN NO EVENT SHALL THE AUTHOR OR DISTRIBUTORS BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY
+# DERIVATIVES THEREOF, EVEN IF THE AUTHOR HAVE BEEN ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# THE AUTHOR AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE
+# IS PROVIDED ON AN "AS IS" BASIS, AND THE AUTHOR AND DISTRIBUTORS HAVE
+# NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
+# MODIFICATIONS.
+#
+# GOVERNMENT USE: If you are acquiring this software on behalf of the
+# U.S. government, the Government shall have only "Restricted Rights"
+# in the software and related documentation as defined in the Federal
+# Acquisition Regulations (FARs) in Clause 52.227.19 (c) (2).  If you
+# are acquiring the software on behalf of the Department of Defense, the
+# software shall be classified as "Commercial Computer Software" and the
+# Government shall have only "Restricted Rights" as defined in Clause
+# 252.227-7013 (c) (1) of DFARs.  Notwithstanding the foregoing, the
+# author grant the U.S. Government and others acting in its behalf
+# permission to use and distribute the software in accordance with the
+# terms specified in this license.
+
+# Symbols meanings that may be used by the widget synopsis:
+#
+#   *option*             --> A mandatory parameter that must be substituted with a proper value.
+#   **option**           --> The command name or a mandatory parameter that must be written verbatim.
+#
+#   ?*option*?           --> An optional parameter that must be substituted with a proper value.
+#   ?**option**?         --> An optional parameter that must be written verbatim.
+#
+#   ?*option* *value*?   --> An optional 'key-value' parameter that must be substituted with proper values.
+#   ?**option** *value*? --> An optional 'key-value' parameter where the former must be written verbatim and
+#                            the latter must be substituted with a proper value.
+
+# Symbols meanings that may be used by the widget infos:
+#
+#   *text*               --> Italic.
+#   **text**             --> Bold.
+#   ***text***           --> Italic-bold
+#
+#   ## text              --> Title.
+#   #### text            --> Chapter.
+#   ###### text          --> Sub-chapter.
+#
+#   [text](https:\\...)  --> Link to an internet page.
+#   [text](/wiki/...)    --> Link to another file in the wiki.
+package provide ::ms::separator 0.1
+
+# Create the mustang **separator** package.
+namespace eval ::ms::separator {}
+
+# Rename the original Tk **ttk::separator** command.
+rename ttk::separator _ttk_separator
+
+# Create aliases for the mustang **separator** command.
+interp alias {} separator      {} ::ms::separator::Command
+interp alias {} ttk::separator {} ::ms::separator::Command
+
+############################
+##                        ##
+##     WIDGET COMMAND     ##
+##                        ##
+############################
+
+## Command
+#
+# Replace the Tk **ttk::separator** widget command.
+#
+# Where:
+#
+# window   Should be the widget pathname address to create.
+#          This address should be unique and all the parents addresses should exists already.
+#          *Window* can either be a real or short address.
+#
+# args     Should be one or more option-value pairs to configure various aspects of the widget.
+#          Any acceptable widget options may be specified.
+#          See 'WIDGET OPTIONS' above for more info.
+#
+# Returns the pathname of the new window created.
+proc ::ms::separator::Command { window { args "" } } {
+    # For the time being we launch the Tk original command with one caveat,
+    # the address provided must be a real address.
+    # Short addresses are not covered until the new command is written.
+    _ttk_separator $window {*}$args
+}
+
+#####################################
+##                                 ##
+##     WIDGET PATHNAME COMMAND     ##
+##                                 ##
+#####################################
+
+## Pathname_Cmd
+#
+# This procedure replaces the Tk widget address command.
+#
+# Where:
+#
+# w      Should be the widget real address involved.
+#
+# cmd    Should be the widget command to use.
+#        The aliased command will provided this data.
+#
+# args   Contains the options needed by the 'cmd', if any.
+#        The aliased command will provided this data.
+#
+# Returned values depends on the 'cmd' provided.
+proc ::ms::separator::Pathname_Cmd { w cmd args } {}
+
+#################################
+##                             ##
+##     WIDGET STYLE UPDATE     ##
+##                             ##
+#################################
+
+## Style_Update
+#
+# Update any separator widget that have stylename as its style.
+#
+# Where:
+#
+# stylename     Should be a string that specifies the stylename that was updated.
+#
+# caller_info   Should be the information on the developer command that generated the call to this procedure.
+#
+# It doesn't return anything.
+proc ::ms::separator::Style_Update { stylename caller_info } {}
+
+######################################
+##                                  ##
+##     EVENT RELATED PROCEDURES     ##
+##                                  ##
+######################################
+
+#*EOF*
