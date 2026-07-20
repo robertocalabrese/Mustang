aec3dc884a537204b2c8cab95de1cff1cc42e9b0
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-07-20 09:33:05 +0200

    pack: add 'pack content' and 'pack slaves'

--- cmds/pack.tcl
+++ cmds/pack.tcl
@@ -201,7 +201,50 @@ proc ::ms::pack::Command { args } {
             }
         }
         content -
-        slaves  {}
+        slaves  {
+            switch -- [llength $args] {
+                1   {
+                    set container $args
+
+                    # Get the 'container' real address.
+                    set result [::ms::Check_Pathname $container invalid]
+                    switch -- $result {
+                        invalid { ::ms::Error "Invalid address, '$container'." $caller_info }
+                        default {
+                            set w    [lindex $result 0]
+                            set type [lindex $result 1]
+                        }
+                    }
+
+                    # Execute the command.
+                    try {
+                        _pack $action $w
+                    } on error { errortext errorcode } {
+                        ::ms::Error "$errortext" $caller_info
+                    } on ok { result } {
+                        # Check the initial address type provided (short or real).
+                        switch -- $type {
+                            short {
+                                set shorts_result [list ]
+                                foreach w $result {
+                                    if { $w in $::ms::addr(reals) } {
+                                        # 'w' is the real address of a widget created by mustang.
+                                        lappend shorts_result $::ms::addr($w,short)
+                                    } else {
+                                        # 'w' is the real address of a widget not created by mustang.
+                                        lappend shorts_result [::ms::Get_Short $w]
+                                    }
+                                }
+
+                                return $shorts_result
+                            }
+                            default { return $result }
+                        }
+                    }
+                }
+                default { ::ms::Error "Invalid number of arguments." $caller_info }
+            }
+        }
         forget {}
         info {}
         propagate {}
