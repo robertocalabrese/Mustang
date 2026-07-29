92216c74f8a3d62cf4c41bd3a81228825337520d
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-07-29 09:08:03 +0200

    grid: fix a missing case scenario when we need to return a short address

--- cmds/grid.tcl
+++ cmds/grid.tcl
@@ -788,9 +788,10 @@ proc ::ms::grid::Command { args } {
                             short {
                                 set shorts_result [list ]
                                 foreach addr $result {
-                                    switch -- [info exists ::ms::addr($addr,short)] {
-                                        0   { lappend shorts_result $addr }
-                                        1   { lappend shorts_result $::ms::addr($addr,short) }
+                                    if { $addr in $::ms::addr(reals) } {
+                                        lappend shorts_result $::ms::addr($addr,short)
+                                    } else {
+                                        lappend shorts_result $addr
                                     }
                                 }
 
@@ -867,8 +868,8 @@ proc ::ms::grid::Command { args } {
                                         #       will always contain the '-in' option value at index '1'.
 
                                         set container [lindex $result 1]
-                                        switch -- [info exists ::ms::addr($container,short)] {
-                                            1   { set result [lreplace $result 1 1 $::ms::addr($container,short)] }
+                                        if { $container in $::ms::addr(reals) } {
+                                            set result [lreplace $result 1 1 $::ms::addr($container,short)]
                                         }
                                     }
                                 }
@@ -911,8 +912,8 @@ proc ::ms::grid::Command { args } {
                                         # Check the initial address type provided (short or real).
                                         switch -- $type {
                                             short {
-                                                switch -- [info exists ::ms::addr($container,short)] {
-                                                    1   { return $::ms::addr($container,short) }
+                                                if { $container in $::ms::addr(reals) } {
+                                                    return $::ms::addr($container,short)
                                                 }
                                             }
                                         }
