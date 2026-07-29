962cfdc5cafcdcc47ec6cba396afbe5162cecc23
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-07-29 10:06:08 +0200

    button: set the default value for each styleable option and if the option is managed by Tk, set also its current value

--- widgets/button.tcl
+++ widgets/button.tcl
@@ -597,6 +597,20 @@ proc ::ms::button::Command { window { args "" } } {
                     }
                 }
             }
+
+            # Set the default value for each styleable option and if the option is managed by Tk, set also its current value.
+            foreach option $::ms::button(styleable,options) {
+                set ::ms::default($w,$option) $::ms::styleopt($::ms::theme,TButton,$option)
+
+                switch -- $::ms::managed_by($w,$option) {
+                    Tk  {
+                        switch -- [info exists ::ms::styleopt($::ms::theme,$::ms::current($w,style),$option)] {
+                            0   { set ::ms::current($w,$option) $::ms::default($w,$option) }
+                            1   { set ::ms::current($w,$option) $::ms::styleopt($::ms::theme,$::ms::current($w,style),$option) }
+                        }
+                    }
+                }
+            }
         }
         default { ::ms::Error "Invalid number of arguments." $caller_info }
     }
