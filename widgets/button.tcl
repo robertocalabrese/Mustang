f2bb1430e9363d8cadc1c8615d8cef6ed53923ce
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-07-29 10:22:10 +0200

    button: set the default value for each styleable option and if the option is managed by Tk, set also its current value

--- widgets/button.tcl
+++ widgets/button.tcl
@@ -1600,7 +1600,21 @@ proc ::ms::button::Pathname_Cmd { w cmd args } {
 # It doesn't return anything.
 proc ::ms::button::Style_Update { stylename caller_info } {
     # Update all the button widgets addresses that have stylename as a style.
-    foreach w $::ms::style($stylename,button,addrs) {}
+    foreach w $::ms::style($stylename,button,addrs) {
+        # Set the default value for each styleable option and if the option is managed by Tk, set also its current value.
+        foreach option $::ms::button(styleable,options) {
+            set ::ms::default($w,$option) $::ms::styleopt($::ms::theme,TButton,$option)
+
+            switch -- $::ms::managed_by($w,$option) {
+                Tk  {
+                    switch -- [info exists ::ms::styleopt($::ms::theme,$stylename,$option)] {
+                        0   { set ::ms::current($w,$option) $::ms::default($w,$option) }
+                        1   { set ::ms::current($w,$option) $::ms::styleopt($::ms::theme,$stylename,$option) }
+                    }
+                }
+            }
+        }
+    }
 
     return ""
 }
