180d8fe99c230af723f6dcd4e488062c8fdc38b2
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-07-29 10:13:36 +0200

    button: initial draft of the pathname command

--- widgets/button.tcl
+++ widgets/button.tcl
@@ -880,7 +880,22 @@ proc ::ms::button::Command { window { args "" } } {
 #        The aliased command will provided this data.
 #
 # Returned values depends on the 'cmd' provided.
-proc ::ms::button::Pathname_Cmd { w cmd args } {}
+proc ::ms::button::Pathname_Cmd { w cmd args } {
+    # Get the caller information.
+    set caller_info [info frame -1]
+
+    # Check the command provided.
+    switch -nocase -- $cmd {
+        cget {}
+        configure {}
+        identify {}
+        instate {}
+        invoke {}
+        state {}
+        style {}
+        default { ::ms::Error "Invalid option, '$cmd'." $caller_info }
+    }
+}
 
 #################################
 ##                             ##
