8736b2e929d486a5ed7767a9fdc1bee134f1f9eb
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-07-29 10:05:27 +0200

    button: initialize the widget variables

--- widgets/button.tcl
+++ widgets/button.tcl
@@ -235,6 +235,75 @@ proc ::ms::button::Command { window { args "" } } {
         0   {
             # Remove any duplicated options (retain only the last ones).
             set args [lsort -increasing -stride 2 -index 0 -unique $args]
+
+            ###############################################
+            ##                                           ##
+            ##     INITIALIZE THE WIDGET'S VARIABLES     ##
+            ##                                           ##
+            ###############################################
+
+            # Set the default widget (not styleable) options.
+            set ::ms::default($w,class)        $::ms::default(button,class)
+            set ::ms::default($w,cmenu)        $::ms::default(button,cmenu)
+            set ::ms::default($w,command)      $::ms::default(button,command)
+            set ::ms::default($w,default)      $::ms::default(button,default)
+            set ::ms::default($w,state)        $::ms::default(button,state)
+            set ::ms::default($w,style)        $::ms::default(button,style)
+            set ::ms::default($w,takefocus)    $::ms::default(button,takefocus)
+            set ::ms::default($w,text)         $::ms::default(button,text)
+            set ::ms::default($w,textvariable) $::ms::default(button,textvariable)
+            set ::ms::default($w,underline)    $::ms::default(button,underline)
+
+            # Set the current widget (not styleable) options.
+            set ::ms::current($w,class)        $::ms::default(button,class)
+            set ::ms::current($w,cmenu)        $::ms::default(button,cmenu)
+            set ::ms::current($w,command)      $::ms::default(button,command)
+            set ::ms::current($w,default)      $::ms::default(button,default)
+            set ::ms::current($w,state)        $::ms::default(button,state)
+            set ::ms::current($w,style)        $::ms::default(button,style)
+            set ::ms::current($w,takefocus)    $::ms::default(button,takefocus)
+            set ::ms::current($w,text)         $::ms::default(button,text)
+            set ::ms::current($w,textvariable) $::ms::default(button,textvariable)
+            set ::ms::current($w,underline)    $::ms::default(button,underline)
+
+            # Set the widget variable needed for internal mechanisms.
+            set ::ms::data($w,classtype) button
+
+            # Set each styleable option to be managed by Tk.
+            #
+            # Note: developer --> The 'option' will be managed directly by the developer and will not follow
+            #                     the relative style indications, mappings included.
+            #
+            #       Tk        --> The 'option' will be managed directly by Tk by following the relative
+            #                     style indications, mappings included (unless stated otherwise in the 'option' info).
+            #
+            #       Each styleable option will always start as managed by Tk.
+            #
+            #       Once a styleable option is set to be managed by the developer, it will not be possible
+            #       to change it back to be managed by Tk.
+            #
+            #       To make a button styleable option managed by the developer, just set your desired value
+            #       for that option through the create or configure command, like:
+            #
+            #           **button** *window* **-background** red
+            #       or
+            #           *window* **configure** **-background** red
+            set ::ms::managed_by($w,anchor)      Tk
+            set ::ms::managed_by($w,background)  Tk
+            set ::ms::managed_by($w,bordercolor) Tk
+            set ::ms::managed_by($w,borderwidth) Tk
+            set ::ms::managed_by($w,charwidth)   Tk
+            set ::ms::managed_by($w,compound)    Tk
+            set ::ms::managed_by($w,cursor)      Tk
+            set ::ms::managed_by($w,darkcolor)   Tk
+            set ::ms::managed_by($w,font)        Tk
+            set ::ms::managed_by($w,foreground)  Tk
+            set ::ms::managed_by($w,image)       Tk
+            set ::ms::managed_by($w,justify)     Tk
+            set ::ms::managed_by($w,lightcolor)  Tk
+            set ::ms::managed_by($w,padding)     Tk
+            set ::ms::managed_by($w,relief)      Tk
+            set ::ms::managed_by($w,shiftrelief) Tk
         }
         default { ::ms::Error "Invalid number of arguments." $caller_info }
     }
