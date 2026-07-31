b521f2287e19a327018070769b7016d37b516396
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-07-31 11:17:42 +0200

    canvas: update the widget and remove any temporary variables that was created

--- widgets/canvas.tcl
+++ widgets/canvas.tcl
@@ -1970,6 +1970,18 @@ proc ::ms::canvas::Pathname_Cmd { w cmd args } {
                                     }
                                 }
                             }
+
+                            # Note: If width and/or height were provided by the developer, and these values were accepted by Tk,
+                            #       then a configure event will be invoked by Tk ('::ms::canvas::Configure').
+                            #       After this event, their values will be registered.
+
+                            update
+
+                            # Cleaning.
+                            unset -nocomplain -- ::ms::temp($w,height) \
+                                                 ::ms::temp($w,width);
+
+                            return ""
                         }
                         default { ::ms::Error "Invalid number of arguments." $caller_info }
                     }
