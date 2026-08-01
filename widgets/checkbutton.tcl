c3903672ec57971ed1e8de06933f39ffb17f3c70
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-01 13:17:59 +0200

    checkbutton: set the default value for each styleable option and if the option is managed by Tk, set also its current value

--- widgets/checkbutton.tcl
+++ widgets/checkbutton.tcl
@@ -1949,7 +1949,21 @@ proc ::ms::checkbutton::Pathname_Cmd { w cmd args } {
 # It doesn't return anything.
 proc ::ms::checkbutton::Style_Update { stylename caller_info } {
     # Update all the checkbutton widgets addresses that have stylename as a style.
-    foreach w $::ms::style($stylename,checkbutton,addrs) {}
+    foreach w $::ms::style($stylename,checkbutton,addrs) {
+        # Set the default value for each styleable option and if the option is managed by Tk, set also its current value.
+        foreach option $::ms::checkbutton(styleable,options) {
+            set ::ms::default($w,$option) $::ms::styleopt($::ms::theme,TCheckbutton,$option)
+
+            switch -- $::ms::managed_by($w,$option) {
+                Tk  {
+                    switch -- [info exists ::ms::styleopt($::ms::theme,$stylename,$option)] {
+                        0   { set ::ms::current($w,$option) $::ms::default($w,$option) }
+                        1   { set ::ms::current($w,$option) $::ms::styleopt($::ms::theme,$stylename,$option) }
+                    }
+                }
+            }
+        }
+    }
 
     return ""
 }
