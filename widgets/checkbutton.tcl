0deb58f3539f0925c4fec8cf2de9447ffe3d3ca8
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-01 10:29:50 +0200

    checkbutton: initialize the widget variables

--- widgets/checkbutton.tcl
+++ widgets/checkbutton.tcl
@@ -232,6 +232,77 @@ proc ::ms::checkbutton::Command { window { args "" } } {
         0   {
             # Remove any duplicated options (retain only the last ones).
             set args [lsort -increasing -stride 2 -index 0 -unique $args]
+
+            ###############################################
+            ##                                           ##
+            ##     INITIALIZE THE WIDGET'S VARIABLES     ##
+            ##                                           ##
+            ###############################################
+
+            # Set the default widget (not styleable) options.
+            set ::ms::default($w,class)        $::ms::default(checkbutton,class)
+            set ::ms::default($w,cmenu)        $::ms::default(checkbutton,cmenu)
+            set ::ms::default($w,command)      $::ms::default(checkbutton,command)
+            set ::ms::default($w,offvalue)     $::ms::default(checkbutton,offvalue)
+            set ::ms::default($w,onvalue)      $::ms::default(checkbutton,onvalue)
+            set ::ms::default($w,state)        $::ms::default(checkbutton,state)
+            set ::ms::default($w,style)        $::ms::default(checkbutton,style)
+            set ::ms::default($w,takefocus)    $::ms::default(checkbutton,takefocus)
+            set ::ms::default($w,text)         $::ms::default(checkbutton,text)
+            set ::ms::default($w,textvariable) $::ms::default(checkbutton,textvariable)
+            set ::ms::default($w,underline)    $::ms::default(checkbutton,underline)
+            set ::ms::default($w,variable)     $::ms::default(checkbutton,variable)
+
+            # Set the current widget (not styleable) options.
+            set ::ms::current($w,class)        $::ms::default(checkbutton,class)
+            set ::ms::current($w,cmenu)        $::ms::default(checkbutton,cmenu)
+            set ::ms::current($w,command)      $::ms::default(checkbutton,command)
+            set ::ms::current($w,offvalue)     $::ms::default(checkbutton,offvalue)
+            set ::ms::current($w,onvalue)      $::ms::default(checkbutton,onvalue)
+            set ::ms::current($w,state)        $::ms::default(checkbutton,state)
+            set ::ms::current($w,style)        $::ms::default(checkbutton,style)
+            set ::ms::current($w,takefocus)    $::ms::default(checkbutton,takefocus)
+            set ::ms::current($w,text)         $::ms::default(checkbutton,text)
+            set ::ms::current($w,textvariable) $::ms::default(checkbutton,textvariable)
+            set ::ms::current($w,underline)    $::ms::default(checkbutton,underline)
+            set ::ms::current($w,variable)     $::ms::default(checkbutton,variable)
+
+            # Set the widget variable needed for internal mechanisms.
+            set ::ms::data($w,classtype) checkbutton
+
+            # Set each styleable option to be managed by Tk.
+            #
+            # Note: developer --> The 'option' will be managed directly by the developer and will not follow
+            #                     the relative style indications, mappings included.
+            #
+            #       Tk        --> The 'option' will be managed directly by Tk by following the relative
+            #                     style indications, mappings included (unless stated otherwise in the 'option' info).
+            #
+            #       Each styleable option will always start as managed by Tk.
+            #
+            #       Once a styleable option is set to be managed by the developer, it will not be possible
+            #       to change it back to be managed by Tk.
+            #
+            #       To make a checkbutton styleable option managed by the developer, just set your desired value
+            #       for that option through the create or configure command, like:
+            #
+            #           **checkbutton** *window* **-shellbackground** red
+            #       or
+            #           *window* **configure** **-shellbackground** red
+            set ::ms::managed_by($w,charwidth)           Tk
+            set ::ms::managed_by($w,cursor)              Tk
+            set ::ms::managed_by($w,font)                Tk
+            set ::ms::managed_by($w,foreground)          Tk
+            set ::ms::managed_by($w,highlightcolor)      Tk
+            set ::ms::managed_by($w,image)               Tk
+            set ::ms::managed_by($w,indicatorbackground) Tk
+            set ::ms::managed_by($w,indicatorforeground) Tk
+            set ::ms::managed_by($w,indicatorrelief)     Tk
+            set ::ms::managed_by($w,justify)             Tk
+            set ::ms::managed_by($w,padding)             Tk
+            set ::ms::managed_by($w,shellbackground)     Tk
+            set ::ms::managed_by($w,spacer)              Tk
+            set ::ms::managed_by($w,wraplength)          Tk
         }
         default { ::ms::Error "Invalid number of arguments." $caller_info }
     }
