3a50d8fc979a3c0b996f7eed3b9382df2712a5dd
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-01 13:18:56 +0200

    checkbutton: update the widget hull object

--- widgets/checkbutton.tcl
+++ widgets/checkbutton.tcl
@@ -1997,6 +1997,37 @@ proc ::ms::checkbutton::Style_Update { stylename caller_info } {
                 set pad_bottom [lindex $::ms::current($w,padding) 3]
             }
         }
+
+        #####################################
+        ##                                 ##
+        ##     UPDATE THE WIDGET STYLE     ##
+        ##                                 ##
+        #####################################
+
+        # Note: 'charwidth', 'cursor', 'font', 'indicatorbackground', 'indicatorrelief', 'justify', 'padding',
+        #       'shellbackground', 'spacer' and 'wraplength' are not allowed to change if the statespec changes.
+
+        ##################
+        ##              ##
+        ##     HULL     ##
+        ##              ##
+        ##################
+
+        # Set the hull object style name.
+        set ::ms::style($w,hull) [string cat "_sb=" $::ms::current($w,shellbackground) \
+                                             ".TFrame"];
+
+        # If needed, create the hull object style name.
+        if { $::ms::style($w,hull) ni $::ms::style($::ms::theme,created_by_mustang) } {
+            _ttk_style configure $::ms::style($w,hull) -background $::ms::current($w,shellbackground)
+
+            # Add the hull object style name to the theme styles list created by mustang.
+            lappend ::ms::style($::ms::theme,created_by_mustang) $::ms::style($w,hull)
+        }
+
+        # Apply the changes.
+        interp invokehidden {} $w configure -cursor $cursor \
+                                             -style $::ms::style($w,hull);
     }
 
     return ""
