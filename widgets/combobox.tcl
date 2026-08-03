af4e262f1287a4b7156d1d23dd23bab715147893
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-03 10:02:22 +0200

    combobox: check the invalidcommand, validate, validatecommand and xscrollcommand options relative to the datatype option provided

--- widgets/combobox.tcl
+++ widgets/combobox.tcl
@@ -840,6 +840,29 @@ proc ::ms::combobox::Command { window { args "" } } {
                     }
                 }
             }
+
+            # Check the invalidcommand, validate, validatecommand and xscrollcommand options
+            # relative to the datatype option provided.
+            switch -- $::ms::current($w,datatype) {
+                alnum      -
+                alpha      -
+                integer    -
+                posinteger -
+                posreal    -
+                real       {
+                    set ::ms::current($w,invalidcommand)  {}
+                    set ::ms::current($w,validate)        key
+                    set ::ms::current($w,validatecommand) [list ::ms::combobox::Validate_KeyPress %W %P]
+
+                    switch -- $::ms::current($w,maxlength) {
+                        0       {}
+                        default {
+                            # Ignore the xscrollcommand provided, if any.
+                            set ::ms::current($w,xscrollcommand) {}
+                        }
+                    }
+                }
+            }
         }
         default { ::ms::Error "Invalid number of arguments." $caller_info }
     }
