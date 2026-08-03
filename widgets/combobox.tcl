dd21d2ca4653ab4c4668dd6be782fd097489eb5a
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-03 10:05:08 +0200

    combobox: set the remaining combobox variables and lists that needs to be set

--- widgets/combobox.tcl
+++ widgets/combobox.tcl
@@ -1310,6 +1310,62 @@ proc ::ms::combobox::Command { window { args "" } } {
                 TCombobox { bindtags $w [list $w _Combobox TCombobox $::ms::addr($w,toplevel) all] }
                 default   { bindtags $w [list $w $::ms::current($w,class) _Combobox TCombobox $::ms::addr($w,toplevel) all] }
             }
+
+            #####################
+            ##                 ##
+            ##     CLOSING     ##
+            ##                 ##
+            #####################
+
+            # Hide the widget pathcommand.
+            interp hide {} $w
+
+            # Create an alias for the widget pathcommand.
+            lappend ::ms::data($w,token) [interp alias {} $w {} ::ms::combobox::Pathname_Cmd $w]
+
+            # If needed, create an alias for the widget short address pathcommand.
+            if { $short_addr ne $w } {
+                lappend ::ms::data($w,token) [interp alias {} $short_addr {} ::ms::combobox::Pathname_Cmd $w]
+            }
+
+            # Set the border object (where the 'Enter' and 'Leave' event will happen).
+            set ::ms::addr($w,border) $w
+
+            # Set the actual widget address (the widget that the developer was intended to build).
+            set ::ms::addr($w,widget) $w
+
+            # Set the structure addresses.
+            set ::ms::addr($w,structure) [list $w]
+
+            # Set the widget real address relative to its short address, 'short_addr'.
+            set ::ms::addr($short_addr,real) $w
+
+            # Set the widget short address relative to its real address, 'w'.
+            set ::ms::addr($w,short) $short_addr
+
+            # Add the widget real and short address into the list of all available real and short addresses.
+            lappend ::ms::addr(reals)  $w
+            lappend ::ms::addr(shorts) $short_addr
+
+            # Add the widget address to the combobox classtype widgets real address list.
+            lappend ::ms::addr(combobox) $w
+
+            # Add the widget address to the combobox classtype real address list with class '::ms::current($w,class)'.
+            lappend ::ms::class($::ms::current($w,class),combobox,addrs) $w
+
+            # Add the widget address to the combobox classtype real address list with style '::ms::current($w,style)'.
+            lappend ::ms::style($::ms::current($w,style),combobox,addrs) $w
+
+            # If needed, add '::ms::current($w,style)' to the available styles for the combobox classtype.
+            if { $::ms::current($w,style) ni $::ms::style(combobox,classtype) } {
+                lappend ::ms::style(combobox,classtype) $::ms::current($w,style)
+            }
+
+            # Depending on the address type provided, return the widget real or short address.
+            switch -- $type {
+                real  { return $w }
+                short { return $short_addr }
+            }
         }
         default { ::ms::Error "Invalid number of arguments." $caller_info }
     }
