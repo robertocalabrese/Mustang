d51b596d533e75ded42e2005e56154be501fbd52
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-03 10:01:24 +0200

    combobox: check the widget state and set the takefocus accordingly

--- widgets/combobox.tcl
+++ widgets/combobox.tcl
@@ -825,6 +825,11 @@ proc ::ms::combobox::Command { window { args "" } } {
                     }
                 }
             }
+
+            # Check the widget state and set the takefocus accordingly.
+            switch -- $::ms::current($w,state) {
+                disabled { set ::ms::current($w,takefocus) 0 }
+            }
         }
         default { ::ms::Error "Invalid number of arguments." $caller_info }
     }
