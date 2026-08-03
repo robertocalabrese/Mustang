97a8da7a05c249e48c98ba8bbd4dd4c0b46e889b
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-03 10:30:06 +0200

    combobox: add the 'Scrollbar_Touchpad' procedure

--- widgets/combobox.tcl
+++ widgets/combobox.tcl
@@ -4763,4 +4763,56 @@ proc ::ms::combobox::Scrollbar_MouseWheel { w amount what } {
     return ""
 }
 
+## Scrollbar_Touchpad
+#
+# Scroll the listbox vertically by units (**TouchpadScroll**) or by pages (**Control-TouchpadScroll**).
+#
+# Where:
+#
+# w         Should be the scrollable widget real address involved.
+#
+# counter   Should be the *serial* field of a **TouchpadScroll** event (**%#**).
+#
+# amount    Should be the delta value of a **TouchpadScroll**/**Control-TouchpadScroll** event.
+#           The delta value represents the rotation units the mouse wheel has been moved.
+#           The sign of the value represents the direction the mouse wheel was scrolled.
+#           *Amount* is normally delivered by the **TouchpadScroll**/**Control-TouchpadScroll**
+#           event with a value of **+120.0** or **-120.0**, depending on the scroll direction.
+#
+#           If the value provided as *amount* is not an integer or a float,
+#           defaults to **+120.0**.
+#
+#           Note: **0** is not allowed. If provided, it will be changed to **+120.0**.
+#
+# what      Should be a string that specifies the unit type.
+#           Allowed values are the word **units** or **pages**.
+#
+#           If not provided, defaults to **units**.
+#
+# It doesn't return anything.
+proc ::ms::combobox::Scrollbar_Touchpad { w counter amount { what units } } {
+    # Acknowledgment: This code is taken (and adapted) from the 'Recent improvements
+    #                 on Tk 9' pdf paper by 'Csaba Nemethi'.
+
+    # **TouchpadScroll** events can be generated about 60 times per second
+    # during a two-finger gesture.
+    # This allow the binding script to respond to every 5th **TouchpadScroll** event
+    # by testing is the 'counter' is divisible by 5.
+    if { [expr { $counter%5 }] != 0 } {
+        return ""
+    }
+
+    # Translate 'amount' in 'delta_x' and 'delta_y'.
+    lassign [::tk::PreciseScrollDeltas $amount] delta_x delta_y
+
+    # Adjust 'delta_y' value, or the movement will be too slow.
+    # 'delta_x' will be ignored.
+    set delta_y [expr { $delta_y*30 }]
+
+    # If there is a movement along the Y axis, launch '::ms::combobox::Popdown_Scrollbar_MouseWheel'.
+    if { $delta_y != 0 } {
+        ::ms::combobox::Popdown_Scrollbar_MouseWheel $w $delta_y $what
+    }
+}
+
 #*EOF*
