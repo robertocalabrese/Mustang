312d58de2de3c130406c546d6ff5f6c6f9ae0ad2
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-03 10:06:57 +0200

    combobox: add the 'delete', 'identify' and 'selection' command

--- widgets/combobox.tcl
+++ widgets/combobox.tcl
@@ -1497,7 +1497,28 @@ proc ::ms::combobox::Pathname_Cmd { w cmd args } {
         }
         delete    -
         identify  -
-        selection {}
+        selection {
+            # Synopsis:
+            #
+            # *window* **delete** *first* ?*last*?
+            # *window* **identify** **element** *x* *y*
+            # *window* **selection** *option* *arg*
+            #    *window* **selection** **clear**
+            #    *window* **selection** **present**
+            #    *window* **selection** **range** *start* *end*
+            switch -- [llength $args] {
+                0       { ::ms::Error "Invalid number of arguments." $caller_info }
+                default {
+                    try {
+                        interp invokehidden {} $w $cmd {*}$args
+                    } on error { errortext errorcode } {
+                        ::ms::Error "$errortext" $caller_info
+                    } on ok { result } {
+                        return $result
+                    }
+                }
+            }
+        }
         insert {}
         get      -
         validate {}
