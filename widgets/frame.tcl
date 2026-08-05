da59b060d9a009f010288819b7522e16fdd8975c
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 11:20:56 +0200

    frame: update the widget and remove any temporary variables that was created

--- widgets/frame.tcl
+++ widgets/frame.tcl
@@ -1900,6 +1900,18 @@ proc ::ms::frame::Pathname_Cmd { w cmd args } {
                                     ::ms::frame::Scrollbar_Update $w
                                 }
                             }
+
+                            # Note: If width and/or height were provided by the developer, and these values were accepted by Tk,
+                            #       then a configure event will be invoked by Tk ('::ms::frame::Configure').
+                            #       After this event, their values will be registered.
+
+                            update
+
+                            # Cleaning.
+                            unset -nocomplain -- ::ms::temp($w,height) \
+                                                 ::ms::temp($w,width);
+
+                            return ""
                         }
                         default { ::ms::Error "Invalid number of arguments." $caller_info }
                     }
