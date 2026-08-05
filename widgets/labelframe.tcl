d6ccb0c2c8daab1936f9895669f70381dd0454b6
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 17:32:52 +0200

    labelframe: add the 'Focus_Out' event procedure

--- widgets/labelframe.tcl
+++ widgets/labelframe.tcl
@@ -4848,4 +4848,44 @@ proc ::ms::labelframe::Focus_In { w } {
 
     return ""
 }
+
+## Focus_Out
+#
+# Manage the **FocusOut** event.
+#
+# Where:
+#
+# w   Should be the widget real address involved.
+#
+# It doesn't return anything.
+proc ::ms::labelframe::Focus_Out { w } {
+    # Check the contextual menu associated with this widget, if any.
+    set cmenu $::ms::current($w,cmenu)
+    switch -- $cmenu {
+        ""  {
+            # Check if a contextual menu was associated with the widget's toplevel.
+            set cmenu $::ms::current($::ms::addr($w,toplevel),cmenu)
+            switch -- $cmenu {
+                ""      {}
+                default {
+                    # If the contextual menu of the widget's toplevel is open do not loose the focus (graphically).
+                    switch -- [_winfo exists $cmenu] {
+                        1   { return "" }
+                    }
+                }
+            }
+        }
+        default {
+            # If the contextual menu of the widget is open do not loose the focus (graphically).
+            switch -- [_winfo exists $cmenu] {
+                1   { return "" }
+            }
+        }
+    }
+
+    # Change the widget dynamic state to '!focus'.
+    ::ms::labelframe::Pathname_Cmd $w state !focus
+
+    return ""
+}
 #*EOF*
