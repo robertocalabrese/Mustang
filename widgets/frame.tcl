b3ed7d3dabe8a3234ab91fb3373184ad84ca054d
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 11:19:23 +0200

    frame: configure the simple frame content object (the actual frame)

--- widgets/frame.tcl
+++ widgets/frame.tcl
@@ -1609,6 +1609,96 @@ proc ::ms::frame::Pathname_Cmd { w cmd args } {
                                     ##     SIMPLE FRAME     ##
                                     ##                      ##
                                     ##########################
+
+                                    ###################
+                                    ##               ##
+                                    ##     FRAME     ##
+                                    ##               ##
+                                    ###################
+
+                                    # Set the widget style name.
+                                    set ::ms::style($w,widget) [string cat "_bg=" $::ms::current($w,background) \
+                                                                           "_bc=" $::ms::current($w,bordercolor) \
+                                                                           "_dc=" $::ms::current($w,darkcolor) \
+                                                                           "_lc=" $::ms::current($w,lightcolor) \
+                                                                           "." $::ms::current($w,style)];
+
+                                    # If needed, create the widget style name.
+                                    if { $::ms::style($w,widget) ni $::ms::style($::ms::theme,created_by_mustang) } {
+                                        _ttk_style configure $::ms::style($w,widget)  -background $::ms::current($w,background) \
+                                                                                     -bordercolor $::ms::current($w,bordercolor) \
+                                                                                       -darkcolor $::ms::current($w,darkcolor) \
+                                                                                      -lightcolor $::ms::current($w,lightcolor);
+
+                                        # Add the widget style name to the theme styles list created by mustang.
+                                        lappend ::ms::style($::ms::theme,created_by_mustang) $::ms::style($w,widget)
+                                    }
+
+                                    # Initialize the widget mapping.
+                                    set mapping [list ]
+
+                                    # background
+                                    switch -- $::ms::managed_by($w,background) {
+                                        developer { lappend mapping -background [list pressed $::ms::current($w,background)] }
+                                        Tk  {
+                                            # Check if a 'background' mapping exists for '::ms::current($w,style)'.
+                                            switch -- [info exists ::ms::stylemap($::ms::theme,$::ms::current($w,style),background)] {
+                                                1   { lappend mapping -background $::ms::stylemap($::ms::theme,$::ms::current($w,style),background) }
+                                            }
+                                        }
+                                    }
+
+                                    # bordercolor
+                                    switch -- $::ms::managed_by($w,bordercolor) {
+                                        developer { lappend mapping -bordercolor [list pressed $::ms::current($w,bordercolor)] }
+                                        Tk  {
+                                            # Check if a 'bordercolor' mapping exists for '::ms::current($w,style)'.
+                                            switch -- [info exists ::ms::stylemap($::ms::theme,$::ms::current($w,style),bordercolor)] {
+                                                1   { lappend mapping -bordercolor $::ms::stylemap($::ms::theme,$::ms::current($w,style),bordercolor) }
+                                            }
+                                        }
+                                    }
+
+                                    # darkcolor
+                                    switch -- $::ms::managed_by($w,darkcolor) {
+                                        developer { lappend mapping -darkcolor [list pressed $::ms::current($w,darkcolor)] }
+                                        Tk  {
+                                            # Check if a 'darkcolor' mapping exists for '::ms::current($w,style)'.
+                                            switch -- [info exists ::ms::stylemap($::ms::theme,$::ms::current($w,style),darkcolor)] {
+                                                1   { lappend mapping -darkcolor $::ms::stylemap($::ms::theme,$::ms::current($w,style),darkcolor) }
+                                            }
+                                        }
+                                    }
+
+                                    # lightcolor
+                                    switch -- $::ms::managed_by($w,lightcolor) {
+                                        developer { lappend mapping -lightcolor [list pressed $::ms::current($w,lightcolor)] }
+                                        Tk  {
+                                            # Check if a 'lightcolor' mapping exists for '::ms::current($w,style)'.
+                                            switch -- [info exists ::ms::stylemap($::ms::theme,$::ms::current($w,style),lightcolor)] {
+                                                1   { lappend mapping -lightcolor $::ms::stylemap($::ms::theme,$::ms::current($w,style),lightcolor) }
+                                            }
+                                        }
+                                    }
+
+                                    # If needed, create the widget mapping.
+                                    if { $mapping ni $::ms::stylemap($::ms::theme,created_by_mustang) } {
+                                        _ttk_style map $::ms::style($w,widget) {*}$mapping
+
+                                        # Add the widget mapping to the stylemap list containing all the mappings
+                                        # created by mustang for the current theme.
+                                        lappend ::ms::stylemap($::ms::theme,created_by_mustang) $mapping
+                                    }
+
+                                    # Apply the changes.
+                                    interp invokehidden {} $w configure -borderwidth $::ms::current($w,borderwidth) \
+                                                                             -cursor $::ms::current($w,cursor) \
+                                                                             -height $::ms::temp($w,height) \
+                                                                            -padding $::ms::current($w,padding) \
+                                                                             -relief $::ms::current($w,relief) \
+                                                                              -style $::ms::style($w,widget) \
+                                                                          -takefocus $::ms::current($w,takefocus) \
+                                                                              -width $::ms::temp($w,width);
                                 }
                                 true {
                                     ##############################
