c9fee934dfe0edb8614db32be2fcce1da2e6a87f
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 11:07:15 +0200

    frame: set the default value for each styleable option and if the option is managed by Tk, set also its current value

--- widgets/frame.tcl
+++ widgets/frame.tcl
@@ -518,6 +518,20 @@ proc ::ms::frame::Command { window { args "" } } {
                     }
                 }
             }
+
+            # Set the default value for each styleable option and if the option is managed by Tk, set also its current value.
+            foreach option $::ms::frame(styleable,options) {
+                set ::ms::default($w,$option) $::ms::styleopt($::ms::theme,TFrame,$option)
+
+                switch -- $::ms::managed_by($w,$option) {
+                    Tk  {
+                        switch -- [info exists ::ms::styleopt($::ms::theme,$::ms::current($w,style),$option)] {
+                            0   { set ::ms::current($w,$option) $::ms::default($w,$option) }
+                            1   { set ::ms::current($w,$option) $::ms::styleopt($::ms::theme,$::ms::current($w,style),$option) }
+                        }
+                    }
+                }
+            }
         }
         default { ::ms::Error "Invalid number of arguments." $caller_info }
     }
