77e750d9d2bfbf2daabcf734b2f99176f5219f44
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 16:01:57 +0200

    label: add the 'Focus_In' event procedure

--- widgets/label.tcl
+++ widgets/label.tcl
@@ -1813,4 +1813,20 @@ proc ::ms::label::Destroy { w } {
     return ""
 }
 
+## Focus_In
+#
+# Manage the **FocusIn** event.
+#
+# Where:
+#
+# w   Should be the widget real address involved.
+#
+# It doesn't return anything.
+proc ::ms::label::Focus_In { w } {
+    # Change the widget dynamic state to 'focus'.
+    ::ms::label::Pathname_Cmd $w state focus
+
+    return ""
+}
+
 #*EOF*
