d3c995a064822a6035e7f417a70bbdf2faaed472
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 17:34:19 +0200

    labelframe: add the 'Scrollbar_Drag' event procedure

--- widgets/labelframe.tcl
+++ widgets/labelframe.tcl
@@ -5090,4 +5090,58 @@ proc ::ms::labelframe::Scrollbar_ButtonRelease {} {
     return ""
 }
 
+## Scrollbar_Drag
+#
+# Manage the **B1-Motion** event on the widget's internal scrollbars.
+#
+# Where:
+#
+# w        Should be the widget real address involved.
+#
+# orient   Specifies a string (**horizontal** or **vertical**) indicating
+#          the orientation of the scrollbar.
+#
+# x, y     Should be the (x,y) mouse pointer coordinates of the event.
+#          These values should be provided by the **B1-Motion** event.
+#
+# It doesn't return anything.
+proc ::ms::labelframe::Scrollbar_Drag { w orient x y } {
+    # Safeguard.
+    # Check if the 'Scrollbar_Drag' operation is allowed or not.
+    switch -- [info exists ::ms::temp(drag_allowed)] {
+        0   { return "" }
+        1   {
+            switch -- $::ms::temp(drag_allowed) {
+                no  { return "" }
+            }
+        }
+    }
+
+    # Compute 'delta_x' and 'delta_y'.
+    set delta_x [expr { $x-$::ms::temp(xpress) }]
+    set delta_y [expr { $y-$::ms::temp(ypress) }]
+
+    # Check the orientation.
+    switch -nocase -- $orient {
+        horizontal {
+            # Compute the 'fraction' after the drag movement.
+            set delta    [$w.container.x delta $delta_x $delta_y]
+            set fraction [expr { $::ms::temp(fraction)+$delta }]
+
+            # Move the horizontal scrollbar to 'fraction'.
+            ::ms::labelframe::Pathname_Cmd $w xview moveto $fraction
+        }
+        vertical {
+            # Compute the 'fraction' after the drag movement.
+            set delta    [$w.container.y delta $delta_x $delta_y]
+            set fraction [expr { $::ms::temp(fraction)+$delta }]
+
+            # Move the vertical scrollbar to 'fraction'.
+            ::ms::labelframe::Pathname_Cmd $w yview moveto $fraction
+        }
+    }
+
+    return ""
+}
+
 #*EOF*
