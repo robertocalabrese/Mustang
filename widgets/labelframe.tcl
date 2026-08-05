754ca184128dfbe62b8c008fc0a8b2ac98f20acd
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 17:03:28 +0200

    labelframe: configure the simple labelframe content object (the actual content)

--- widgets/labelframe.tcl
+++ widgets/labelframe.tcl
@@ -2735,6 +2735,55 @@ proc ::ms::labelframe::Pathname_Cmd { w cmd args } {
                                     $w.container.border configure -borderwidth $::ms::current($w,borderwidth) \
                                                                        -relief $::ms::current($w,relief) \
                                                                         -style $::ms::style($w,border);
+
+                                    #####################
+                                    ##                 ##
+                                    ##     CONTENT     ##
+                                    ##                 ##
+                                    #####################
+
+                                    # Set the content object style name.
+                                    set ::ms::style($w,content) [string cat "_bg=" $::ms::current($w,background) \
+                                                                            "." $::ms::current($w,style)];
+
+                                    # If needed, create the content object style name.
+                                    if { $::ms::style($w,content) ni $::ms::style($::ms::theme,created_by_mustang) } {
+                                        _ttk_style configure $::ms::style($w,content) -background $::ms::current($w,background)
+
+                                        # Add the content object style name to the theme styles list created by mustang.
+                                        lappend ::ms::style($::ms::theme,created_by_mustang) $::ms::style($w,content)
+                                    }
+
+                                    # Initialize the content object mapping.
+                                    set mapping [list ]
+
+                                    # background
+                                    switch -- $::ms::managed_by($w,background) {
+                                        developer { lappend mapping -background [list pressed $::ms::current($w,background)] }
+                                        Tk  {
+                                            # Check if a 'background' mapping exists for '::ms::current($w,style)'.
+                                            switch -- [info exists ::ms::stylemap($::ms::theme,$::ms::current($w,style),background)] {
+                                                1   { lappend mapping -background $::ms::stylemap($::ms::theme,$::ms::current($w,style),background) }
+                                            }
+                                        }
+                                    }
+
+                                    # If needed, create the content object mapping.
+                                    if { $mapping ni $::ms::stylemap($::ms::theme,created_by_mustang) } {
+                                        _ttk_style map $::ms::style($w,content) {*}$mapping
+
+                                        # Add the content object mapping to the stylemap list containing all the mappings
+                                        # created by mustang for the current theme.
+                                        lappend ::ms::stylemap($::ms::theme,created_by_mustang) $mapping
+                                    }
+
+                                    # Apply the changes.
+                                    $w.container.border.content configure    -cursor $::ms::current($w,cursor) \
+                                                                             -height $::ms::temp($w,height) \
+                                                                            -padding $::ms::current($w,padding) \
+                                                                              -style $::ms::style($w,content) \
+                                                                          -takefocus $::ms::current($w,takefocus) \
+                                                                              -width $::ms::temp($w,width);
                                 }
                                 true {
                                     ###################################
