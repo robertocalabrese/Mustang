1db68f73bfa1bb20348a28559132ca521da5d778
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 11:22:00 +0200

    frame: set the default value for each styleable option and if the option is managed by Tk, set also its current value

--- widgets/frame.tcl
+++ widgets/frame.tcl
@@ -2654,7 +2654,21 @@ proc ::ms::frame::Pathname_Cmd { w cmd args } {
 # It doesn't return anything.
 proc ::ms::frame::Style_Update { stylename caller_info } {
     # Update all the frame widgets that have stylename as a style.
-    foreach w $::ms::style($stylename,frame,addrs) {}
+    foreach w $::ms::style($stylename,frame,addrs) {
+        # Set the default value for each styleable option and if the option is managed by Tk, set also its current value.
+        foreach option $::ms::frame(styleable,options) {
+            set ::ms::default($w,$option) $::ms::styleopt($::ms::theme,TFrame,$option)
+
+            switch -- $::ms::managed_by($w,$option) {
+                Tk  {
+                    switch -- [info exists ::ms::styleopt($::ms::theme,$stylename,$option)] {
+                        0   { set ::ms::current($w,$option) $::ms::default($w,$option) }
+                        1   { set ::ms::current($w,$option) $::ms::styleopt($::ms::theme,$stylename,$option) }
+                    }
+                }
+            }
+        }
+    }
 
     return ""
 }
