4b224447942e849e2e64592a6cad6c49daf45333
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 11:24:25 +0200

    frame: update the widget viewport and content objects (the latter is the actual frame)

--- widgets/frame.tcl
+++ widgets/frame.tcl
@@ -2910,6 +2910,57 @@ proc ::ms::frame::Style_Update { stylename caller_info } {
                 $w.border configure -borderwidth $::ms::current($w,borderwidth) \
                                          -relief $::ms::current($w,relief) \
                                           -style $::ms::style($w,border);
+
+                ##################################
+                ##                              ##
+                ##     VIEWPORT AND CONTENT     ##
+                ##                              ##
+                ##################################
+
+                # Note: The viewport and the content objects will have the same style, '::ms::style($w,content)'.
+
+                # Set the content and viewport objects style name.
+                set ::ms::style($w,content) [string cat "_bg=" $::ms::current($w,background) \
+                                                        "." $stylename];
+
+                # If needed, create the content and viewport objects style name.
+                if { $::ms::style($w,content) ni $::ms::style($::ms::theme,created_by_mustang) } {
+                    _ttk_style configure $::ms::style($w,content) -background $::ms::current($w,background)
+
+                    # Add the content and viewport objects style name to the theme styles list created by mustang.
+                    lappend ::ms::style($::ms::theme,created_by_mustang) $::ms::style($w,content)
+                }
+
+                # Initialize the content and viewport objects mapping.
+                set mapping [list ]
+
+                # background
+                switch -- $::ms::managed_by($w,background) {
+                    developer { lappend mapping -background [list pressed $::ms::current($w,background)] }
+                    Tk  {
+                        # Check if a 'background' mapping exists for 'stylename'.
+                        switch -- [info exists ::ms::stylemap($::ms::theme,$stylename,background)] {
+                            1   { lappend mapping -background $::ms::stylemap($::ms::theme,$stylename,background) }
+                        }
+                    }
+                }
+
+                # If needed, create the content and viewport objects mapping.
+                if { $mapping ni $::ms::stylemap($::ms::theme,created_by_mustang) } {
+                    _ttk_style map $::ms::style($w,content) {*}$mapping
+
+                    # Add the content and viewport objects mapping to the stylemap list containing all the mappings
+                    # created by mustang for the current theme.
+                    lappend ::ms::stylemap($::ms::theme,created_by_mustang) $mapping
+                }
+
+                # Apply the changes.
+                $w.border.viewport configure  -cursor $::ms::current($w,cursor) \
+                                               -style $::ms::style($w,content);
+
+                $w.border.viewport.content configure  -cursor $::ms::current($w,cursor) \
+                                                     -padding $::ms::current($w,padding) \
+                                                       -style $::ms::style($w,content);
             }
         }
     }
