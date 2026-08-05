0fd5117b11cc2a106edf97399c6c61821c3e9731
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 15:55:45 +0200

    label: initial draft of the pathname command

--- widgets/label.tcl
+++ widgets/label.tcl
@@ -839,7 +839,21 @@ proc ::ms::label::Command { window { args "" } } {
 #        The aliased command will provided this data.
 #
 # Returned values depends on the 'cmd' provided.
-proc ::ms::label::Pathname_Cmd { w cmd args } {}
+proc ::ms::label::Pathname_Cmd { w cmd args } {
+    # Get the caller information.
+    set caller_info [info frame -1]
+
+    # Check the command provided.
+    switch -nocase -- $cmd {
+        cget {}
+        configure {}
+        identify {}
+        instate {}
+        state {}
+        style {}
+        default { ::ms::Error "Invalid option, '$cmd'." $caller_info }
+    }
+}
 
 #################################
 ##                             ##
