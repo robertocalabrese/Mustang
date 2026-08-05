809db2aca27d6094cd31f2d05f52ec9f9476082d
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 17:00:30 +0200

    labelframe: initial draft of the widget configure

--- widgets/labelframe.tcl
+++ widgets/labelframe.tcl
@@ -2408,6 +2408,74 @@ proc ::ms::labelframe::Pathname_Cmd { w cmd args } {
                                     set textvariable $::ms::current($w,textvariable)
                                 }
                             }
+
+                            ##################################
+                            ##                              ##
+                            ##     CONFIGURE THE WIDGET     ##
+                            ##                              ##
+                            ##################################
+
+                            # Note: 'anchor', 'borderwidth', 'compound', 'cursor', 'font', 'padding' and 'relief'
+                            #       are not allowed to change if the statespec changes.
+
+                            set background  $::ms::styleopt($::ms::theme,TLabelframe.Label,background)
+                            set bordercolor $::ms::styleopt($::ms::theme,TLabelframe.Label,bordercolor)
+                            set borderwidth $::ms::styleopt($::ms::theme,TLabelframe.Label,borderwidth)
+                            set charwidth   $::ms::styleopt($::ms::theme,TLabelframe.Label,charwidth)
+                            set darkcolor   $::ms::styleopt($::ms::theme,TLabelframe.Label,darkcolor)
+                            set lightcolor  $::ms::styleopt($::ms::theme,TLabelframe.Label,lightcolor)
+                            set padding     $::ms::styleopt($::ms::theme,TLabelframe.Label,padding)
+                            set relief      $::ms::styleopt($::ms::theme,TLabelframe.Label,relief)
+
+                            # Check if '::ms::current($w,style).Label' exists among the styles known by the current theme.
+                            # If not, set it as 'TLabelframe.Label'.
+                            set labelframe_title_style [string cat $::ms::current($w,style) ".Label"]
+                            if { ($labelframe_title_style in $::ms::style($::ms::theme)) && ($labelframe_title_style ne "TLabelframe.Label") } {
+                                # Check if a layout exists for '::ms::current($w,style).Label'.
+                                # If not, create one by mirroring the 'TLabelframe' layout for the current theme.
+                                if { $labelframe_title_style ni $::ms::layouts($::ms::theme) } {
+                                    _ttk_style layout $labelframe_title_style [_ttk_style layout TLabelframe.Label]
+                                }
+
+                                # Get the labelframe title style options, if any.
+                                foreach option [list  background \
+                                                     bordercolor \
+                                                     borderwidth \
+                                                       charwidth \
+                                                       darkcolor \
+                                                      lightcolor \
+                                                         padding \
+                                                          relief] {
+                                    switch -- [info exists ::ms::styleopt($::ms::theme,$labelframe_title_style,$option)] {
+                                        1   { set $option $::ms::styleopt($::ms::theme,$labelframe_title_style,$option) }
+                                    }
+                                }
+                            }
+
+                            # Set the anchor variable.
+                            switch -- $::ms::current($w,anchor) {
+                                ne  { set anchor ne }
+                                nw  { set anchor nw }
+                                n   { set anchor center }
+                            }
+
+                            # Check if the widget is scrollable or not.
+                            switch -- $ms::current($w,scrollable) {
+                                false {
+                                    ###############################
+                                    ##                           ##
+                                    ##     SIMPLE LABELFRAME     ##
+                                    ##                           ##
+                                    ###############################
+                                }
+                                true {
+                                    ###################################
+                                    ##                               ##
+                                    ##     SCROLLABLE LABELFRAME     ##
+                                    ##                               ##
+                                    ###################################
+                                }
+                            }
                         }
                         default { ::ms::Error "Invalid number of arguments." $caller_info }
                     }
