a2cede4cfafd8694ba6a1796103e0a9a5f61b2a9
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 09:31:35 +0200

    entry: check that the configure args forms a valid option/value list

--- widgets/entry.tcl
+++ widgets/entry.tcl
@@ -1376,7 +1376,19 @@ proc ::ms::entry::Pathname_Cmd { w cmd args } {
                         ::ms::Error "Invalid configure option, '$args'." $caller_info
                     }
                 }
-                default {}
+                default {
+                    # Check that the command's 'args' forms a valid 'option/value' list.
+                    switch -- [expr { [llength $args]%2 }] {
+                        0   {
+                            # Remove any duplicated options (retain only the last ones).
+                            set args [lsort -increasing -stride 2 -index 0 -unique $args]
+
+                            # Set a variable that indicates if a new datatype has been provided or not.
+                            set new_datatype false
+                        }
+                        default { ::ms::Error "Invalid number of arguments." $caller_info }
+                    }
+                }
             }
         }
         insert {
