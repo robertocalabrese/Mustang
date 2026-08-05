fcb4b1beaf22af00093e79630cdba1fd69cb4291
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 11:18:16 +0200

    frame: set the current option values for each styleable option managed by TK

--- widgets/frame.tcl
+++ widgets/frame.tcl
@@ -1579,6 +1579,18 @@ proc ::ms::frame::Pathname_Cmd { w cmd args } {
                                     }
                                 }
                             }
+
+                            # Set the current option values for each styleable option managed by Tk.
+                            foreach option $::ms::frame(styleable,options) {
+                                switch -- $::ms::managed_by($w,$option) {
+                                    Tk  {
+                                        switch -- [info exists ::ms::styleopt($::ms::theme,$::ms::current($w,style),$option)] {
+                                            0   { set ::ms::current($w,$option) $::ms::default($w,$option) }
+                                            1   { set ::ms::current($w,$option) $::ms::styleopt($::ms::theme,$::ms::current($w,style),$option) }
+                                        }
+                                    }
+                                }
+                            }
                         }
                         default { ::ms::Error "Invalid number of arguments." $caller_info }
                     }
