411902661b043ab5c6e1e1751a01dc39e65760f6
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-05 16:46:53 +0200

    labelframe: set the remaining simple labelframe variables and lists that needs to be set

--- widgets/labelframe.tcl
+++ widgets/labelframe.tcl
@@ -1205,6 +1205,52 @@ proc ::ms::labelframe::Command { window { args "" } } {
 
                     # Add the labelframe to the related toplevel keyboard pages navigation bindings.
                     ::ms::Enable_Traversal $w
+
+                    #####################
+                    ##                 ##
+                    ##     CLOSING     ##
+                    ##                 ##
+                    #####################
+
+                    # Set the widget real address relative to its short address, 'short_addr'.
+                    set ::ms::addr($short_addr,real) $w
+
+                    # Set the widget short addresses relative to its real address, 'w'.
+                    # They will all point to the widget hull object short address.
+                    set ::ms::addr($w,short)                          $short_addr
+                    set ::ms::addr($w.title,short)                    $short_addr
+                    set ::ms::addr($w.container,short)                $short_addr
+                    set ::ms::addr($w.container.border,short)         $short_addr
+                    set ::ms::addr($w.container.border.content,short) $short_addr
+
+                    # Add the widget real and short address into the list of all available real and short addresses.
+                    lappend ::ms::addr(reals) $w \
+                                              $w.title \
+                                              $w.container \
+                                              $w.container.border \
+                                              $w.container.border.content;
+
+                    lappend ::ms::addr(shorts) $short_addr
+
+                    # Set the border object (where the 'Enter' and 'Leave' event will happen).
+                    set ::ms::addr($w,border) $w.container.border
+
+                    # Set the actual widget address (the widget that the developer was intended to build).
+                    set ::ms::addr($w,widget) $w.container.border.content
+
+                    # Set the structure addresses.
+                    # Is important to note that the scrollbar addresses must not be included.
+                    set ::ms::addr($w,structure) [list $w \
+                                                       $w.title \
+                                                       $w.container \
+                                                       $w.container.border \
+                                                       $w.container.border.content];
+
+                    # Add the widget address to the megawidget addresses list.
+                    lappend ::ms::addr(megawidgets) $w
+
+                    # Add the widget address to the megawidget container addresses list.
+                    lappend ::ms::addr(megawidgets,containers) $w
                 }
                 true {
                     ###################################
