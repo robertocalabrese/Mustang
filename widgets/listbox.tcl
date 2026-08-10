867a9e5bf6dc695ac9c4a8342f5abc307cff065e
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-10 12:01:34 +0200

    listbox: add the 'style' command

--- widgets/listbox.tcl
+++ widgets/listbox.tcl
@@ -1409,7 +1409,15 @@ proc ::ms::listbox::Pathname_Cmd { w cmd args } {
                 default { ::ms::Error "Invalid number of arguments." $caller_info }
             }
         }
-        style {}
+        style {
+            # Synopsis:
+            #
+            # *window* **style**
+            switch -- [llength $args] {
+                0       { return $::ms::current($w,style) }
+                default { ::ms::Error "Invalid number of arguments." $caller_info }
+            }
+        }
         xview {}
         yview {}
         default { ::ms::Error "Invalid option, '$cmd'." $caller_info }
