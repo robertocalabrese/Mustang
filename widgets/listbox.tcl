e7e6aac718dc0e6eae02d28ebbc599edffd3daea
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-10 12:17:55 +0200

    listbox: add the 'Hover' procedure

--- widgets/listbox.tcl
+++ widgets/listbox.tcl
@@ -3291,4 +3291,60 @@ proc ::ms::listbox::Home { w } {
     return ""
 }
 
+## Hover
+#
+# Manage the **Enter** and **Leave** event on the widget.
+#
+# Where:
+#
+# w      Should be the widget real address involved.
+#
+# X, Y   Should be the mouse pointer (X,Y) root coordinates.
+#        These value are provided directly by the **Enter** or **Leave** event.
+#
+# It doesn't return anything.
+proc ::ms::listbox::Hover { w X Y } {
+    # Get the dimensions of the widget that acts as a border object.# Get the border object dimensions.
+    set height [_winfo height $::ms::addr($w,border)]
+    set width  [_winfo width  $::ms::addr($w,border)]
+
+    # Get the north-west (nw) root coordinates of the widget that acts as a border object.# Get the border object north-west (nw) root coordinates.
+    set X_nw [_winfo rootx $::ms::addr($w,border)]
+    set Y_nw [_winfo rooty $::ms::addr($w,border)]
+
+    # Get the widget south-east (se) root coordinates of the widget that acts as a border object.# Get the address south-east (se) root coordinates.
+    set X_se [expr { $X_nw+$width }]
+    set Y_se [expr { $Y_nw+$height }]
+
+    # Check if the mouse pointer coordinates are inside or outside the widget acting as a border object.
+    if { ($X <= $X_nw) || ($X >= $X_se) || ($Y <= $Y_nw) || ($Y >= $Y_se) } {
+        # The mouse cursor is outside the widget acting as a border object.
+
+        # Change the widget dynamic state to '!hover'.
+        ::ms::listbox::Pathname_Cmd $w state !hover
+
+        # Check if there are items associated to the listbox.
+        switch -- $::ms::current($w,values) {
+            ""      {}
+            default {
+                # Recolor any index with the default colors (background and foreground).
+                set index 0
+                while { $index < [$w.listbox size] } {
+                    $w.listbox itemconfigure $index -background $::ms::current($w,background) \
+                                                    -foreground $::ms::current($w,foreground);
+
+                    incr index
+                }
+            }
+        }
+    } else {
+        # The mouse cursor is inside the widget acting as a border object.
+
+        # Change the widget dynamic state to 'hover'.
+        ::ms::listbox::Pathname_Cmd $w state hover
+    }
+
+    return ""
+}
+
 #*EOF*
