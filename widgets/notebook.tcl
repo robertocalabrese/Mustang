efb7da78993bad5ed5e1b9e6f21b134c42fab91b
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-12 09:11:05 +0200

    notebook: add the 'Configure' procedure

--- widgets/notebook.tcl
+++ widgets/notebook.tcl
@@ -1920,4 +1920,25 @@ proc ::ms::notebook::Style_Update { stylename caller_info } {
 ##                                  ##
 ######################################
 
+## Configure
+#
+# Manage the **Configure** event on a widget.
+#
+# Where:
+#
+# w   Should be the widget real address involved.
+#
+# It doesn't return anything.
+proc ::ms::notebook::Configure { w } {
+    # Check if we are here due to a widget configure command or not.
+    switch -- [info exists ::ms::temp($w,height)] {
+        1   {
+            set ::ms::current($w,height) $::ms::temp($w,height)
+            set ::ms::current($w,width)  $::ms::temp($w,width)
+        }
+    }
+
+    return ""
+}
+
 #*EOF*
