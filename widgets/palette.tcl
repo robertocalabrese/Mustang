00e3c60c48ec48b3dda53753a4216d7549dd2fca
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-12 16:59:11 +0200

    palette: add the 'Popdown_PageUp' procedure

--- widgets/palette.tcl
+++ widgets/palette.tcl
@@ -5099,4 +5099,46 @@ proc ::ms::palette::Popdown_PageDown { w } {
     return -code break
 }
 
+## Popdown_PageUp
+#
+# Move the listbox view towards the top by one page and
+# select the first visible row.
+#
+# Where:
+#
+# w   Should be the palette real address involved.
+#
+# It doesn't return anything.
+proc ::ms::palette::Popdown_PageUp { w } {
+    if { [llength $::ms::data($w,colornames)] > $::ms::current($w,rows) } {
+        # Scroll one page towards the top of the popdown window.
+        $w.popdown.f.lb yview scroll -1 pages
+
+        # Get the index of the current top visible row.
+        set index [$w.popdown.f.lb index @0,0]
+
+        # Select and activate the current top visible row.
+        $w.popdown.f.lb activate $index
+        $w.popdown.f.lb selection clear 0 end
+        $w.popdown.f.lb selection set $index
+
+        # Set the preview color and its bordercolor (black or white).
+        set preview_color [lindex $::ms::data($w,hexadecimals) $index]
+        switch -- [string length $preview_color] {
+            10      { set bordercolor [::ms::palette::Black_Or_White $preview_color 12] }
+            13      { set bordercolor [::ms::palette::Black_Or_White $preview_color 16] }
+            default { set bordercolor [::ms::palette::Black_Or_White $preview_color 8 ] }
+        }
+
+        # Apply the changes to the preview object.
+        $w.preview configure          -background $preview_color \
+                             -highlightbackground $bordercolor \
+                                  -highlightcolor $bordercolor;
+    } else {
+        ::ms::palette::Popdown_Home $w
+    }
+
+    return -code break
+}
+
 #*EOF*
