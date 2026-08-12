071f0e8ea1faac2177e73135482da6137e8f3edb
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-12 11:50:50 +0200

    palette: add the '_Palette' bindings

--- widgets/palette.tcl
+++ widgets/palette.tcl
@@ -64,6 +64,81 @@
 #   [text](/wiki/...)    --> Link to another file in the wiki.
 package provide ::ms::palette 0.1
 
+###############################
+##                           ##
+##     _PALETTE BINDINGS     ##
+##                           ##
+###############################
+
+# Activate/Deactivate
+_bind _Palette <Activate>   { ::ms::palette::Pathname_Cmd %W state !background; break }
+_bind _Palette <Deactivate> { ::ms::palette::Pathname_Cmd %W state  background; break }
+
+# ButtonPress-1
+_bind _Palette <ButtonPress-1> { ::ms::Focus_The_Widget_Or_Its_Toplevel %W; break }
+
+# Contextual menu
+_bind _Palette <<ContextMenu>> { ::ms::Show_ContextMenu %W %X %Y shell; break }
+
+# Destroy
+_bind _Palette <Destroy> { ::ms::palette::Destroy %W; break }
+
+# Enter
+_bind _Palette <Enter> { ::ms::palette::Pathname_Cmd %W state !hover; break }
+
+# FocusIn/FocusOut
+_bind _Palette <FocusIn>  { ::ms::palette::FocusIn  %W; break }
+_bind _Palette <FocusOut> { ::ms::palette::FocusOut %W; break }
+
+# Mousewheel and Touchpad
+
+# Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+# and move that scrollbar by one unit up or down (depending on the mousewheel direction).
+# If none of the widget's parents meets the required condition, don't do anything.
+_bind _Palette <MouseWheel> { ::ms::Scroll_Parent_Y %W %D units; break }
+
+# Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+# and move that scrollbar by one unit left or right (depending on the mousewheel direction).
+# If none of the widget's parents meets the required condition, don't do anything.
+_bind _Palette <Shift-MouseWheel> { ::ms::Scroll_Parent_X %W %D units; break }
+
+# Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+# and move that scrollbar by one page up or down (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Palette <Control-MouseWheel> { ::ms::Scroll_Parent_Y %W %D pages; break }
+
+# Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+# and move that scrollbar by one page left or right (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Palette <Control-Shift-MouseWheel> { ::ms::Scroll_Parent_X %W %D pages; break }
+
+# Note: **TouchpadScroll** and **Control-TouchpadScroll** only works on Windows and macOS.
+#       On Linux they will be ignored and touchpads movements will be processed as mousewheel events.
+
+# This binding movement will happen on two different planes, horizontal (1) and vertical (2).
+# These two planes may involve different widgets depending on the active scrollbars on them and on the
+# touchpad direction.
+#   1 - Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+#       and move that scrollbar by one page left or right (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the horizontal axis.
+#
+#   2 - Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+#       and move that scrollbar by one page up or down (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the vertical axis.
+_bind _Palette <TouchpadScroll> { ::ms::palette::Touchpad %W %# %D; break }
+
+# This binding movement will happen on two different planes, horizontal (1) and vertical (2).
+# These two planes may involve different widgets depending on the active scrollbars on them and on the
+# touchpad direction.
+#   1 - Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+#       and move that scrollbar by one page left or right (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the horizontal axis.
+#
+#   2 - Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+#       and move that scrollbar by one page up or down (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the vertical axis.
+_bind _Palette <Control-TouchpadScroll> { ::ms::Touchpad_Parent %W %# %D pages; break }
+
 # Create the mustang **palette** package.
 namespace eval ::ms::palette {}
 
