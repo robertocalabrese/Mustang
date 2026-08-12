4c3ed903016c0d548da75afeb0ab61228f3a9f5a
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-12 16:41:52 +0200

    palette: set the default value for each styleable option and if the option is managed by Tk, set also its current value

--- widgets/palette.tcl
+++ widgets/palette.tcl
@@ -2704,7 +2704,21 @@ proc ::ms::palette::Style_Update { stylename caller_info } {
     }
 
     # Update all the palette widgets addresses that have stylename as a style.
-    foreach w $::ms::style($stylename,palette,addrs) {}
+    foreach w $::ms::style($stylename,palette,addrs) {
+        # Set the default value for each styleable option and if the option is managed by Tk, set also its current value.
+        foreach option $::ms::palette(styleable,options) {
+            set ::ms::default($w,$option) $::ms::styleopt($::ms::theme,TPalette,$option)
+
+            switch -- $::ms::managed_by($w,$option) {
+                Tk  {
+                    switch -- [info exists ::ms::styleopt($::ms::theme,$stylename,$option)] {
+                        0   { set ::ms::current($w,$option) $::ms::default($w,$option) }
+                        1   { set ::ms::current($w,$option) $::ms::styleopt($::ms::theme,$stylename,$option) }
+                    }
+                }
+            }
+        }
+    }
 
     return ""
 }
