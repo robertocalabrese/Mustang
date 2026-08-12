816c08b03b739a7947fe1ce3165563d3ffcfc845
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-12 16:42:27 +0200

    palette: check the widget state and set the cursor accordingly

--- widgets/palette.tcl
+++ widgets/palette.tcl
@@ -2718,6 +2718,19 @@ proc ::ms::palette::Style_Update { stylename caller_info } {
                 }
             }
         }
+
+        # Check the widget state and set the cursor accordingly.
+        switch -- $::ms::current($w,state) {
+            disabled { set cursor arrow }
+            readonly {
+                # Check the windowing system.
+                switch -- [_tk windowingsystem] {
+                    aqua    { set cursor hand1 }
+                    default { set cursor arrow }
+                }
+            }
+            normal { set cursor $::ms::current($w,cursor) }
+        }
     }
 
     return ""
