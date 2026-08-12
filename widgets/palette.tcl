0ea8c4f2696c6bccc5614ea3b8b63f002dff91c7
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-12 12:05:53 +0200

    palette: add the 'set' command

--- widgets/palette.tcl
+++ widgets/palette.tcl
@@ -1728,7 +1728,44 @@ proc ::ms::palette::Pathname_Cmd { w cmd args } {
                 default { ::ms::Error "Invalid number of arguments." $caller_info }
             }
         }
-        set {}
+        set {
+            # Synopsis:
+            #
+            # *window* **set** *value*
+            switch -- [llength $args] {
+                1   {
+                    # Check that the value provided exists inside the '::ms::data($w,colornames)' list.
+                    set index [lsearch -exact -nocase $::ms::data($w,colornames) $args]
+                    switch -- $index {
+                        -1      {}
+                        default {
+                            # Update the current index, value and preview color.
+                            set ::ms::data($w,current_index) $index
+                            set ::ms::data($w,current_value) [lindex $::ms::data($w,colornames)   $index]
+                            set ::ms::data($w,current_hex)   [lindex $::ms::data($w,hexadecimals) $index]
+
+                            # Apply the changes to the combobox object.
+                            $w.combobox current $index
+
+                            # Set the bordercolor of the preview object (black or white).
+                            switch -- [string length $::ms::data($w,current_hex)] {
+                                10      { set bordercolor [::ms::palette::Black_Or_White $::ms::data($w,current_hex) 12] }
+                                13      { set bordercolor [::ms::palette::Black_Or_White $::ms::data($w,current_hex) 16] }
+                                default { set bordercolor [::ms::palette::Black_Or_White $::ms::data($w,current_hex) 8 ] }
+                            }
+
+                            # Apply the changes to the preview object.
+                            $w.preview configure          -background $::ms::data($w,current_hex) \
+                                                 -highlightbackground $bordercolor \
+                                                      -highlightcolor $bordercolor;
+                        }
+                    }
+
+                    return ""
+                }
+                default { ::ms::Error "Invalid number of arguments." $caller_info }
+            }
+        }
         state {}
         style {}
         xview {}
