6f88e948071649de76abc0512f7f24b3a4f95630
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-12 11:52:51 +0200

    palette: set the widget real and short addresses

--- widgets/palette.tcl
+++ widgets/palette.tcl
@@ -237,9 +237,13 @@ interp alias {} palette {} ::ms::palette::Command
 #
 # Returns the pathname of the new window created.
 proc ::ms::palette::Command { window { args "" } } {
-    # For the time being return the empty string.
-    # Palettes are not covered until the new command is written.
-    return ""
+    # Get the caller information.
+    set caller_info [info frame -1]
+
+    # Check the 'window' address and set its real and short addresses.
+    set addresses  [::ms::Check_Widget_Address $window $caller_info]
+    set w          [lindex $addresses 0]
+    set short_addr [lindex $addresses 1]
 }
 
 #####################################
