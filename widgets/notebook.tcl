7779d89fb00c45de9e7ea5af79f7f311c3a454e5
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-12 09:00:21 +0200

    notebook: add the 'cget' command

--- widgets/notebook.tcl
+++ widgets/notebook.tcl
@@ -753,7 +753,24 @@ proc ::ms::notebook::Pathname_Cmd { w cmd args } {
                 return ""
             }
         }
-        cget {}
+        cget {
+            # Synopsis:
+            #
+            # *window* **cget** *option*
+            switch -- [llength $args] {
+                0   { ::ms::Error "Missing cget option." $caller_info }
+                1   {
+                    # Check if the option provided is a valid 'styleable' or 'non-styleable' option.
+                    set option [string range $args 1 end]
+                    if { ($option in $::ms::notebook(non_styleable,options)) || ($option in $::ms::notebook(styleable,options))} {
+                        return $::ms::current($w,$option)
+                    } else {
+                        ::ms::Error "Invalid option, '$args'." $caller_info
+                    }
+                }
+                default { ::ms::Error "Invalid option, '$args'." $caller_info }
+            }
+        }
         configure {}
         forget {}
         hide {}
