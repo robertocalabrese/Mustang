f6030a1c4f88dcc1abf054b5d8488b1f3c456d65
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-13 12:20:39 +0200

    label: refactor the automatic translation

--- widgets/label.tcl
+++ widgets/label.tcl
@@ -1415,19 +1415,13 @@ proc ::ms::label::Command { window { args "" } } {
             # Enable/Disable the widget automatic translation.
             switch -- $::ms::current($w,textvariable) {
                 ""  {
-                    switch -- [llength $::ms::current($w,text)] {
-                        0       { set ::ms::data($w,translated_text) $::ms::current($w,text) }
-                        default {
-                            # Automatic translation: ON
-                            set ::ms::data($w,translated_text) [::msgcat::mc {*}$::ms::current($w,text)]
-                        }
-                    }
-
-                    set textvariable ::ms::data($w,translated_text)
+                    # Automatic translation: ON
+                    set ::ms::data($w,translated_text) [::msgcat::mc {*}$::ms::current($w,text)]
+                    set text_variable ::ms::data($w,translated_text)
                 }
                 default {
                     # Automatic translation: OFF
-                    set textvariable $::ms::current($w,textvariable)
+                    set text_variable $::ms::current($w,textvariable)
                 }
             }
 
@@ -1548,7 +1542,7 @@ proc ::ms::label::Command { window { args "" } } {
                                  -style $::ms::style($w,widget) \
                              -takefocus $takefocus \
                                   -text "" \
-                          -textvariable $textvariable \
+                          -textvariable $text_variable \
                              -underline $::ms::current($w,underline) \
                                  -width $::ms::current($w,charwidth) \
                             -wraplength $::ms::current($w,wraplength);
@@ -2050,19 +2044,13 @@ proc ::ms::label::Pathname_Cmd { w cmd args } {
                             # Enable/Disable the widget automatic translation.
                             switch -- $::ms::current($w,textvariable) {
                                 ""  {
-                                    switch -- [llength $::ms::current($w,text)] {
-                                        0       { set ::ms::data($w,translated_text) $::ms::current($w,text) }
-                                        default {
-                                            # Automatic translation: ON
-                                            set ::ms::data($w,translated_text) [::msgcat::mc {*}$::ms::current($w,text)]
-                                        }
-                                    }
-
-                                    set textvariable ::ms::data($w,translated_text)
+                                    # Automatic translation: ON
+                                    set ::ms::data($w,translated_text) [::msgcat::mc {*}$::ms::current($w,text)]
+                                    set text_variable ::ms::data($w,translated_text)
                                 }
                                 default {
                                     # Automatic translation: OFF
-                                    set textvariable $::ms::current($w,textvariable)
+                                    set text_variable $::ms::current($w,textvariable)
                                 }
                             }
 
@@ -2181,7 +2169,7 @@ proc ::ms::label::Pathname_Cmd { w cmd args } {
                                                                        -state $::ms::current($w,state) \
                                                                        -style $::ms::style($w,widget) \
                                                                    -takefocus $takefocus \
-                                                                -textvariable $textvariable \
+                                                                -textvariable $text_variable \
                                                                    -underline $::ms::current($w,underline) \
                                                                        -width $::ms::current($w,charwidth) \
                                                                   -wraplength $::ms::current($w,wraplength);
