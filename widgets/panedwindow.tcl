b582c6e33ef0c2fd45de1abae1132b2e8e640468
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-13 10:31:07 +0200

    panedwindow: check the configure options provided

--- widgets/panedwindow.tcl
+++ widgets/panedwindow.tcl
@@ -765,6 +765,182 @@ proc ::ms::panedwindow::Pathname_Cmd { w cmd args } {
 
                             # Remove any duplicated options (retain only the last ones).
                             set args [lsort -increasing -stride 2 -index 0 -unique $args]
+
+                            ##################################################
+                            ##                                              ##
+                            ##     CHECK THE CONFIGURE OPTIONS PROVIDED     ##
+                            ##                                              ##
+                            ##################################################
+
+                            # Check the remaining widget's options, if any.
+                            foreach { option value } $args {
+                                switch -nocase -- $option {
+                                    -background {
+                                        set value [::ms::Check_Color $value invalid]
+                                        switch -- $value {
+                                            invalid { continue }
+                                        }
+
+                                        set ::ms::current($w,background)    $value
+                                        set ::ms::managed_by($w,background) developer
+                                    }
+                                    -bordercolor {
+                                        set value [::ms::Check_Color $value invalid]
+                                        switch -- $value {
+                                            invalid { continue }
+                                        }
+
+                                        set ::ms::current($w,bordercolor)    $value
+                                        set ::ms::managed_by($w,bordercolor) developer
+                                    }
+                                    -borderwidth {
+                                        set value [::ms::Check_Measure $value invalid]
+                                        switch -- $value {
+                                            invalid { continue }
+                                        }
+
+                                        set ::ms::current($w,borderwidth)    $value
+                                        set ::ms::managed_by($w,borderwidth) developer
+                                    }
+                                    -class {}
+                                    -cursor {
+                                        set value [string tolower $value]
+                                        if { ($value eq "") || ($value in $::ms::machine(os,cursors)) } {
+                                            set ::ms::current($w,cursor)    $value
+                                            set ::ms::managed_by($w,cursor) developer
+                                        }
+                                    }
+                                    -darkcolor {
+                                        set value [::ms::Check_Color $value invalid]
+                                        switch -- $value {
+                                            invalid { continue }
+                                        }
+
+                                        set ::ms::current($w,darkcolor)    $value
+                                        set ::ms::managed_by($w,darkcolor) developer
+                                    }
+                                    -height {
+                                        switch -- [string is integer -strict $value] {
+                                            0   {
+                                                set value [::ms::Check_Measure $value invalid]
+                                                switch -- $value {
+                                                    invalid { continue }
+                                                }
+                                            }
+                                            1   {
+                                                if { $value < 0 } {
+                                                    set value 0
+                                                }
+                                            }
+                                        }
+
+                                        set ::ms::temp($w,height) $value
+                                    }
+                                    -lightcolor {
+                                        set value [::ms::Check_Color $value invalid]
+                                        switch -- $value {
+                                            invalid { continue }
+                                        }
+
+                                        set ::ms::current($w,lightcolor)    $value
+                                        set ::ms::managed_by($w,lightcolor) developer
+                                    }
+                                    -orient {}
+                                    -relief {
+                                        set value [string tolower $value]
+                                        switch -- $value {
+                                            flat   -
+                                            groove -
+                                            raised -
+                                            ridge  -
+                                            solid  -
+                                            sunken {
+                                                set ::ms::current($w,relief)    $value
+                                                set ::ms::managed_by($w,relief) developer
+                                            }
+                                        }
+                                    }
+                                    -state {
+                                        set value [string tolower $value]
+                                        switch -- $value {
+                                            normal   -
+                                            disabled { set ::ms::current($w,state) $value }
+                                        }
+                                    }
+                                    -style {
+                                        if { $value in $::ms::style($::ms::theme) } {
+                                            # Check if exists a layout for the style provided.
+                                            # If not, create one by mirroring the current theme 'TPanedwindow' layout.
+                                            if { $value ni $::ms::layouts($::ms::theme) } {
+                                                _ttk_style layout $value [_ttk_style layout TPanedwindow]
+                                            }
+
+                                            # Remove the widget address from the panedwindow real address list that contains all the
+                                            # widgets addresses with style '::ms::current($w,style)'.
+                                            set index [lsearch -exact $::ms::style($::ms::current($w,style),panedwindow,addrs) $w]
+                                            switch -- $index {
+                                                -1      {}
+                                                default { set ::ms::style($::ms::current($w,style),panedwindow,addrs) [lremove $::ms::style($::ms::current($w,style),panedwindow,addrs) $index] }
+                                            }
+
+                                            # Add the widget address to the address list that contains all the
+                                            # widgets addresses with style 'value'.
+                                            lappend ::ms::style($value,panedwindow,addrs) $w
+
+                                            # If needed, remove the '::ms::current($w,style)' from the list that contains the available styles
+                                            # for the panedwindow classtype.
+                                            switch -- [llength $::ms::style($::ms::current($w,style),panedwindow,addrs)] {
+                                                0   {
+                                                    set index [lsearch -exact $::ms::style(panedwindow,classtype) $::ms::current($w,style)]
+                                                    switch -- $index {
+                                                        -1      {}
+                                                        default { set ::ms::style(panedwindow,classtype) [lremove $::ms::style(panedwindow,classtype) $index] }
+                                                    }
+                                                }
+                                            }
+
+                                            # If needed, add 'value' to the available styles for the panedwindow classtype.
+                                            if { $value ni $::ms::style(panedwindow,classtype) } {
+                                                lappend ::ms::style(panedwindow,classtype) $value
+                                            }
+
+                                            # Update the current style associated with the widget with 'value'.
+                                            set ::ms::current($w,style) $value
+                                        }
+                                    }
+                                    -takefocus {
+                                        switch -nocase -- $value {
+                                            0        -
+                                            no       -
+                                            off      -
+                                            false    -
+                                            disabled { set ::ms::current($w,takefocus) 0 }
+                                            1        -
+                                            yes      -
+                                            on       -
+                                            true     -
+                                            enabled  { set ::ms::current($w,takefocus) 1 }
+                                        }
+                                    }
+                                    -width {
+                                        switch -- [string is integer -strict $value] {
+                                            0   {
+                                                set value [::ms::Check_Measure $value invalid]
+                                                switch -- $value {
+                                                    invalid { continue }
+                                                }
+                                            }
+                                            1   {
+                                                if { $value < 0 } {
+                                                    set value 0
+                                                }
+                                            }
+                                        }
+
+                                        set ::ms::temp($w,width) $value
+                                    }
+                                }
+                            }
                         }
                         default { ::ms::Error "Invalid number of arguments." $caller_info }
                     }
