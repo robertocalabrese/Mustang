0e9f9fdf7dc0297b2e3dcbab61dbcbbadbc0c059
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-13 15:24:17 +0200

    progressbar: add the 'FocusIn' procedure

--- widgets/progressbar.tcl
+++ widgets/progressbar.tcl
@@ -1817,4 +1817,20 @@ proc ::ms::progressbar::Destroy { w } {
     return ""
 }
 
+## FocusIn
+#
+# Manage the **FocusIn** event on the widget.
+#
+# Where:
+#
+# w   Should be the widget real address involved.
+#
+# It doesn't return anything.
+proc ::ms::progressbar::FocusIn { w } {
+    # Change the widget dynamic state to 'focus'.
+    ::ms::progressbar::Pathname_Cmd $w state focus
+
+    return ""
+}
+
 #*EOF*
