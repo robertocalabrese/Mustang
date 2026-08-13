e65be3abbcb21f5c0403930e7646cceb0ddcf7be
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-13 16:01:33 +0200

    radiobutton: check the widget's options provided

--- widgets/radiobutton.tcl
+++ widgets/radiobutton.tcl
@@ -297,6 +297,266 @@ proc ::ms::radiobutton::Command { window { args "" } } {
             set ::ms::managed_by($w,shellbackground)     Tk
             set ::ms::managed_by($w,spacer)              Tk
             set ::ms::managed_by($w,wraplength)          Tk
+
+            #################################################
+            ##                                             ##
+            ##     CHECK THE WIDGET'S OPTIONS PROVIDED     ##
+            ##                                             ##
+            #################################################
+
+            # Check the remaining options, if any.
+            foreach { option value } $args {
+                switch -nocase -- $option {
+                    -charwidth {
+                        switch -- [string is integer -strict $value] {
+                            0   { continue }
+                        }
+
+                        set ::ms::current($w,charwidth)    $value
+                        set ::ms::managed_by($w,charwidth) developer
+                    }
+                    -class { set ::ms::current($w,class) $value }
+                    -cmenu {
+                        set value [string trim $value]
+                        if { ($value eq "") || ($value in $::ms::addr(cmenu)) } {
+                            set ::ms::current($w,cmenu) $value
+                        }
+                    }
+                    -command { set ::ms::current($w,command) $value }
+                    -cursor {
+                        set value [string tolower $value]
+                        if { ($value eq "") || ($value in $::ms::machine(os,cursors)) } {
+                            set ::ms::current($w,cursor)    $value
+                            set ::ms::managed_by($w,cursor) developer
+                        }
+                    }
+                    -font {
+                        if { $value in [font names] } {
+                            set ::ms::current($w,font)    $value
+                            set ::ms::managed_by($w,font) developer
+                        }
+                    }
+                    -foreground {
+                        set value [::ms::Check_Color $value invalid]
+                        switch -- $value {
+                            invalid { continue }
+                        }
+
+                        set ::ms::current($w,foreground)    $value
+                        set ::ms::managed_by($w,foreground) developer
+                    }
+                    -highlightcolor {
+                        set value [::ms::Check_Color $value invalid]
+                        switch -- $value {
+                            invalid { continue }
+                        }
+
+                        set ::ms::current($w,highlightcolor)    $value
+                        set ::ms::managed_by($w,highlightcolor) developer
+                    }
+                    -image {
+                        switch -- [::ms::Check_Image $value] {
+                            invalid { continue }
+                        }
+
+                        set ::ms::current($w,image)    $value
+                        set ::ms::managed_by($w,image) developer
+                    }
+                    -indicatorbackground {
+                        set value [::ms::Check_Color $value invalid]
+                        switch -- $value {
+                            invalid { continue }
+                        }
+
+                        set ::ms::current($w,indicatorbackground)    $value
+                        set ::ms::managed_by($w,indicatorbackground) developer
+                    }
+                    -indicatorforeground {
+                        set value [::ms::Check_Color $value invalid]
+                        switch -- $value {
+                            invalid { continue }
+                        }
+
+                        set ::ms::current($w,indicatorforeground)    $value
+                        set ::ms::managed_by($w,indicatorforeground) developer
+                    }
+                    -indicatorrelief {
+                        set value [string tolower $value]
+                        switch -- $value {
+                            flat   -
+                            groove -
+                            raised -
+                            ridge  -
+                            solid  -
+                            sunken {
+                                set ::ms::current($w,indicatorrelief)    $value
+                                set ::ms::managed_by($w,indicatorrelief) developer
+                            }
+                        }
+                    }
+                    -justify {
+                        set value [string tolower $value]
+                        switch -- $value {
+                            center -
+                            left   -
+                            right  {
+                                set ::ms::current($w,justify)    $value
+                                set ::ms::managed_by($w,justify) developer
+                            }
+                        }
+                    }
+                    -padding {
+                        switch -- [llength $value] {
+                            1   {
+                                set value [::ms::Check_Measure $value invalid]
+                                switch -- $value {
+                                    invalid { continue }
+                                }
+
+                                set ::ms::current($w,padding) [list $value]
+                            }
+                            2   {
+                                set pad_horizontal [::ms::Check_Measure [lindex $value 0] invalid]
+                                switch -- $pad_horizontal {
+                                    invalid { continue }
+                                }
+
+                                set pad_vertical [::ms::Check_Measure [lindex $value 1] invalid]
+                                switch -- $pad_vertical {
+                                    invalid { continue }
+                                }
+
+                                set ::ms::current($w,padding) [list $pad_horizontal $pad_vertical]
+                            }
+                            3   {
+                                set pad_left [::ms::Check_Measure [lindex $value 0] invalid]
+                                switch -- $pad_left {
+                                    invalid { continue }
+                                }
+
+                                set pad_vertical [::ms::Check_Measure [lindex $value 1] invalid]
+                                switch -- $pad_vertical {
+                                    invalid { continue }
+                                }
+
+                                set pad_right [::ms::Check_Measure [lindex $value 2] invalid]
+                                switch -- $pad_right {
+                                    invalid { continue }
+                                }
+
+                                set ::ms::current($w,padding) [list $pad_left $pad_vertical $pad_right]
+                            }
+                            4   {
+                                set pad_left [::ms::Check_Measure [lindex $value 0] invalid]
+                                switch -- $pad_left {
+                                    invalid { continue }
+                                }
+
+                                set pad_top [::ms::Check_Measure [lindex $value 1] invalid]
+                                switch -- $pad_top {
+                                    invalid { continue }
+                                }
+
+                                set pad_right [::ms::Check_Measure [lindex $value 2] invalid]
+                                switch -- $pad_right {
+                                    invalid { continue }
+                                }
+
+                                set pad_bottom [::ms::Check_Measure [lindex $value 3] invalid]
+                                switch -- $pad_bottom {
+                                    invalid { continue }
+                                }
+
+                                set ::ms::current($w,padding) [list $pad_left $pad_top $pad_right $pad_bottom]
+                            }
+                            default { continue }
+                        }
+
+                        set ::ms::managed_by($w,padding) developer
+                    }
+                    -shellbackground {
+                        set value [::ms::Check_Color $value invalid]
+                        switch -- $value {
+                            invalid { continue }
+                        }
+
+                        set ::ms::current($w,shellbackground)    $value
+                        set ::ms::managed_by($w,shellbackground) developer
+                    }
+                    -spacer {
+                        set value [::ms::Check_Measure $value invalid]
+                        switch -- $value {
+                            invalid { continue }
+                        }
+
+                        set ::ms::current($w,spacer)    $value
+                        set ::ms::managed_by($w,spacer) developer
+                    }
+                    -state {
+                        set value [string tolower $value]
+                        switch -- $value {
+                            disabled -
+                            normal   { set ::ms::current($w,state) $value }
+                        }
+                    }
+                    -style {
+                        if { $value in $::ms::style($::ms::theme) } {
+                            # Check if exists a layout for the style provided.
+                            # If not, create one by mirroring the current theme 'TRadiobutton' layout.
+                            if { $value ni $::ms::layouts($::ms::theme) } {
+                                _ttk_style layout $value [_ttk_style layout TRadiobutton]
+                            }
+
+                            set ::ms::current($w,style) $value
+                        }
+                    }
+                    -takefocus {
+                        switch -nocase -- $value {
+                            0        -
+                            no       -
+                            off      -
+                            false    -
+                            disabled { set ::ms::current($w,takefocus) 0 }
+                            1        -
+                            yes      -
+                            on       -
+                            true     -
+                            enabled  { set ::ms::current($w,takefocus) 1 }
+                        }
+                    }
+                    -text { set ::ms::current($w,text) $value }
+                    -textvariable {
+                        switch -- [info exists $value] {
+                            1   { set ::ms::current($w,textvariable) $value }
+                        }
+                    }
+                    -underline {
+                        switch -- [string is integer -strict $value] {
+                            1   {
+                                if { $value >= -1 } {
+                                    set ::ms::current($w,underline) $value
+                                }
+                            }
+                        }
+                    }
+                    -value { set ::ms::current($w,value) $value }
+                    -variable {
+                        switch -- [info exists $value] {
+                            0   { continue }
+                        }
+
+                        set ::ms::current($w,variable) $value
+                    }
+                    -wraplength {
+                        switch -- [string is integer -strict $value] {
+                            0   { continue }
+                        }
+
+                        set ::ms::current($w,wraplength)    $value
+                        set ::ms::managed_by($w,wraplength) developer
+                    }
+                }
+            }
         }
         default { ::ms::Error "Invalid number of arguments." $caller_info }
     }
