37e9c69c6ddca69d9d9c6991d29a625fca2ae874
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-13 16:13:47 +0200

    radiobutton: add the 'identify' command

--- widgets/radiobutton.tcl
+++ widgets/radiobutton.tcl
@@ -1129,7 +1129,57 @@ proc ::ms::radiobutton::Pathname_Cmd { w cmd args } {
             }
         }
         configure {}
-        identify {}
+        identify {
+            # Synopsis:
+            #
+            # *window* **identify** **element** *x* *y*
+            switch -- [llength $args] {
+                3   {
+                    # Check that the first argument of 'args' is the word "element".
+                    switch -- [lindex $args 0] {
+                        element {}
+                        default { ::ms::Error "Invalid option, '$args'." $caller_info }
+                    }
+
+                    set x [lindex $args 1]
+                    set y [lindex $args 2]
+
+                    # Check that the coordinates provided are valid.
+                    switch -- [string is integer -strict $x] {
+                        0   { ::ms::Error "Invalid coordinate, '$x'." $caller_info }
+                    }
+
+                    switch -- [string is integer -strict $y] {
+                        0   { ::ms::Error "Invalid coordinate, '$y'." $caller_info }
+                    }
+
+                    # Get the root coordinates of the north-west corner of the container ('$w').
+                    set rootx [_winfo rootx $w]
+                    set rooty [_winfo rooty $w]
+
+                    # Transform the relative coordinates provided into root coordinates.
+                    set X [expr { $rootx+$x }]
+                    set Y [expr { $rooty+$y }]
+
+                    # Get the widget address containing the point given by the root coordinates calculated.
+                    set widget [_winfo containing -display $w $X $Y]
+
+                    # Return the name of the object, or an empty string if there are no checkbutton objects at the coordinates provided.
+                    if { $widget eq $w } {
+                        return "Radiobutton.hull"
+                    } elseif { $widget eq "$w.indicator" } {
+                        return "Radiobutton.indicator"
+                    } elseif { $widget eq "$w.label" } {
+                        return "Radiobutton.label"
+                    } elseif { $widget eq "$w.highlight" } {
+                        return "Radiobutton.highlight"
+                    } else {
+                        return ""
+                    }
+                }
+                default { ::ms::Error "Invalid number of arguments." $caller_info }
+            }
+        }
         instate {}
         invoke {}
         state {}
