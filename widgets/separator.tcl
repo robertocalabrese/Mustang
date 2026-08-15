d5c76116b00ca98298321efa528ee4d470ef6336
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-15 10:20:03 +0200

    separator: check the configure options provided

--- widgets/separator.tcl
+++ widgets/separator.tcl
@@ -570,6 +570,100 @@ proc ::ms::separator::Pathname_Cmd { w cmd args } {
                         0   {
                             # Remove any duplicated options (retain only the last ones).
                             set args [lsort -increasing -stride 2 -index 0 -unique $args]
+
+                            ##################################################
+                            ##                                              ##
+                            ##     CHECK THE CONFIGURE OPTIONS PROVIDED     ##
+                            ##                                              ##
+                            ##################################################
+
+                            # Check the widget options provided.
+                            foreach { option value } $args {
+                                switch -nocase -- $option {
+                                    -background {
+                                        set value [::ms::Check_Color $value invalid]
+                                        switch -- $value {
+                                            invalid { continue }
+                                        }
+
+                                        set ::ms::current($w,background)    $value
+                                        set ::ms::managed_by($w,background) developer
+                                    }
+                                    -class {}
+                                    -cmenu {
+                                        set value [string trim $value]
+                                        if { ($value eq "") || ($value in $::ms::addr(cmenu)) } {
+                                            set ::ms::current($w,cmenu) $value
+                                        }
+                                    }
+                                    -cursor {
+                                        set value [string tolower $value]
+                                        if { ($value eq "") || ($value in $::ms::machine(os,cursors)) } {
+                                            set ::ms::current($w,cursor)    $value
+                                            set ::ms::managed_by($w,cursor) developer
+                                        }
+                                    }
+                                    -orient -
+                                    -state  {}
+                                    -style  {
+                                        if { $value ni $::ms::style($::ms::theme) } {
+                                            continue
+                                        }
+
+                                        # Check if exists a layout for the style provided.
+                                        # If not, create one by mirroring the current theme 'TSeparator' layout.
+                                        if { $value ni $::ms::layouts($::ms::theme) } {
+                                            _ttk_style layout $value [_ttk_style layout TSeparator]
+                                        }
+
+                                        # Remove the widget address from the separator classtype real address list that contains all the
+                                        # widgets addresses with style '::ms::current($w,style)'.
+                                        set index [lsearch -exact $::ms::style($::ms::current($w,style),separator,addrs) $w]
+                                        switch -- $index {
+                                            -1      {}
+                                            default { set ::ms::style($::ms::current($w,style),separator,addrs) [lremove $::ms::style($::ms::current($w,style),separator,addrs) $index] }
+                                        }
+
+                                        # Add the widget address to the address list that contains all the
+                                        # widgets addresses with style 'value'.
+                                        lappend ::ms::style($value,separator,addrs) $w
+
+                                        # If needed, remove the '::ms::current($w,style)' from the list that contains the available styles
+                                        # for the separator classtype.
+                                        switch -- [llength $::ms::style($::ms::current($w,style),separator,addrs)] {
+                                            0   {
+                                                set index [lsearch -exact $::ms::style(separator,classtype) $::ms::current($w,style)]
+                                                switch -- $index {
+                                                    -1      {}
+                                                    default { set ::ms::style(separator,classtype) [lremove $::ms::style(separator,classtype) $index] }
+                                                }
+                                            }
+                                        }
+
+                                        # If needed, add 'value' to the available styles for the separator classtype.
+                                        if { $value ni $::ms::style(separator,classtype) } {
+                                            lappend ::ms::style(separator,classtype) $value
+                                        }
+
+                                        # Update the current style associated with the widget with 'value'.
+                                        set ::ms::current($w,style) $value
+                                    }
+                                    -takefocus {
+                                        switch -nocase -- $value {
+                                            0        -
+                                            no       -
+                                            off      -
+                                            false    -
+                                            disabled { set ::ms::current($w,takefocus) 0 }
+                                            1        -
+                                            yes      -
+                                            on       -
+                                            true     -
+                                            enabled  { set ::ms::current($w,takefocus) 1 }
+                                        }
+                                    }
+                                }
+                            }
                         }
                         default { ::ms::Error "Invalid number of arguments." $caller_info }
                     }
