b23b1b2be6bd587f4045dd363e18d8fc06732c13
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-15 09:26:02 +0200

    scale: add the 'cget' command

--- widgets/scale.tcl
+++ widgets/scale.tcl
@@ -857,7 +857,24 @@ proc ::ms::scale::Pathname_Cmd { w cmd args } {
 
     # Check the command provided.
     switch -nocase -- $cmd {
-        cget {}
+        cget {
+            # Synopsis:
+            #
+            # *window* **cget** *option*
+            switch -- [llength $args] {
+                0   { ::ms::Error "Missing cget option." $caller_info }
+                1   {
+                    # Check if the option provided is a valid 'styleable' or 'non-styleable' option.
+                    set option [string range $args 1 end]
+                    if { ($option in $::ms::scale(non_styleable,options)) || ($option in $::ms::scale(styleable,options))} {
+                        return $::ms::current($w,$option)
+                    } else {
+                        ::ms::Error "Invalid option, '$args'." $caller_info
+                    }
+                }
+                default { ::ms::Error "Invalid option, '$args'." $caller_info }
+            }
+        }
         configure {}
         coords -
         get    {}
