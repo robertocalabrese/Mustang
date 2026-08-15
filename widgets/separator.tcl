1036c927b8e617b458dc239fc9644cc9ec699763
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-15 10:24:28 +0200

    separator: add the 'Destroy' event procedure

--- widgets/separator.tcl
+++ widgets/separator.tcl
@@ -1040,4 +1040,104 @@ proc ::ms::separator::Style_Update { stylename caller_info } {
 ##                                  ##
 ######################################
 
+## Destroy
+#
+# Manage the **Destroy** event on the widget.
+#
+# Where:
+#
+# w   Should be the widget real address involved.
+#
+# It doesn't return anything.
+proc ::ms::separator::Destroy { w } {
+    # Get the short address related to the widget real address.
+    set short_addr $::ms::addr($w,short)
+
+    # Destroy the aliased widget pathcommands.
+    foreach token $::ms::data($w,token) {
+        interp alias {} $token {}
+    }
+
+    # Remove the widget real address from the widgets real address list.
+    set index [lsearch -exact $::ms::addr(reals) $w]
+    switch -- $index {
+        -1      {}
+        default { set ::ms::addr(reals) [lremove $::ms::addr(reals) $index] }
+    }
+
+    # Remove the widget short address from the widgets short address list.
+    set index [lsearch -exact $::ms::addr(shorts) $short_addr]
+    switch -- $index {
+        -1      {}
+        default { set ::ms::addr(shorts) [lremove $::ms::addr(shorts) $index] }
+    }
+
+    # Remove the widget address from the separator widgets real address list.
+    set index [lsearch -exact $::ms::addr(separator) $w]
+    switch -- $index {
+        -1      {}
+        default { set ::ms::addr(separator) [lremove $::ms::addr(separator) $index] }
+    }
+
+    # Remove the widget address from the separator classtype real address list with class '::ms::current($w,class)'.
+    set index [lsearch -exact $::ms::class($::ms::current($w,class),separator,addrs) $w]
+    switch -- $index {
+        -1      {}
+        default { set ::ms::class($::ms::current($w,class),separator,addrs) [lremove $::ms::class($::ms::current($w,class),separator,addrs) $index] }
+    }
+
+    # Remove the widget address from the separator classtype real address list with style '::ms::current($w,style)'.
+    set index [lsearch -exact $::ms::style($::ms::current($w,style),separator,addrs) $w]
+    switch -- $index {
+        -1      {}
+        default { set ::ms::style($::ms::current($w,style),separator,addrs) [lremove $::ms::style($::ms::current($w,style),separator,addrs) $index] }
+    }
+
+    # If needed, remove the '::ms::current($w,style)' from the list that contains the available styles for the separator classtype.
+    switch -- [llength $::ms::style($::ms::current($w,style),separator,addrs)] {
+        0   {
+            set index [lsearch -exact $::ms::style(separator,classtype) $::ms::current($w,style)]
+            switch -- $index {
+                -1      {}
+                default { set ::ms::style(separator,classtype) [lremove $::ms::style(separator,classtype) $index] }
+            }
+        }
+    }
+
+    # Destroy every widget's variables previously created.
+    unset -nocomplain -- ::ms::addr($short_addr,real) \
+                         ::ms::addr($w,short);
+
+    unset -nocomplain -- ::ms::addr($w,border) \
+                         ::ms::addr($w,structure) \
+                         ::ms::addr($w,toplevel) \
+                         ::ms::addr($w,widget);
+
+    unset -nocomplain -- ::ms::current($w,background) \
+                         ::ms::current($w,class) \
+                         ::ms::current($w,cursor) \
+                         ::ms::current($w,orient) \
+                         ::ms::current($w,state) \
+                         ::ms::current($w,style) \
+                         ::ms::current($w,takefocus);
+
+    unset -nocomplain -- ::ms::data($w,classtype) \
+                         ::ms::data($w,token);
+
+    unset -nocomplain -- ::ms::default($w,background) \
+                         ::ms::default($w,class) \
+                         ::ms::default($w,cursor) \
+                         ::ms::default($w,orient) \
+                         ::ms::default($w,state) \
+                         ::ms::default($w,style) \
+                         ::ms::default($w,takefocus);
+
+    unset -nocomplain -- ::ms::managed_by($w,background) \
+                         ::ms::managed_by($w,cursor);
+
+    unset -nocomplain -- ::ms::style($w,widget)
+
+    return ""
+}
+
 #*EOF*
