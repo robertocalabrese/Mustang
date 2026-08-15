91f01c143bcd22052d31d2ec5eb6d3c5c8e32414
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-15 09:31:34 +0200

    scale: if the parent style layout is not known by mustang, set it as the current theme layout orientation

--- widgets/scale.tcl
+++ widgets/scale.tcl
@@ -1204,6 +1204,11 @@ proc ::ms::scale::Pathname_Cmd { w cmd args } {
                                     }
                                 }
                             }
+
+                            # If the parent style layout is not known by mustang, set it as the current theme layout orientation.
+                            if { $parent_style ni $::ms::layouts($::ms::theme) } {
+                                _ttk_style layout $parent_style [_ttk_style layout [string cat $orient "." TScale]]
+                            }
                         }
                         default { ::ms::Error "Invalid number of arguments." $caller_info }
                     }
