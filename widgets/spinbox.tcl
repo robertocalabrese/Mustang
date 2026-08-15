cf5a34c65d09e80b199d3bfec99384344e1eb98e
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-15 16:14:55 +0200

    spinbox: add the 'Check_TextVariable' procedure

--- widgets/spinbox.tcl
+++ widgets/spinbox.tcl
@@ -4371,4 +4371,27 @@ proc ::ms::spinbox::Return { w } {
     return ""
 }
 
+## Check_TextVariable
+#
+# Validate the textvariable value, everytime it changes.
+#
+# Where:
+#
+# w        Should be the widget real address involved.
+#
+# name1,
+# name2,
+# op       Should be the tracing arguments.
+#          Do not pass any value, the **trace** command will automatically pass these values.
+#
+# It doesn't return anything.
+proc ::ms::spinbox::Check_TextVariable { w name1 name2 op } {
+    switch -- $::ms::current($w,datatype) {
+        none    {}
+        default { ::ms::spinbox::Return $w }
+    }
+
+    return ""
+}
+
 #*EOF*
