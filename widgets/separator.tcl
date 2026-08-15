e0680554040d8d35adf2c3adc9466a5ee84d090e
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-15 10:14:57 +0200

    separator: set the default value for each styleable option and if the option is managed by Tk, set also its current value

--- widgets/separator.tcl
+++ widgets/separator.tcl
@@ -316,6 +316,20 @@ proc ::ms::separator::Command { window { args "" } } {
                     }
                 }
             }
+
+            # Set the default value for each styleable option and if the option is managed by Tk, set also its current value.
+            foreach option $::ms::separator(styleable,options) {
+                set ::ms::default($w,$option) $::ms::styleopt($::ms::theme,TSeparator,$option)
+
+                switch -- $::ms::managed_by($w,$option) {
+                    Tk  {
+                        switch -- [info exists ::ms::styleopt($::ms::theme,$::ms::current($w,style),$option)] {
+                            0   { set ::ms::current($w,$option) $::ms::default($w,$option) }
+                            1   { set ::ms::current($w,$option) $::ms::styleopt($::ms::theme,$::ms::current($w,style),$option) }
+                        }
+                    }
+                }
+            }
         }
         default { ::ms::Error "Invalid number of arguments." $caller_info }
     }
