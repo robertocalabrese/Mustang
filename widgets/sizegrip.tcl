5da86435293a2e1d9d05a22e0d67beedc8ddfd4a
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-15 15:19:29 +0200

    sizegrip: set the default value for each styleable option and if the option is managed by Tk, set also its current value

--- widgets/sizegrip.tcl
+++ widgets/sizegrip.tcl
@@ -814,7 +814,21 @@ proc ::ms::sizegrip::Pathname_Cmd { w cmd args } {
 # It doesn't return anything.
 proc ::ms::sizegrip::Style_Update { stylename caller_info } {
     # Update all the sizegrip widgets addresses that have stylename as a style.
-    foreach w $::ms::style($stylename,sizegrip,addrs) {}
+    foreach w $::ms::style($stylename,sizegrip,addrs) {
+        # Set the default value for each styleable option and if the option is managed by Tk, set also its current value.
+        foreach option $::ms::sizegrip(styleable,options) {
+            set ::ms::default($w,$option) $::ms::styleopt($::ms::theme,TEntry,$option)
+
+            switch -- $::ms::managed_by($w,$option) {
+                Tk  {
+                    switch -- [info exists ::ms::styleopt($::ms::theme,$stylename,$option)] {
+                        0   { set ::ms::current($w,$option) $::ms::default($w,$option) }
+                        1   { set ::ms::current($w,$option) $::ms::styleopt($::ms::theme,$stylename,$option) }
+                    }
+                }
+            }
+        }
+    }
 
     return ""
 }
