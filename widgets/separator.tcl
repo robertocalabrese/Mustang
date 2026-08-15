e8bc55ebb50b5d4b6d2cce7b25179577dc656e86
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-15 10:15:26 +0200

    separator: if the parent style layout is not known by mustang, set it as the current theme layout orientation

--- widgets/separator.tcl
+++ widgets/separator.tcl
@@ -353,6 +353,11 @@ proc ::ms::separator::Command { window { args "" } } {
                     }
                 }
             }
+
+            # If the parent style layout is not known by mustang, set it as the current theme layout orientation.
+            if { $parent_style ni $::ms::layouts($::ms::theme) } {
+                _ttk_style layout $parent_style [_ttk_style layout [string cat $orient "." TSeparator]]
+            }
         }
         default { ::ms::Error "Invalid number of arguments." $caller_info }
     }
