4399be1adc8c4d73a804c69ee94965d60fde9095
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-15 09:29:08 +0200

    scale: add the 'style' command

--- widgets/scale.tcl
+++ widgets/scale.tcl
@@ -1027,7 +1027,20 @@ proc ::ms::scale::Pathname_Cmd { w cmd args } {
                 default { ::ms::Error "Invalid number of arguments." $caller_info }
             }
         }
-        style {}
+        style {
+            # Synopsis:
+            #
+            # *window* **style**
+            switch -- [llength $args] {
+                0   {
+                    switch -- [string last "." $::ms::current($w,style)] {
+                        -1      { return [string cat [string totitle $::ms::current($w,orient)] "." $::ms::current($w,style)] }
+                        default { return $::ms::current($w,style) }
+                    }
+                }
+                default { ::ms::Error "Invalid number of arguments." $caller_info }
+            }
+        }
         default { ::ms::Error "Invalid option, '$cmd'." $caller_info }
     }
 }
