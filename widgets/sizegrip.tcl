b9b66134aa121d7c2fde90e6efab6ec68ce76d04
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-15 15:10:02 +0200

    sizegrip: add the '_Sizegrip' bindings

--- widgets/sizegrip.tcl
+++ widgets/sizegrip.tcl
@@ -63,6 +63,81 @@
 #   [text](https:\\...)  --> Link to an internet page.
 #   [text](/wiki/...)    --> Link to another file in the wiki.
 package provide ::ms::sizegrip 0.1
+################################
+##                            ##
+##     _SIZEGRIP BINDINGS     ##
+##                            ##
+################################
+
+# Activate/Deactivate
+_bind _Sizegrip <Activate>   { ::ms::sizegrip::Pathname_Cmd %W state !background; break }
+_bind _Sizegrip <Deactivate> { ::ms::sizegrip::Pathname_Cmd %W state  background; break }
+
+# Buttonpress-1
+_bind _Sizegrip <ButtonPress-1>   { ::ms::sizegrip::ButtonPress %W %X %Y; break }
+_bind _Sizegrip <B1-Motion>       { ::ms::sizegrip::Drag        %W %X %Y; break }
+_bind _Sizegrip <ButtonRelease-1> { ::ms::sizegrip::Drag_Release; break }
+
+# Contextual menu
+_bind _Sizegrip <<ContextMenu>> { ::ms::Show_ContextMenu %W %X %Y cmenu; break }
+
+# Destroy
+_bind _Sizegrip <Destroy> { ::ms::sizegrip::Destroy %W; break }
+
+# Enter/Leave
+_bind _Sizegrip <Enter> { ::ms::sizegrip::Pathname_Cmd %W state  hover; break }
+_bind _Sizegrip <Leave> { ::ms::sizegrip::Pathname_Cmd %W state !hover; break }
+
+# FocusIn/FocusOut
+_bind _Sizegrip <FocusIn>  { ::ms::sizegrip::FocusIn  %W; break }
+_bind _Sizegrip <FocusOut> { ::ms::sizegrip::FocusOut %W; break }
+
+# Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+# and move that scrollbar by one unit up or down (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Sizegrip <MouseWheel> { ::ms::Scroll_Parent_Y %W %D units; break }
+
+# Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+# and move that scrollbar by one unit left or right (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Sizegrip <Shift-MouseWheel> { ::ms::Scroll_Parent_X %W %D units; break }
+
+# Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+# and move that scrollbar by one page up or down (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Sizegrip <Control-MouseWheel> { ::ms::Scroll_Parent_Y %W %D pages; break }
+
+# Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+# and move that scrollbar by one page left or right (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Sizegrip <Control-Shift-MouseWheel> { ::ms::Scroll_Parent_X %W %D pages; break }
+
+# Note: **TouchpadScroll** and **Control-TouchpadScroll** only works on Windows and macOS.
+#       On Linux they will be ignored and touchpads movements will be processed as mousewheel events.
+
+# This binding movement will happen on two different planes, horizontal (1) and vertical (2).
+# These two planes may involve different widgets depending on the active scrollbars on them and on the
+# touchpad direction.
+#   1 - Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+#       and move that scrollbar by one unit left or right (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the horizontal axis.
+#
+#   2 - Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+#       and move that scrollbar by one unit up or down (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the vertical axis.
+_bind _Sizegrip <TouchpadScroll> { ::ms::Touchpad_Parent %W %# %D units; break }
+
+# This binding movement will happen on two different planes, horizontal (1) and vertical (2).
+# These two planes may involve different widgets depending on the active scrollbars on them and on the
+# touchpad direction.
+#   1 - Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+#       and move that scrollbar by one page left or right (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the horizontal axis.
+#
+#   2 - Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+#       and move that scrollbar by one page up or down (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the vertical axis.
+_bind _Sizegrip <Control-TouchpadScroll> { ::ms::Touchpad_Parent %W %# %D pages; break }
 
 # Create the mustang **sizegrip** package.
 namespace eval ::ms::sizegrip {}
