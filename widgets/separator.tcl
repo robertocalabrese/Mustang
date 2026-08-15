a32d9f61af56535ab5112f609dd08af31a4d6278
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-15 10:12:03 +0200

    separator: add the '_Separator' bindings

--- widgets/separator.tcl
+++ widgets/separator.tcl
@@ -64,6 +64,80 @@
 #   [text](/wiki/...)    --> Link to another file in the wiki.
 package provide ::ms::separator 0.1
 
+#################################
+##                             ##
+##     _SEPARATOR BINDINGS     ##
+##                             ##
+#################################
+
+# Activate/Deactivate
+_bind _Separator <Activate>   { ::ms::separator::Pathname_Cmd %W state !background; break }
+_bind _Separator <Deactivate> { ::ms::separator::Pathname_Cmd %W state  background; break }
+
+# Buttonpress-1
+_bind _Separator <ButtonPress-1> { ::ms::Focus_The_Widget_Or_Its_Toplevel %W; break }
+
+# Contextual menu
+_bind _Separator <<ContextMenu>> { ::ms::Show_ContextMenu %W %X %Y cmenu; break }
+
+# Destroy
+_bind _Separator <Destroy> { ::ms::separator::Destroy %W; break }
+
+# Enter/Leave
+_bind _Separator <Enter> { ::ms::separator::Pathname_Cmd %W state  hover; break }
+_bind _Separator <Leave> { ::ms::separator::Pathname_Cmd %W state !hover; break }
+
+# FocusIn/FocusOut
+_bind _Separator <FocusIn>  { ::ms::separator::FocusIn  %W; break }
+_bind _Separator <FocusOut> { ::ms::separator::FocusOut %W; break }
+
+# Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+# and move that scrollbar by one unit up or down (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Separator <MouseWheel> { ::ms::Scroll_Parent_Y %W %D units; break }
+
+# Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+# and move that scrollbar by one unit left or right (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Separator <Shift-MouseWheel> { ::ms::Scroll_Parent_X %W %D units; break }
+
+# Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+# and move that scrollbar by one page up or down (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Separator <Control-MouseWheel> { ::ms::Scroll_Parent_Y %W %D pages; break }
+
+# Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+# and move that scrollbar by one page left or right (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Separator <Control-Shift-MouseWheel> { ::ms::Scroll_Parent_X %W %D pages; break }
+
+# Note: **TouchpadScroll** and **Control-TouchpadScroll** only works on Windows and macOS.
+#       On Linux they will be ignored and touchpads movements will be processed as mousewheel events.
+
+# This binding movement will happen on two different planes, horizontal (1) and vertical (2).
+# These two planes may involve different widgets depending on the active scrollbars on them and on the
+# touchpad direction.
+#   1 - Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+#       and move that scrollbar by one unit left or right (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the horizontal axis.
+#
+#   2 - Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+#       and move that scrollbar by one unit up or down (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the vertical axis.
+_bind _Separator <TouchpadScroll> { ::ms::Touchpad_Parent %W %# %D units; break }
+
+# This binding movement will happen on two different planes, horizontal (1) and vertical (2).
+# These two planes may involve different widgets depending on the active scrollbars on them and on the
+# touchpad direction.
+#   1 - Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+#       and move that scrollbar by one page left or right (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the horizontal axis.
+#
+#   2 - Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+#       and move that scrollbar by one page up or down (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the vertical axis.
+_bind _Separator <Control-TouchpadScroll> { ::ms::Touchpad_Parent %W %# %D pages; break }
+
 # Create the mustang **separator** package.
 namespace eval ::ms::separator {}
 
