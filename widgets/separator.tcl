8582685ccc496700c1577e816f63f2df8316bb20
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-15 10:22:41 +0200

    separator: configure the widget

--- widgets/separator.tcl
+++ widgets/separator.tcl
@@ -704,6 +704,62 @@ proc ::ms::separator::Pathname_Cmd { w cmd args } {
                             if { $parent_style ni $::ms::layouts($::ms::theme) } {
                                 _ttk_style layout $parent_style [_ttk_style layout [string cat $orient "." TSeparator]]
                             }
+
+                            ##################################
+                            ##                              ##
+                            ##     CONFIGURE THE WIDGET     ##
+                            ##                              ##
+                            ##################################
+
+                            # Note: 'cursor' is not allowed to change if the statespec changes.
+
+                            #######################
+                            ##                   ##
+                            ##     SEPARATOR     ##
+                            ##                   ##
+                            #######################
+
+                            # Set the widget style name.
+                            set ::ms::style($w,widget) [string cat "_bg=" $::ms::current($w,background) \
+                                                                   "." $parent_style];
+
+                            # If needed, create the widget style name.
+                            if { $::ms::style($w,widget) ni $::ms::style($::ms::theme,created_by_mustang) } {
+                                _ttk_style configure $::ms::style($w,widget) -background $::ms::current($w,background)
+
+                                # Add the widget style name to the theme styles list created by mustang.
+                                lappend ::ms::style($::ms::theme,created_by_mustang) $::ms::style($w,widget)
+                            }
+
+                            # Initialize the widget mapping.
+                            set mapping [list ]
+
+                            # background
+                            switch -- $::ms::managed_by($w,background) {
+                                developer { lappend mapping -background [list pressed $::ms::current($w,background)] }
+                                Tk  {
+                                    # Check if a 'background' mapping exists for 'stylename'.
+                                    switch -- [info exists ::ms::stylemap($::ms::theme,$stylename,background)] {
+                                        1   { lappend mapping -background $::ms::stylemap($::ms::theme,$stylename,background) }
+                                    }
+                                }
+                            }
+
+                            # If needed, create the widget mapping.
+                            if { $mapping ni $::ms::stylemap($::ms::theme,created_by_mustang) } {
+                                _ttk_style map $::ms::style($w,widget) {*}$mapping
+
+                                # Add the widget mapping to the stylemap list containing all the mappings
+                                # created by mustang for the current theme.
+                                lappend ::ms::stylemap($::ms::theme,created_by_mustang) $mapping
+                            }
+
+                            # Apply the changes.
+                            interp invokehidden {} $w configure    -cursor $::ms::current($w,cursor) \
+                                                                    -style $::ms::style($w,widget) \
+                                                                -takefocus $::ms::current($w,takefocus);
+
+                            return ""
                         }
                         default { ::ms::Error "Invalid number of arguments." $caller_info }
                     }
