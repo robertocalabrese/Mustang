8ea0411350d22d1eb800a4fbacfe9dca279df436
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-19 06:48:36 +0200

    text: set the remaining variables and lists that needs to be set

--- widgets/text.tcl
+++ widgets/text.tcl
@@ -1898,6 +1898,40 @@ proc ::ms::text::Command { window { args "" } } {
                     lappend ::ms::addr(megawidgets,scrollable) $w
                 }
             }
+
+            #####################
+            ##                 ##
+            ##     CLOSING     ##
+            ##                 ##
+            #####################
+
+            # Hide the widget pathcommand.
+            interp hide {} $w
+
+            # Create an alias for the widget pathcommand.
+            lappend ::ms::data($w,token) [interp alias {} $w {} ::ms::text::Pathname_Cmd $w]
+
+            # If needed, create an alias for the widget short address pathcommand.
+            if { $short_addr ne $w } {
+                lappend ::ms::data($w,token) [interp alias {} $short_addr {} ::ms::text::Pathname_Cmd $w]
+            }
+
+            # Add the widget address to the text classtype real address list with class '::ms::current($w,class)'.
+            lappend ::ms::class($::ms::current($w,class),text,addrs) $w
+
+            # Add the widget address to the text classtype real address list with style '::ms::current($w,style)'.
+            lappend ::ms::style($::ms::current($w,style),text,addrs) $w
+
+            # If needed, add '::ms::current($w,style)' to the available styles for the text classtype.
+            if { $::ms::current($w,style) ni $::ms::style(text,classtype) } {
+                lappend ::ms::style(text,classtype) $::ms::current($w,style)
+            }
+
+            # Depending on the address type provided, return the widget real or short address.
+            switch -- $type {
+                real  { return $w }
+                short { return $short_addr }
+            }
         }
         default { ::ms::Error "Invalid number of arguments." $caller_info }
     }
