1234881d3ccc65afed5f89c3e3cd12198edb4d97
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-19 09:47:02 +0200

    toolbutton: add the '_Toolbutton' bindings

--- widgets/toolbutton.tcl
+++ widgets/toolbutton.tcl
@@ -64,6 +64,90 @@
 #   [text](/wiki/...)    --> Link to another file in the wiki.
 package provide ::ms::toolbutton 0.1
 
+##################################
+##                              ##
+##     _TOOLBUTTON BINDINGS     ##
+##                              ##
+##################################
+
+# Activate/Deactivate
+_bind _Toolbutton <Activate>   { ::ms::toolbutton::Pathname_Cmd %W state !background; break }
+_bind _Toolbutton <Deactivate> { ::ms::toolbutton::Pathname_Cmd %W state  background; break }
+
+# ButtonPress-1
+_bind _Toolbutton <ButtonPress-1>   { ::ms::toolbutton::ButtonPress %W; break }
+_bind _Toolbutton <Button1-Leave>   { break }
+_bind _Toolbutton <Button1-Enter>   { break }
+_bind _Toolbutton <ButtonRelease-1> { ::ms::toolbutton::Invoke %W; break }
+
+# Contextual menu
+_bind _Toolbutton <<ContextMenu>> { ::ms::Show_ContextMenu %W %X %Y cmenu; break }
+
+# Destroy
+_bind _Toolbutton <Destroy> { ::ms::toolbutton::Destroy %W; break }
+
+# Enter/Leave
+_bind _Toolbutton <Enter> { ::ms::toolbutton::Pathname_Cmd %W state  hover; break }
+_bind _Toolbutton <Leave> { ::ms::toolbutton::Pathname_Cmd %W state !hover; break }
+
+# FocusIn/FocusOut
+_bind _Toolbutton <FocusIn>  { ::ms::toolbutton::FocusIn  %W; break }
+_bind _Toolbutton <FocusOut> { ::ms::toolbutton::FocusOut %W; break }
+
+# Return/KP_Enter/space
+_bind _Toolbutton <Return>   { ::ms::toolbutton::Invoke %W; break }
+_bind _Toolbutton <KP_Enter> { ::ms::toolbutton::Invoke %W; break }
+_bind _Toolbutton <space>    { ::ms::toolbutton::Invoke %W; break }
+
+# Mousewheel and Touchpad
+
+# Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+# and move that scrollbar by one unit up or down (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Toolbutton <MouseWheel> { ::ms::Scroll_Parent_Y %W %D units; break }
+
+# Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+# and move that scrollbar by one unit left or right (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Toolbutton <Shift-MouseWheel> { ::ms::Scroll_Parent_X %W %D units; break }
+
+# Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+# and move that scrollbar by one page up or down (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Toolbutton <Control-MouseWheel> { ::ms::Scroll_Parent_Y %W %D pages; break }
+
+# Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+# and move that scrollbar by one page left or right (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _Toolbutton <Control-Shift-MouseWheel> { ::ms::Scroll_Parent_X %W %D pages; break }
+
+# Note: **TouchpadScroll** and **Control-TouchpadScroll** only works on Windows and macOS.
+#       On Linux they will be ignored and touchpads movements will be processed as mousewheel events.
+
+# This binding movement will happen on two different planes, horizontal (1) and vertical (2).
+# These two planes may involve different widgets depending on the active scrollbars on them and on the
+# touchpad direction.
+#   1 - Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+#       and move that scrollbar by one unit left or right (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the horizontal axis.
+#
+#   2 - Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+#       and move that scrollbar by one unit up or down (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the vertical axis.
+_bind _Toolbutton <TouchpadScroll> { ::ms::Touchpad_Parent %W %# %D units; break }
+
+# This binding movement will happen on two different planes, horizontal (1) and vertical (2).
+# These two planes may involve different widgets depending on the active scrollbars on them and on the
+# touchpad direction.
+#   1 - Try to find the innermost widget's scrollable parent with an active horizontal scrollbar
+#       and move that scrollbar by one page left or right (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the horizontal axis.
+#
+#   2 - Try to find the innermost widget's scrollable parent with an active vertical scrollbar
+#       and move that scrollbar by one page up or down (depending on the touchpad direction).
+#       If none of the widget's parent meets the required condition, don't do anything on the vertical axis.
+_bind _Toolbutton <Control-TouchpadScroll> { ::ms::Touchpad_Parent %W %# %D pages; break }
+
 # Create the mustang **toolbutton** package.
 namespace eval ::ms::toolbutton {}
 
