fa8d6312ffada7e22c793d828411951d72806a8b
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-19 10:19:19 +0200

    toolbutton: enable/disable the widget automatic translation

--- widgets/toolbutton.tcl
+++ widgets/toolbutton.tcl
@@ -1309,6 +1309,19 @@ proc ::ms::toolbutton::Pathname_Cmd { w cmd args } {
                                     set takefocus $::ms::current($w,takefocus)
                                 }
                             }
+
+                            # Enable/Disable the widget automatic translation.
+                            switch -- $::ms::current($w,textvariable) {
+                                ""  {
+                                    # Automatic translation: ON
+                                    set ::ms::data($w,translated_text) [::msgcat::mc {*}$::ms::current($w,text)]
+                                    set text_variable ::ms::data($w,translated_text)
+                                }
+                                default {
+                                    # Automatic translation: OFF
+                                    set text_variable $::ms::current($w,textvariable)
+                                }
+                            }
                         }
                         default { ::ms::Error "Invalid number of arguments." $caller_info }
                     }
