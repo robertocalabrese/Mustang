c984f370cbcea96f121f76d48db23cf1cc4f7d81
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-19 16:19:37 +0200

    toplevel: set the internal '-padding' option to always show the horizontal and vertical padding

--- widgets/toplevel.tcl
+++ widgets/toplevel.tcl
@@ -570,6 +570,12 @@ proc ::ms::toplevel::Command { window { args "" } } {
                     }
                 }
             }
+
+            # Set the internal '-padding' option to always show the horizontal and vertical padding.
+            switch -- [llength $::ms::current($w,padding)] {
+                1       { set ::ms::data($w,padding) [list $::ms::current($w,padding) $::ms::current($w,padding)] }
+                default { set ::ms::data($w,padding) $::ms::current($w,padding) }
+            }
         }
         default { ::ms::Error "Invalid number of arguments." $caller_info }
     }
