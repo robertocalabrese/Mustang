d455d06744ba0f71f4fa2a65db55999d5cf70473
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-19 06:36:21 +0200

    text: add the '_V_Scrollbar_Text' bindings

--- widgets/text.tcl
+++ widgets/text.tcl
@@ -714,6 +714,64 @@ _bind _H_Scrollbar_Text <TouchpadScroll> { ::ms::Touchpad_Widget_X [_winfo paren
 # If none of the widget's parent meets the required condition, don't do anything on the horizontal axis.
 _bind _H_Scrollbar_Text <Control-TouchpadScroll> { ::ms::Touchpad_Widget_X [_winfo parent %W] %# %D pages; break }
 
+########################################
+##                                    ##
+##     _V_SCROLLBAR_TEXT BINDINGS     ##
+##                                    ##
+########################################
+
+# ButtonPress-1
+_bind _V_Scrollbar_Text <ButtonPress-1>   { ::ms::text::Scrollbar_ButtonPress [_winfo parent %W] vertical %x %y; break }
+_bind _V_Scrollbar_Text <B1-Motion>       { ::ms::text::Scrollbar_Drag        [_winfo parent %W] vertical %x %y; break }
+_bind _V_Scrollbar_Text <ButtonRelease-1> { ::ms::text::Scrollbar_ButtonRelease; break }
+
+# Enter/Leave
+_bind _V_Scrollbar_Text <Enter> { ::ms::text::Hover [_winfo parent %W] %X %Y; break }
+_bind _V_Scrollbar_Text <Leave> { ::ms::text::Hover [_winfo parent %W] %X %Y; break }
+
+# Mousewheel and Touchpad
+
+# If the widget's vertical scrollbar is active, move the text object by one unit up or down
+# (depending on the mousewheel direction).
+# Otherwise, try to find the innermost widget's scrollable parent with an active vertical scrollbar
+# and move that scrollbar by one unit up or down (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _V_Scrollbar_Text <MouseWheel> { ::ms::Scroll_Widget_Y [_winfo parent %W] %D units; break }
+
+# If the widget's vertical scrollbar is active, move the text object by one page up or down
+# (depending on the mousewheel direction).
+# Otherwise, try to find the innermost widget's scrollable parent with an active vertical scrollbar
+# and move that scrollbar by one page up or down (depending on the mousewheel direction).
+# If none of the widget's parent meets the required condition, don't do anything.
+_bind _V_Scrollbar_Text <Control-MouseWheel> { ::ms::Scroll_Widget_Y [_winfo parent %W] %D pages; break }
+
+# Note: **TouchpadScroll** and **Control-TouchpadScroll** only works on Windows and macOS.
+#       On Linux they will be ignored and touchpads movements will be processed as mousewheel events.
+
+# This binding movement will happen on two different planes, horizontal and vertical.
+# These two planes may involve different widgets depending on the active scrollbars on them and on the
+# touchpad direction.
+# In this case, only the vertical plane is involved.
+#
+# If the widget's vertical scrollbar is active, move the text object by one unit up or down
+# (depending on the touchpad direction).
+# Otherwise, try to find the innermost widget's scrollable parent with an active vertical scrollbar
+# and move that scrollbar by one unit up or down (depending on the touchpad direction).
+# If none of the widget's parent meets the required condition, don't do anything on the vertical axis.
+_bind _V_Scrollbar_Text <TouchpadScroll> { ::ms::Touchpad_Widget_Y [_winfo parent %W] %# %D units; break }
+
+# This binding movement will happen on two different planes, horizontal and vertical.
+# These two planes may involve different widgets depending on the active scrollbars on them and on the
+# touchpad direction.
+# In this case, only the vertical plane is involved.
+#
+# If the widget's vertical scrollbar is active, move the text object by one page up or down
+# (depending on the touchpad direction).
+# Otherwise, try to find the innermost widget's scrollable parent with an active vertical scrollbar
+# and move that scrollbar by one page up or down (depending on the touchpad direction).
+# If none of the widget's parent meets the required condition, don't do anything on the vertical axis.
+_bind _V_Scrollbar_Text <Control-TouchpadScroll> { ::ms::Touchpad_Widget_Y [_winfo parent %W] %# %D pages; break }
+
 # Create the mustang **text** package.
 namespace eval ::ms::text {}
 
