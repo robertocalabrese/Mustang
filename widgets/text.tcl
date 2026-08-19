51e028beebc3be3f190f5d2a8b4c4bc1372aca8c
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-19 07:01:01 +0200

    text: update the widget and remove any temporary variables that was created

--- widgets/text.tcl
+++ widgets/text.tcl
@@ -2805,6 +2805,18 @@ proc ::ms::text::Pathname_Cmd { w cmd args } {
                                     ::ms::text::Scrollbar_Update $w
                                 }
                             }
+
+                            # Note: If width and/or height were provided by the developer, and these values were accepted by Tk,
+                            #       then a configure event will be invoked by Tk ('::ms::text::Configure').
+                            #       After this event, their values will be registered.
+
+                            update
+
+                            # Cleaning.
+                            unset -nocomplain -- ::ms::temp($w,height) \
+                                                 ::ms::temp($w,width);
+
+                            return ""
                         }
                         default { ::ms::Error "Invalid number of arguments." $caller_info }
                     }
