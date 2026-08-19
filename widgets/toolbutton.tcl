f7c05e3fb5f97937a1eb64035b2a7e17232fc5f7
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-19 10:20:46 +0200

    toolbutton: update all the toolbutton widgets that have stylename as a style

--- widgets/toolbutton.tcl
+++ widgets/toolbutton.tcl
@@ -1665,7 +1665,12 @@ proc ::ms::toolbutton::Pathname_Cmd { w cmd args } {
 # caller_info   Should be the information on the developer command that generated the call to this procedure.
 #
 # It doesn't return anything.
-proc ::ms::toolbutton::Style_Update { stylename caller_info } {}
+proc ::ms::toolbutton::Style_Update { stylename caller_info } {
+    # Update all the toolbutton widgets addresses that have stylename as a style.
+    foreach w $::ms::style($stylename,toolbutton,addrs) {}
+
+    return ""
+}
 
 ######################################
 ##                                  ##
