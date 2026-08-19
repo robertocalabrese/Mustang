d590c9a379ff3b414561f03662b8b93950cd0c76
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-19 10:16:49 +0200

    toolbutton: add the initial draft of the 'configure' command

--- widgets/toolbutton.tcl
+++ widgets/toolbutton.tcl
@@ -925,7 +925,38 @@ proc ::ms::toolbutton::Pathname_Cmd { w cmd args } {
                 default { ::ms::Error "Invalid option, '$args'." $caller_info }
             }
         }
-        configure {}
+        configure {
+            # Synopsis:
+            #
+            # *window* **configure**
+            # *window* **configure** *option*
+            # *window* **configure** *option* *value*
+            # *window* **configure** *option* *value* ... ?*option* *value*?
+            switch -- [llength $args] {
+                0   {
+                    # 'non-styleable' options.
+                    foreach option $::ms::toolbutton(non_styleable,options) {
+                        lappend result [list $option $::ms::default($w,$option) $::ms::current($w,$option)]
+                    }
+
+                    # 'styleable' options.
+                    foreach option $::ms::toolbutton(styleable,options) {
+                        lappend result [list $option $::ms::default($w,$option) $::ms::current($w,$option)]
+                    }
+
+                    return [lsort -dictionary -increasing -index 0 $result]
+                }
+                1   {
+                    set option [string range $args 1 end]
+                    if { ($option in $::ms::toolbutton(non_styleable,options)) || ($option in $::ms::toolbutton(styleable,options)) } {
+                        return [list $::ms::default($w,$option) $::ms::current($w,$option)]
+                    } else {
+                        ::ms::Error "Invalid configure option, '$args'." $caller_info
+                    }
+                }
+                default {}
+            }
+        }
         identify {
             # Synopsis:
             #
