725429d3217675f0c7ebf06d126fb15a5c97a493
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-19 06:57:53 +0200

    text: set the internal '-padding' option to always show the horizontal and vertical padding

--- widgets/text.tcl
+++ widgets/text.tcl
@@ -2591,6 +2591,12 @@ proc ::ms::text::Pathname_Cmd { w cmd args } {
                                     set takefocus $::ms::current($w,takefocus)
                                 }
                             }
+
+                            # Set the internal '-padding' option to always show the horizontal and vertical padding.
+                            switch -- [llength $::ms::current($w,padding)] {
+                                1       { set ::ms::data($w,padding) [list $::ms::current($w,padding) $::ms::current($w,padding)] }
+                                default { set ::ms::data($w,padding) $::ms::current($w,padding) }
+                            }
                         }
                         default { ::ms::Error "Invalid number of arguments." $caller_info }
                     }
