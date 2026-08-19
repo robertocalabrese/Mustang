dbe59314f05721028570a6140c52fd378f49e514
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-19 10:21:47 +0200

    toolbutton: check the widget state and set the cursor accordingly

--- widgets/toolbutton.tcl
+++ widgets/toolbutton.tcl
@@ -1681,6 +1681,12 @@ proc ::ms::toolbutton::Style_Update { stylename caller_info } {
                 }
             }
         }
+
+        # Check the widget state and set the cursor accordingly.
+        switch -- $::ms::current($w,state) {
+            disabled { set cursor arrow }
+            normal   { set cursor $::ms::current($w,cursor) }
+        }
     }
 
     return ""
