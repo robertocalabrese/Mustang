5e298c7181f49ef4bdf4850850ca4be07a07af3c
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-21 18:05:05 +0200

    treeview: add the 'FocusOut' procedure

--- widgets/treeview.tcl
+++ widgets/treeview.tcl
@@ -4286,4 +4286,44 @@ proc ::ms::treeview::FocusIn { w } {
     return ""
 }
 
+## FocusOut
+#
+# Manage the **FocusOut** event on the widget.
+#
+# Where:
+#
+# w   Should be the widget real address involved.
+#
+# It doesn't return anything.
+proc ::ms::treeview::FocusOut { w } {
+    # Check the contextual menu associated with this widget, if any.
+    set cmenu $::ms::current($w,cmenu)
+    switch -- $cmenu {
+        ""  {
+            # Check if a contextual menu was associated with the widget's toplevel.
+            set cmenu $::ms::current($::ms::addr($w,toplevel),cmenu)
+            switch -- $cmenu {
+                ""      {}
+                default {
+                    # If the contextual menu of the widget's toplevel is open do not loose the focus (graphically).
+                    switch -- [_winfo exists $cmenu] {
+                        1   { return "" }
+                    }
+                }
+            }
+        }
+        default {
+            # If the contextual menu of the widget is open do not loose the focus (graphically).
+            switch -- [_winfo exists $cmenu] {
+                1   { return "" }
+            }
+        }
+    }
+
+    # Change the widget dynamic state to '!focus'.
+    ::ms::treeview::Pathname_Cmd $w state !focus
+
+    return ""
+}
+
 #*EOF*
