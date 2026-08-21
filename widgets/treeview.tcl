ed183483fbd31260437a2555003c93642a255799
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-21 17:38:10 +0200

    treeview: add the 'instate' command

--- widgets/treeview.tcl
+++ widgets/treeview.tcl
@@ -2132,7 +2132,67 @@ proc ::ms::treeview::Pathname_Cmd { w cmd args } {
                 default { ::ms::Error "Invalid number of arguments." $caller_info }
             }
         }
-        instate {}
+        instate {
+            # Synopsis:
+            #
+            # *window* **instate** *statespec* ?*script*?
+
+            # Check if the widget is scrollable or not.
+            switch -- $::ms::current($w,scrollable) {
+                false { set address [list interp invokehidden {} $w] }
+                true  { set address [list $w.treeview] }
+            }
+
+            switch -- [llength $args] {
+                0   { ::ms::Error "Missing statespec." $caller_info }
+                1   {
+                    set statespec $args
+
+                    # Check the 'statespec' provided.
+                    switch -- $statespec {
+                        ""      -
+                        normal  { set statespec $::ms::data(statespec,normal) }
+                        default {
+                            foreach state $statespec {
+                                switch -- [::ms::Check_State $state] {
+                                    invalid { ::ms::Error "Invalid statespec, '$state'." $caller_info }
+                                }
+                            }
+                        }
+                    }
+
+                    # Execute the command.
+                    return [{*}$address instate $statespec]
+                }
+                2   {
+                    set statespec [lindex $args 0]
+                    set script    [lindex $args 1]
+
+                    # Check the 'statespec' provided.
+                    switch -- $statespec {
+                        ""      -
+                        normal  { set statespec $::ms::data(statespec,normal) }
+                        default {
+                            foreach state $statespec {
+                                switch -- [::ms::Check_State $state] {
+                                    invalid { ::ms::Error "Invalid statespec, '$state'." $caller_info }
+                                }
+                            }
+                        }
+                    }
+
+                    # Execute the command.
+                    try {
+                        {*}$address instate $statespec $script
+                    } on error {} {
+                        ::ms::Error "Invalid script." $caller_info
+                    } on ok { result } {
+                        return $result
+                    }
+                }
+                default { ::ms::Error "Invalid number of arguments." $caller_info }
+            }
+        }
         selection {}
         state {}
         style {}
