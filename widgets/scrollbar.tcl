5023e0d50edb18e6821ffb91ec2d8222009c000e
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-24 12:08:29 +0200

    scrollbar: add the initial draft of the 'configure' command

--- widgets/scrollbar.tcl
+++ widgets/scrollbar.tcl
@@ -758,7 +758,38 @@ proc ::ms::scrollbar::Pathname_Cmd { w cmd args } {
                 default { ::ms::Error "Invalid number of arguments." $caller_info }
             }
         }
-        configure {}
+        configure {
+            # Synopsis:
+            #
+            # *window* **configure**
+            # *window* **configure** *option*
+            # *window* **configure** *option* *value*
+            # *window* **configure** *option* *value* ... ?*option* *value*?
+            switch -- [llength $args] {
+                0   {
+                    # 'non-styleable' options.
+                    foreach option $::ms::scrollbar(non_styleable,options) {
+                        lappend result [list $option $::ms::default($w,$option) $::ms::current($w,$option)]
+                    }
+
+                    # 'styleable' options.
+                    foreach option $::ms::scrollbar(styleable,options) {
+                        lappend result [list $option $::ms::default($w,$option) $::ms::current($w,$option)]
+                    }
+
+                    return [lsort -dictionary -increasing -index 0 $result]
+                }
+                1   {
+                    set option [string range $args 1 end]
+                    if { ($option in $::ms::scrollbar(non_styleable,options)) || ($option in $::ms::scrollbar(styleable,options)) } {
+                        return [list $::ms::default($w,$option) $::ms::current($w,$option)]
+                    } else {
+                        ::ms::Error "Invalid configure option, '$args'." $caller_info
+                    }
+                }
+                default {}
+            }
+        }
         delta    -
         fraction {
             # Synopsis:
