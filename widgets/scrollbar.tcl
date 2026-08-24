761a6afe1a67e314fb3cda697a41471fcecced59
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-24 12:12:32 +0200

    scrollbar: set the default value for each styleable option and if the option is managed by Tk, set also its current value

--- widgets/scrollbar.tcl
+++ widgets/scrollbar.tcl
@@ -1513,7 +1513,21 @@ proc ::ms::scrollbar::Style_Update { stylename caller_info } {
     }
 
     # Update all the scrollbar widgets that have stylename as a style.
-    foreach w $::ms::style($stylename,scrollbar,addrs) {}
+    foreach w $::ms::style($stylename,scrollbar,addrs) {
+        # Set the default value for each styleable option and if the option is managed by Tk, set also its current value.
+        foreach option $::ms::scrollbar(styleable,options) {
+            set ::ms::default($w,$option) $::ms::styleopt($::ms::theme,TScrollbar,$option)
+
+            switch -- $::ms::managed_by($w,$option) {
+                Tk  {
+                    switch -- [info exists ::ms::styleopt($::ms::theme,$stylename,$option)] {
+                        0   { set ::ms::current($w,$option) $::ms::default($w,$option) }
+                        1   { set ::ms::current($w,$option) $::ms::styleopt($::ms::theme,$stylename,$option) }
+                    }
+                }
+            }
+        }
+    }
 
     return ""
 }
