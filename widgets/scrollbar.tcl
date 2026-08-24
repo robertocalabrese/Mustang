a91ba52a763f91550d98f9bf9ca59b0005e67cfc
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-24 12:10:41 +0200

    scrollbar: if the parent style layout is not known by mustang, set it as the current theme layout orientation

--- widgets/scrollbar.tcl
+++ widgets/scrollbar.tcl
@@ -1039,6 +1039,11 @@ proc ::ms::scrollbar::Pathname_Cmd { w cmd args } {
                                     }
                                 }
                             }
+
+                            # If the parent style layout is not known by mustang, set it as the current theme layout orientation.
+                            if { $parent_style ni $::ms::layouts($::ms::theme) } {
+                                _ttk_style layout $parent_style [_ttk_style layout [string cat $orient "." TScrollbar]]
+                            }
                         }
                         default { ::ms::Error "Invalid number of arguments." $caller_info }
                     }
