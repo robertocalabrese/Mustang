f1ab7296803eff61e623263b335af11c4900145c
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-24 12:17:39 +0200

    scrollbar: add the 'Touchpad' procedure

--- widgets/scrollbar.tcl
+++ widgets/scrollbar.tcl
@@ -2017,4 +2017,87 @@ proc ::ms::scrollbar::Scroll { w amount { what units } } {
     return ""
 }
 
+## Touchpad
+#
+# Move the address linked to the widget along the X or Y axis (depending on the widget orientation axis).
+#
+# Where:
+#
+# w         Should be the scrollable widget real address involved.
+#
+# counter   Should be the *serial* field of a **TouchpadScroll** event (**%#**).
+#
+# amount    Should be the delta value of a **TouchpadScroll** event.
+#           The delta value represents the rotation units the mouse wheel has been moved.
+#           The sign of the value represents the direction the mouse wheel was scrolled.
+#           *Amount* is normally delivered by the **TouchpadScroll** event with a value of
+#           **+120.0** or **-120.0**, depending on the scroll direction.
+#
+#           If the value provided as *amount* is not an integer or a float,
+#           defaults to **+120.0**.
+#
+#           Note: **0** is not allowed. If provided, it will be changed to **+120.0**.
+#
+# what      Should be a string that specifies the unit type.
+#           Allowed values are the word **units** or **pages**.
+#           If not provided, defaults to **units**.
+#
+# It doesn't return anything.
+proc ::ms::scrollbar::Touchpad { w counter amount { what units } } {
+    # Acknowledgment: This code is taken (and adapted) from the 'Recent improvements
+    #                 on Tk 9' pdf paper by 'Csaba Nemethi'.
+
+    # <TouchpadScroll> events can be generated about 60 times per second
+    # during a two-finger gesture.
+    # This code allows the binding script to respond to every 5th <TouchpadScroll> event
+    # by testing if the 'counter' is divisible by 5.
+    if { [expr { $counter%5 }] != 0 } {
+        return ""
+    }
+
+    # Translate 'amount' in 'delta_x' and 'delta_y'.
+    lassign [::tk::PreciseScrollDeltas $amount] delta_x delta_y
+
+    # Check the 'what' provided.
+    switch -- $what {
+        units {
+            # Adjust 'delta_x' and 'delta_y' values, or the movement will be too slow.
+            set delta_x [expr { $delta_x*30 }]
+            set delta_y [expr { $delta_y*30 }]
+        }
+    }
+
+    # Set the right delta relative to the widget orientation axis.
+    switch -- $::ms::current($w,orient) {
+        horizontal {
+            # Check if there was a movement along the X axis.
+            if { $delta_x != 0 } {
+                # Move the horizontal scrollbar by 'delta_x' of 'what'.
+                ::ms::scrollbar::Scroll $w $delta_x $what
+            }
+
+            # Check if there was a movement along the Y axis.
+            if { $delta_y != 0 } {
+                # Move the parent widget by 'delta_y' of 'what'.
+                ::ms::Scroll_Parent_Y $w $delta_y $what
+            }
+        }
+        vertical {
+            # Check if there was a movement along the X axis.
+            if { $delta_x != 0 } {
+                # Move the parent widget by 'delta_x' of 'what'.
+                ::ms::Scroll_Parent_X $w $delta_x $what
+            }
+
+            # Check if there was a movement along the Y axis.
+            if { $delta_y != 0 } {
+                # Move the vertical scrollbar by 'delta_y' of 'what'.
+                ::ms::scrollbar::Scroll $w $delta_y $what
+            }
+        }
+    }
+
+    return ""
+}
+
 #*EOF*
