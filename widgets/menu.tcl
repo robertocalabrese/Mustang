2e7204f20f56c98e4af2992baee2b2f959762d6b
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-27 17:08:54 +0200

    menu: create the widget

--- widgets/menu.tcl
+++ widgets/menu.tcl
@@ -339,6 +339,38 @@ proc ::ms::menu::Command { window { args "" } } {
                     }
                 }
             }
+
+            ###############################
+            ##                           ##
+            ##     CREATE THE WIDGET     ##
+            ##                           ##
+            ###############################
+
+            # Note: 'activebackground', 'activeborderwidth', 'activeforeground', 'activerelief', 'activerelief', 'background', 'borderwidth', 'cursor',
+            #       'disabledbackground', 'font', 'foreground', 'relief' and 'selectcolor' are not allowed to change if the statespec changes.
+
+            # Create the widget.
+            menu $w   -activebackground $::ms::current($w,activebackground) \
+                     -activeborderwidth $::ms::current($w,activeborderwidth) \
+                      -activeforeground $::ms::current($w,activeforeground) \
+                          -activerelief $::ms::current($w,activerelief) \
+                            -background $::ms::current($w,background) \
+                           -borderwidth $::ms::current($w,borderwidth) \
+                                -cursor $::ms::current($w,cursor) \
+                    -disabledforeground $::ms::current($w,disabledforeground) \
+                                  -font $::ms::current($w,font) \
+                            -foreground $::ms::current($w,foreground) \
+                           -postcommand $::ms::current($w,postcommand) \
+                                -relief $::ms::current($w,relief) \
+                           -selectcolor $::ms::current($w,selectcolor) \
+                             -takefocus $::ms::current($w,takefocus) \
+                               -tearoff 0 \
+                        -tearoffcommand {} \
+                                 -title {} \
+                                  -type menubar;
+
+            # Set the widget toplevel.
+            set ::ms::addr($w,toplevel) [_winfo toplevel $w]
         }
         default { ::ms::Error "Invalid number of arguments." $caller_info }
     }
