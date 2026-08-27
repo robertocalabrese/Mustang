0cd3a15b5f0e8e0aa3670a11029088d8883483af
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-27 17:17:31 +0200

    menu: add the 'cget' command

--- widgets/menu.tcl
+++ widgets/menu.tcl
@@ -504,7 +504,24 @@ proc ::ms::menu::Pathname_Cmd { w cmd args } {
                 }
             }
         }
-        cget {}
+        cget {
+            # Synopsis:
+            #
+            # *window* **cget** *option*
+            switch -- [llength $args] {
+                0   { ::ms::Error "Missing cget option." $caller_info }
+                1   {
+                    # Check if the option provided is a valid 'styleable' or 'non-styleable' option.
+                    set option [string tolower [string range $args 1 end]]
+                    if { ($option in $::ms::menu(non_styleable,options)) || ($option in $::ms::menu(styleable,options)) } {
+                        return $::ms::current($w,$option)
+                    } else {
+                        ::ms::Error "Invalid option, '$args'." $caller_info
+                    }
+                }
+                default { ::ms::Error "Invalid option, '$args'." $caller_info }
+            }
+        }
         clone  -
         delete {}
         configure {}
