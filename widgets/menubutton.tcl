7b9f61bb6878231e100a939dde95536f0b896a03
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-29 17:36:01 +0200

    menubutton: set the current option values for each styleable option managed by TK

--- widgets/menubutton.tcl
+++ widgets/menubutton.tcl
@@ -1367,6 +1367,18 @@ proc ::ms::menubutton::Pathname_Cmd { w cmd args } {
                                     }
                                 }
                             }
+
+                            # Set the current option values for each styleable option managed by Tk.
+                            foreach option $::ms::menubutton(styleable,options) {
+                                switch -- $::ms::managed_by($w,$option) {
+                                    Tk  {
+                                        switch -- [info exists ::ms::styleopt($::ms::theme,$::ms::current($w,style),$option)] {
+                                            0   { set ::ms::current($w,$option) $::ms::default($w,$option) }
+                                            1   { set ::ms::current($w,$option) $::ms::styleopt($::ms::theme,$::ms::current($w,style),$option) }
+                                        }
+                                    }
+                                }
+                            }
                         }
                         default { ::ms::Error "Invalid number of arguments." $caller_info }
                     }
