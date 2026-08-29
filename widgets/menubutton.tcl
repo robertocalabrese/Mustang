2a48c8dcf713bf7eb17f6add2d56238a55d3aeb5
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-29 17:34:20 +0200

    menubutton: add the 'style' command

--- widgets/menubutton.tcl
+++ widgets/menubutton.tcl
@@ -1138,7 +1138,15 @@ proc ::ms::menubutton::Pathname_Cmd { w cmd args } {
                 default { ::ms::Error "Invalid number of arguments." $caller_info }
             }
         }
-        style {}
+        style {
+            # Synopsis:
+            #
+            # *window* **style**
+            switch -- [llength $args] {
+                0       { return $::ms::current($w,style) }
+                default { ::ms::Error "Invalid number of arguments." $caller_info }
+            }
+        }
         default { ::ms::Error "Invalid option, '$cmd'." $caller_info }
     }
 }
