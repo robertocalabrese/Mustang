a9379e0ec3d9be702e7f664731ad067750e80192
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-08-30 12:20:59 +0200

    canvas: fix the 'cget' command

--- widgets/canvas.tcl
+++ widgets/canvas.tcl
@@ -3554,7 +3554,7 @@ proc ::ms::canvas::Pathname_Cmd { w cmd args } {
                 0   { ::ms::Error "Missing cget option." $caller_info }
                 1   {
                     # Check if the option provided is a valid 'styleable' or 'non-styleable' option.
-                    set option [string range $args 1 end]
+                    set option [string tolower [string range $args 1 end]]
                     if { ($option in $::ms::canvas(non_styleable,options)) || ($option in $::ms::canvas(styleable,options)) } {
                         return $::ms::current($w,$option)
                     } else {
