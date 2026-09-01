35c6c6faef57c9acddf2757cc1bad73e2c1fca5b
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-01 08:31:42 +0200

    checkbutton: refactor the 'Return' procedure

--- widgets/checkbutton.tcl
+++ widgets/checkbutton.tcl
@@ -3302,19 +3302,20 @@ proc ::ms::checkbutton::FocusOut { w } {
 #
 # It doesn't return anything.
 proc ::ms::checkbutton::Return { w } {
-    # Check the widget state.
+    # Check the widget's state.
     switch -- $::ms::current($w,state) {
-        normal {
-            # Invoke the widget command, if any.
-            switch -- $::ms::current($w,command) {
-                ""      {}
-                default {
-                    try {
-                        uplevel #0 [list $w.indicator invoke]
-                    } on error { errortext errorcode } {
-                        ::ms::Error "$errortext" ""
-                    }
-                }
+        disabled { return "" }
+    }
+
+    # Check if there is a command associated with the widget.
+    switch -- $::ms::current($w,command) {
+        ""      {}
+        default {
+            # Invoke the command associated with the widget.
+            try {
+                uplevel #0 [list {*}$::ms::current($w,command)]
+            } on error {} {
+                ::ms::Error "Invalid command, '$::ms::current($w,command)'." ""
             }
         }
     }
