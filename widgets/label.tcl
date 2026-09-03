09676a6644481ae076a37906bb1f47a67906c0b3
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-03 11:56:10 +0200

    label: refactor the 'FocusOut' procedure

--- widgets/label.tcl
+++ widgets/label.tcl
@@ -2629,32 +2629,18 @@ proc ::ms::label::Destroy { w } {
 #
 # It doesn't return anything.
 proc ::ms::label::FocusOut { w } {
-    # Check the contextual menu associated with this widget, if any.
+    # Check if a contextual menu was assigned to the widget.
+    # If not, use the contextual menu of the widget's toplevel.
     set cmenu $::ms::current($w,cmenu)
     switch -- $cmenu {
-        ""  {
-            # Check if a contextual menu was associated with the widget's toplevel.
-            set cmenu $::ms::current($::ms::addr($w,toplevel),cmenu)
-            switch -- $cmenu {
-                ""      {}
-                default {
-                    # If the contextual menu of the widget's toplevel is open do not loose the focus (graphically).
-                    switch -- [_winfo exists $cmenu] {
-                        1   { return "" }
-                    }
-                }
-            }
-        }
-        default {
-            # If the contextual menu of the widget is open do not loose the focus (graphically).
-            switch -- [_winfo exists $cmenu] {
-                1   { return "" }
-            }
-        }
+        ""  { set cmenu $::ms::current($::ms::addr($w,toplevel),cmenu) }
     }
 
-    # Change the widget dynamic state to '!focus'.
-    ::ms::label::Pathname_Cmd $w state !focus
+    # If 'cmenu' exists (meaning it's open), do not loose the focus (graphically).
+    switch -- [_winfo exists $cmenu] {
+        0   { interp invokehidden {} $w state [list !focus] }
+        1   { interp invokehidden {} $w state [list  focus] }
+    }
 
     return ""
 }
