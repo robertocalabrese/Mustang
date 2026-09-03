1a3ea1414bd81e66ce6e9f8585ae879ee8e83e15
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-03 15:37:53 +0200

    panedwindow: refactor the 'identify' command

--- widgets/panedwindow.tcl
+++ widgets/panedwindow.tcl
@@ -1877,13 +1877,18 @@ proc ::ms::panedwindow::Pathname_Cmd { w cmd args } {
         identify {
             # Synopsis:
             #
-            # *window* **identify** *component* *x* *y*
-            #    *window* **identify** **element** *x* *y*
-            #    *window* **identify** **sash** *x* *y*
+            # *window* **identify** *x* *y*
+            # *window* **identify** **element** *x* *y*
+            # *window* **identify** **sash** *x* *y*
             switch -- [llength $args] {
+                2   {
+                    set x [lindex $args 0]
+                    set y [lindex $args 1]
+                }
                 3   {
-                    # Check that the first argument of 'args' is the word 'element' or 'sash'.
-                    switch -- [lindex $args 0] {
+                    # Check that the first argument of 'args' is the word "element" or 'sash'.
+                    set subcommand [lindex $args 0]
+                    switch -- $subcommand {
                         element -
                         sash    {}
                         default { ::ms::Error "Invalid option, '$args'." $caller_info }
@@ -1891,36 +1896,48 @@ proc ::ms::panedwindow::Pathname_Cmd { w cmd args } {
 
                     set x [lindex $args 1]
                     set y [lindex $args 2]
-
-                    # Check that the coordinates provided are valid.
-                    switch -- [string is integer -strict $x] {
-                        0   { ::ms::Error "Invalid coordinate, '$x'." $caller_info }
-                    }
-
-                    switch -- [string is integer -strict $y] {
-                        0   { ::ms::Error "Invalid coordinate, '$y'." $caller_info }
-                    }
-
-                    # Check if the coordinates provided falls upon the widget's sash.
-                    try {
-                        interp invokehidden {} $w identify sash $x $y
-                    } on error { errortext errorcode } {
-                        ::ms::Error "$errortext" $caller_info
-                    } on ok { result } {
-                        # Check the subcommand provided.
-                        switch -- [lindex $args 0] {
-                            element {
-                                switch -- $result {
-                                    ""   { set result "Panedwindow.client" }
-                                    sash { set result "Panedwindow.sash"   }
-                                }
-                            }
-                        }
-
-                        return $result
-                    }
                 }
                 default { ::ms::Error "Invalid number of arguments." $caller_info }
+            }
+
+            # Check that the (x,y) relative coordinates provided are valid.
+            switch -- [string is integer -strict $x] {
+                0   { return "" }
+                1   {
+                    # Check that the 'x' coordinate is a positive integer ('0' included).
+                    if { $x < 0 } {
+                        return ""
+                    }
+                }
+            }
+
+            switch -- [string is integer -strict $y] {
+                0   { return "" }
+                1   {
+                    # Check that the 'y' coordinate is a positive integer ('0' included).
+                    if { $y < 0 } {
+                        return ""
+                    }
+                }
+            }
+
+            # Check if the coordinates provided falls upon the widget's sash.
+            try {
+                interp invokehidden {} $w identify $subcommand $x $y
+            } on error { errortext errorcode } {
+                ::ms::Error "$errortext" $caller_info
+            } on ok { result } {
+                # Check the subcommand provided.
+                switch -- $subcommand {
+                    element {
+                        switch -- $result {
+                            sash    { set result "Panedwindow.sash" }
+                            default { set result "Panedwindow.client" }
+                        }
+                    }
+                }
+
+                return $result
             }
         }
         insert {
