eb0f4642d60f79edda817f8c0d11413eb40777f5
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-03 15:30:35 +0200

    panedwindow: remove the 'Hover' procedure

--- widgets/panedwindow.tcl
+++ widgets/panedwindow.tcl
@@ -840,10 +840,6 @@ _bind _Panedwindow <Motion> { ::ms::panedwindow::Set_Cursor   %W %x %y; break }
 _bind _Panedwindow <Enter>  { ::ms::panedwindow::Reset_Cursor %W; break }
 _bind _Panedwindow <Leave>  { ::ms::panedwindow::Reset_Cursor %W; break }
 
-# Enter/Leave
-_bind _Panedwindow <Enter> [list +::ms::panedwindow::Hover %W %X %Y]
-_bind _Panedwindow <Leave> [list +::ms::panedwindow::Hover %W %X %Y]
-
 # FocusIn/FocusOut
 _bind _Panedwindow <FocusIn>  { ::ms::panedwindow::Pathname_Cmd %W state  focus; break }
 _bind _Panedwindow <FocusOut> { ::ms::panedwindow::Pathname_Cmd %W state !focus; break }
@@ -2433,47 +2429,6 @@ proc ::ms::panedwindow::Destroy { w } {
                          ::ms::managed_by($w,relief);
 
     unset -nocomplain -- ::ms::style($w,widget)
-
-    return ""
-}
-
-## Hover
-#
-# Manage the **Enter** and **Leave** event on the widget.
-#
-# Where:
-#
-# w      Should be the widget real address involved.
-#
-# X, Y   Should be the mouse pointer (X,Y) root coordinates.
-#        These value are provided directly by the **Enter** or **Leave** event.
-#
-# It doesn't return anything.
-proc ::ms::panedwindow::Hover { w X Y } {
-    # Get the dimensions of the widget that acts as a border object.# Get the border object dimensions.
-    set height [_winfo height $::ms::addr($w,border)]
-    set width  [_winfo width  $::ms::addr($w,border)]
-
-    # Get the north-west (nw) root coordinates of the widget that acts as a border object.# Get the border object north-west (nw) root coordinates.
-    set X_nw [_winfo rootx $::ms::addr($w,border)]
-    set Y_nw [_winfo rooty $::ms::addr($w,border)]
-
-    # Get the widget south-east (se) root coordinates of the widget that acts as a border object.# Get the address south-east (se) root coordinates.
-    set X_se [expr { $X_nw+$width }]
-    set Y_se [expr { $Y_nw+$height }]
-
-    # Check if the mouse pointer coordinates are inside or outside the widget acting as a border object.
-    if { ($X <= $X_nw) || ($X >= $X_se) || ($Y <= $Y_nw) || ($Y >= $Y_se) } {
-        # The mouse cursor is outside the widget acting as a border object.
-
-        # Change the widget dynamic state to '!hover'.
-        ::ms::panedwindow::Pathname_Cmd $w state !hover
-    } else {
-        # The mouse cursor is inside the widget acting as a border object.
-
-        # Change the widget dynamic state to 'hover'.
-        ::ms::panedwindow::Pathname_Cmd $w state hover
-    }
 
     return ""
 }
