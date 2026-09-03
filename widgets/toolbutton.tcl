f29021275ee19894fb7cb104f74fb148c2b2e2b0
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-03 16:01:37 +0200

    toolbutton: refactor the 'ButtonPress' procedure

--- widgets/toolbutton.tcl
+++ widgets/toolbutton.tcl
@@ -2735,66 +2735,23 @@ proc ::ms::toolbutton::Style_Update { stylename caller_info } {
 proc ::ms::toolbutton::ButtonPress { w } {
     # Check the widget's state.
     switch -- $::ms::current($w,state) {
-        disabled {
-            # Check the parent of the widget address provided, if any.
-            set parent [_winfo parent $w]
-            switch -- $parent {
-                ""      {}
-                default {
-                    # Propagate the action to the widget's parents.
+        disabled { return "" }
+    }
 
-                    # ATTENTION!
-                    #
-                    # This is a recursive loop. The only way to exit is:
-                    #   - If there is no more parent to check for.
-                    #   - If 'parent' is a scrollable megawidget.
-                    set i 1
-                    while { $i > 0 } {
-                        # Check if 'parent' belongs to a scrollable megawidget.
-                        if { $parent in $::ms::addr(megawidgets,scrollable) } {
-                            _focus -force $parent
-                            return ""
-                        }
+    # Check if the widget is focussable or not.
+    switch -- [::ms::Is_Focussable $w] {
+        0   { return "" }
+    }
 
-                        # Check the next parent, if any.
-                        set parent [_winfo parent $parent]
-                        switch -- $parent {
-                            ""  {
-                                # There are no more parents to check for.
-                                # Stop the recursive iteration.
-                                break
-                            }
-                        }
-                    }
-                }
-            }
+    # Check if the widget is already focussed.
+    switch -- [interp invokehidden {} $w instate [list !focus]] {
+        1   {
+            # Focus the widget.
+            _focus -force $w
 
-            # Check if the widget's toplevel was created by mustang.
-            switch -- [info exists ::ms::data($::ms::addr($w,toplevel),classtype)] {
-                0   {
-                    # If possible, focus the widget's toplevel.
-                    try {
-                        _focus -force [_winfo toplevel $w]
-                    } on error {} {
-                        # Do nothing
-                    }
-                }
-                1   {
-                    # Check the widget's toplevel takefocus.
-                    switch -- $::ms::current($::ms::addr($w,toplevel),takefocus) {
-                        0   {
-                            # Momentarily set the toplevel takefocus to '1'.
-                            # We will re-establish its original takefocus value later, during its 'FocusOut' event.
-                            interp invokehidden {} $::ms::addr($w,toplevel) configure -takefocus 1
-                        }
-                    }
-
-                    # Focus the widget's toplevel.
-                    _focus -force $::ms::addr($w,toplevel)
-                }
-            }
+            # Change the widget dynamic state to 'focus'.
+            interp invokehidden {} $w state [list focus]
         }
-        default { ::ttk::clickToFocus $w }
     }
 
     return ""
