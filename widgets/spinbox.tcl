5d2b6ce0ae7c0e2a2e30ad887fb766e71923cbd8
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-04 16:12:56 +0200

    spinbox: reorganize the code

--- widgets/spinbox.tcl
+++ widgets/spinbox.tcl
@@ -5027,6 +5027,29 @@ proc ::ms::spinbox::ButtonPress { w x y mode } {
     return ""
 }
 
+## Check_TextVariable
+#
+# Validate the textvariable value, everytime it changes.
+#
+# Where:
+#
+# w        Should be the widget real address involved.
+#
+# name1,
+# name2,
+# op       Should be the tracing arguments.
+#          Do not pass any value, the **trace** command will automatically pass these values.
+#
+# It doesn't return anything.
+proc ::ms::spinbox::Check_TextVariable { w name1 name2 op } {
+    switch -- $::ms::current($w,datatype) {
+        none    {}
+        default { ::ms::spinbox::Return $w }
+    }
+
+    return ""
+}
+
 ## Decrement
 #
 # Manage the **Decrement** event upon the widget.
@@ -5733,29 +5756,6 @@ proc ::ms::spinbox::Return { w } {
         set ::ms::data($w,current_value) $value
 
         ::ms::Execute_Widget_Cmd $w
-    }
-
-    return ""
-}
-
-## Check_TextVariable
-#
-# Validate the textvariable value, everytime it changes.
-#
-# Where:
-#
-# w        Should be the widget real address involved.
-#
-# name1,
-# name2,
-# op       Should be the tracing arguments.
-#          Do not pass any value, the **trace** command will automatically pass these values.
-#
-# It doesn't return anything.
-proc ::ms::spinbox::Check_TextVariable { w name1 name2 op } {
-    switch -- $::ms::current($w,datatype) {
-        none    {}
-        default { ::ms::spinbox::Return $w }
     }
 
     return ""
