3f9a04b0be32b967856d3350e5b0246fd0005238
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-05 10:30:24 +0200

    palette: refactor the 'identify' command

--- widgets/palette.tcl
+++ widgets/palette.tcl
@@ -3747,8 +3747,13 @@ proc ::ms::palette::Pathname_Cmd { w cmd args } {
         identify {
             # Synopsis:
             #
+            # *window* **identify** *x* *y*
             # *window* **identify** **element** *x* *y*
             switch -- [llength $args] {
+                2   {
+                    set x [lindex $args 0]
+                    set y [lindex $args 1]
+                }
                 3   {
                     # Check that the first argument of 'args' is the word "element".
                     switch -- [lindex $args 0] {
@@ -3758,48 +3763,54 @@ proc ::ms::palette::Pathname_Cmd { w cmd args } {
 
                     set x [lindex $args 1]
                     set y [lindex $args 2]
+                }
+                default { ::ms::Error "Invalid number of arguments." $caller_info }
+            }
 
-                    # Check that the coordinates provided are valid.
-                    switch -- [string is integer -strict $x] {
-                        0   { ::ms::Error "Invalid coordinate, '$x'." $caller_info }
-                    }
-
-                    switch -- [string is integer -strict $y] {
-                        0   { ::ms::Error "Invalid coordinate, '$y'." $caller_info }
-                    }
-
-                    # Get the root coordinates of the north-west corner of the container ('$w').
-                    set rootx [_winfo rootx $w]
-                    set rooty [_winfo rooty $w]
-
-                    # Transform the relative coordinates provided into root coordinates.
-                    set X [expr { $rootx+$x }]
-                    set Y [expr { $rooty+$y }]
-
-                    # Get the widget address containing the point given by the root coordinates calculated.
-                    set widget [_winfo containing -display $w $X $Y]
-
-                    # Return the name of the object, or an empty string if there are no canvas objects at the coordinates provided.
-                    if { $widget eq $w } {
-                        return "Palette.hull"
-                    } elseif { $widget eq "$w.preview" } {
-                        return "Palette.preview"
-                    } elseif { $widget eq "$w.combobox" } {
-                        try {
-                            interp invokehidden {} $w identify element $x $y
-                        } on error { errortext errorcode } {
-                            ::ms::Error "$errortext" $caller_info
-                        } on ok { result } {
-                            switch -nocase -- $result {
-                                textarea { return "Palette.textarea"  }
-                                default  { return "Palette.downarrow" }
-                            }
-                        }
-                    } else {
+            # Check that the (x,y) relative coordinates provided are valid.
+            switch -- [string is integer -strict $x] {
+                0   { return "" }
+                1   {
+                    # Check that the 'x' coordinate is a positive integer ('0' included).
+                    if { $x < 0 } {
                         return ""
                     }
                 }
-                default { ::ms::Error "Invalid number of arguments." $caller_info }
+            }
+
+            switch -- [string is integer -strict $y] {
+                0   { return "" }
+                1   {
+                    # Check that the 'y' coordinate is a positive integer ('0' included).
+                    if { $y < 0 } {
+                        return ""
+                    }
+                }
+            }
+
+            # Execute the command.
+            if { $widget eq $w } {
+                return "Palette.hull"
+            } elseif { $widget eq "$w.preview" } {
+                return "Palette.preview"
+            } else {
+                # Go deeper...
+                try {
+                    $w.combobox identify element $x $y
+                } on error { errortext errorcode } {
+                    ::ms::Error "$errortext" $caller_info
+                } on ok { result } {
+                    switch -nocase -- $result {
+                        downarrow { return "Palette.downarrow" }
+                        default   {
+                            # Check the widget's state.
+                            switch -- $::ms::current($w,state) {
+                                normal  { return "Palette.textarea" }
+                                default { return "Palette.label" }
+                            }
+                        }
+                    }
+                }
             }
         }
         insert {
