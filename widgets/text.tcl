7613f4461e0993a3830673b02bc6dd79a50a164c
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-07 09:34:48 +0200

    text: refactor the 'Clear_IME_Marked_Text' procedure

--- widgets/text.tcl
+++ widgets/text.tcl
@@ -7669,6 +7669,12 @@ proc ::ms::text::Accent_Backspace { w } {
 #
 # It doesn't return anything.
 proc ::ms::text::Clear_IME_Marked_Text { w } {
+    # Check the widget's state.
+    switch -- $::ms::current($w,state) {
+        disabled -
+        readonly { return "" }
+    }
+
     # Check if the widget is scrollable or not.
     switch -- $::ms::current($w,scrollbar) {
         false { set address [list interp invokehidden {} $w] }
