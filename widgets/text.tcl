b8a0eb1d424875f0db438e0132074f624934e7b7
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-07 11:24:27 +0200

    text: refactor the 'Select_None' procedure

--- widgets/text.tcl
+++ widgets/text.tcl
@@ -9112,6 +9112,12 @@ proc ::ms::text::Select_Key { w new } {
 #
 # It doesn't return anything.
 proc ::ms::text::Select_None { w } {
+    # Check the widget's state.
+    switch -- $::ms::current($w,state) {
+        disabled -
+        readonly { return "" }
+    }
+
     # Check if the widget is scrollable or not.
     switch -- $::ms::current($w,scrollbar) {
         false { set address [list interp invokehidden {} $w] }
