a7508296b96f5be72855ad1abe488dfca19ef9fc
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-07 11:31:08 +0200

    text: refactor the 'Redo' procedure

--- widgets/text.tcl
+++ widgets/text.tcl
@@ -9484,6 +9484,12 @@ proc ::ms::text::Undo { w } {
 #
 # It doesn't return anything.
 proc ::ms::text::Redo { w } {
+    # Check the widget's state.
+    switch -- $::ms::current($w,state) {
+        disabled -
+        readonly { return "" }
+    }
+
     # Check if the widget is scrollable or not.
     switch -- $::ms::current($w,scrollbar) {
         false { set address [list interp invokehidden {} $w] }
