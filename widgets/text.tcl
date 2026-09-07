af024d5776ce3d88b18d8dd1e2d8e4c220afa6f3
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-07 09:33:02 +0200

    text: refactor the 'Delete' procedure

--- widgets/text.tcl
+++ widgets/text.tcl
@@ -7509,6 +7509,12 @@ proc ::ms::text::Backspace { w } {
 #
 # It doesn't return anything.
 proc ::ms::text::Delete { w } {
+    # Check the widget's state.
+    switch -- $::ms::current($w,state) {
+        disabled -
+        readonly { return "" }
+    }
+
     # Check if the widget is scrollable or not.
     switch -- $::ms::current($w,scrollbar) {
         false { set address [list interp invokehidden {} $w] }
