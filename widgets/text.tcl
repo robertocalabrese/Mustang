51e20dd0f8a82043ae35eef15ba3375b172d94e2
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-07 09:34:23 +0200

    text: refactor the 'Accent_Backspace' procedure

--- widgets/text.tcl
+++ widgets/text.tcl
@@ -7641,6 +7641,12 @@ proc ::ms::text::Delete_Till_LineEnd { w } {
 #
 # It doesn't return anything.
 proc ::ms::text::Accent_Backspace { w } {
+    # Check the widget's state.
+    switch -- $::ms::current($w,state) {
+        disabled -
+        readonly { return "" }
+    }
+
     # Check if the widget is scrollable or not.
     switch -- $::ms::current($w,scrollbar) {
         false { set address [list interp invokehidden {} $w] }
