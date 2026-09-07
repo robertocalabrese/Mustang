be55d1bb5ea7308bd11a73de0c137d14abc59cfe
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-07 09:28:55 +0200

    text: refactor the 'Clear' procedure

--- widgets/text.tcl
+++ widgets/text.tcl
@@ -7192,6 +7192,12 @@ proc ::ms::text::Scrollbar_Update { w } {
 #
 # It doesn't return anything.
 proc ::ms::text::Clear { w } {
+    # Check the widget's state.
+    switch -- $::ms::current($ww,state) {
+        disabled -
+        readonly { return "" }
+    }
+
     # Check if the widget is scrollable or not.
     switch -- $::ms::current($w,scrollbar) {
         false { set address [list interp invokehidden {} $w] }
