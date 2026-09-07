1e91d87157618cf5635900a0eb562622ca7bb6be
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-07 09:35:03 +0200

    text: refactor the 'Start_IME_Marked_Text' procedure

--- widgets/text.tcl
+++ widgets/text.tcl
@@ -7697,6 +7697,12 @@ proc ::ms::text::Clear_IME_Marked_Text { w } {
 #
 # It doesn't return anything.
 proc ::ms::text::Start_IME_Marked_Text { w } {
+    # Check the widget's state.
+    switch -- $::ms::current($w,state) {
+        disabled -
+        readonly { return "" }
+    }
+
     # Check if the widget is scrollable or not.
     switch -- $::ms::current($w,scrollbar) {
         false { set address [list interp invokehidden {} $w] }
