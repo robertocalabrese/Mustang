cc04220a52bc5700e5a5bf9c74f006eba484b2f0
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-07 11:19:05 +0200

    text: refactor the 'Next_Line' procedure

--- widgets/text.tcl
+++ widgets/text.tcl
@@ -8606,14 +8606,14 @@ proc ::ms::text::Next_Line { w } {
                 ""  {
                     # Check the widget's state.
                     switch -- $::ms::current($w,state) {
-                        disabled { ::ms::Scroll_Parent_Y   $w -120.0 units }
+                        readonly { ::ms::Scroll_Parent_Y   $w -120.0 units }
                         normal   { ::ms::text::Move_Cursor $w [::ms::text::Line_Index 1] }
                     }
                 }
                 default {
                     # Check the widget's state.
                     switch -- $::ms::current($w,state) {
-                        disabled { ::ms::Scroll_Widget_Y   $w -120.0 units }
+                        readonly { ::ms::Scroll_Widget_Y   $w -120.0 units }
                         normal   { ::ms::text::Move_Cursor $w [::ms::text::Line_Index 1] }
                     }
                 }
@@ -8625,14 +8625,14 @@ proc ::ms::text::Next_Line { w } {
                 off {
                     # Check the widget's state.
                     switch -- $::ms::current($w,state) {
-                        disabled { ::ms::Scroll_Parent_Y   $w -120.0 units }
+                        readonly { ::ms::Scroll_Parent_Y   $w -120.0 units }
                         normal   { ::ms::text::Move_Cursor $w [::ms::text::Line_Index 1] }
                     }
                 }
                 on  {
                     # Check the widget's state.
                     switch -- $::ms::current($w,state) {
-                        disabled { ::ms::Scroll_Widget_Y   $w -120.0 units }
+                        readonly { ::ms::Scroll_Widget_Y   $w -120.0 units }
                         normal   { ::ms::text::Move_Cursor $w [::ms::text::Line_Index 1] }
                     }
                 }
