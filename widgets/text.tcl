042196d6538d07c3300e01977d16733008524c3c
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-07 11:23:47 +0200

    text: refactor the 'Select_All' procedure

--- widgets/text.tcl
+++ widgets/text.tcl
@@ -9022,6 +9022,12 @@ proc ::ms::text::Previous_Paragraph { w } {
 #
 # It doesn't return anything.
 proc ::ms::text::Select_All { w } {
+    # Check the widget's state.
+    switch -- $::ms::current($w,state) {
+        disabled -
+        readonly { return "" }
+    }
+
     # Check if the widget is scrollable or not.
     switch -- $::ms::current($w,scrollbar) {
         false { set address [list interp invokehidden {} $w] }
