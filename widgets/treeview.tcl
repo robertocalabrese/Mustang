ced775fd1ea56d169d73af692028fbc63fa4990a
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-08 12:19:35 +0200

    treeview: fix the '-cmenu' option description

--- widgets/treeview.tcl
+++ widgets/treeview.tcl
@@ -178,22 +178,29 @@
 #
 # **-cmenu**                  Specifies the contextual menu address that will be assigned to the widget.
 #
-#                             The contextual menu will be assign to the *treeview* object of the megawidget.
-#                             If the *cmenu* value is the empty string or invalid, nothing will happen.
+#                             Simple treeview (**-scrollable false**):
+#                                The contextual menu will be assign to the widget.
+#                                If the *cmenu* value is the empty string or invalid, nothing will happen.
 #
-#                             The *hull* object will rather use the contextual menu of the widget's toplevel, if any.
-#                             If the developer needs a different contextual menu for it, a variable called
-#                             '::ms::data($short_addr,cmenu,shell)' can be set with a valid contextual menu address in
-#                             order to be used instead of the toplevel one.
+#                             Scrollable treeview (**-scrollable true**):
+#                                The contextual menu will be assign to the *content* object of the megawidget (the *treeview* object).
+#                                If the *cmenu* value is the empty string or invalid, nothing will happen.
 #
-#                             Note: '$short_addr' must be the short address of the treeview widget.
-#                                   See the [tk](/wiki/commands/tk.md) command to know more about short and real address.
+#                                The *hull* and *fake scrollbars* objects will use the contextual menu of the widget's toplevel, if any.
+#                                If the developer needs a different contextual menu for it, a variable called
+#                                '::ms::data($short_addr,cmenu,shell)' can be set with a valid contextual menu address in
+#                                order to be used instead of the toplevel one.
 #
-#                             If '::ms::data($short_addr,cmenu,shell)' is set with an empty string or with an invalid contextual menu
-#                             address, it will be ignored and the contextual menu of the widget's toplevel (if any) will be used.
-#                             If the widget's toplevel doesn't have a contextual menu, nothing will happen.
+#                                Note: '$short_addr' must be the short address of the treeview widget.
+#                                      See the [tk](/wiki/commands/tk.md) command to know more about short and real address.
 #
-#                             The *scrollbar* objects are not supposed to have a contextual menu and will not be link with any.
+#                                If '::ms::data($short_addr,cmenu,shell)' is set with an empty string or with an invalid
+#                                contextual menu address, it will be ignored and the contextual menu of the widget's toplevel
+#                                (if any) will be used. If the widget's toplevel doesn't have a contextual menu, nothing will happen.
+#
+#                                The *scrollbar* objects are not supposed to have a contextual menu and will not be link with any.
+#
+#                             Note: If '::ms::data($short_addr,cmenu,shell)' is set for a simple treeview widget, it will be silently ignored.
 #
 #                             If not provided, defaults to the empty string.
 #
