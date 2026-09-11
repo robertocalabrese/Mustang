58d4a8c2f7f83a0aeae9e5ee720e01e8e37ff970
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-11 10:57:05 +0200

    menubutton: refactor the 'ButtonPress' procedure

--- widgets/menubutton.tcl
+++ widgets/menubutton.tcl
@@ -3135,6 +3135,11 @@ proc ::ms::menubutton::ButtonPress { w } {
         disabled { return "" }
     }
 
+    # Check if the widget is focussable or not.
+    switch -- [::ms::Is_Focussable $w] {
+        0   { return "" }
+    }
+
     # Check if there is a menu associated with the widget.
     switch -- $::ms::current($w,menu) {
         ""  { return "" }
