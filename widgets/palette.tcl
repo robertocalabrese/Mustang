e6196268d149c4255e82b4b7dfb66599a5024179
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-11 11:47:37 +0200

    palette: put the note in the 'Popdown_Touchpad' procedure description

--- widgets/palette.tcl
+++ widgets/palette.tcl
@@ -6858,6 +6858,10 @@ proc ::ms::palette::Popdown_Tab { popdown dir } {
 #
 # Manage the **TouchpadScroll** and **Control-TouchpadScroll** events on the popdown window.
 #
+# Note: This code is taken (and adapted) from the 'Recent improvements
+#       on Tk 9' pdf paper by 'Csaba Nemethi'.
+#       All credits goes to him.
+#
 # Where:
 #
 # w         Should be the scrollable widget real address involved.
@@ -6888,9 +6892,6 @@ proc ::ms::palette::Popdown_Tab { popdown dir } {
 #
 # It doesn't return anything.
 proc ::ms::palette::Popdown_Touchpad { w x y counter amount { what units } } {
-    # Acknowledgment: This code is taken (and adapted) from the 'Recent improvements
-    #                 on Tk 9' pdf paper by 'Csaba Nemethi'.
-
     # **TouchpadScroll** events can be generated about 60 times per second
     # during a two-finger gesture.
     # This allow the binding script to respond to every 5th **TouchpadScroll** event
