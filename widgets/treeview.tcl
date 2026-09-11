f8dabfa88f45d950d5d6bc47bf3c057a77dd21bb
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-11 12:26:49 +0200

    treeview: refactor the 'DoubleClick' procedure

--- widgets/treeview.tcl
+++ widgets/treeview.tcl
@@ -6306,6 +6306,11 @@ proc ::ms::treeview::DoubleClick { w x y } {
         disabled { return "" }
     }
 
+    # Check if the widget is focussable or not.
+    switch -- [::ms::Is_Focussable $::ms::addr($w,widget)] {
+        0   { return "" }
+    }
+
     # Check if the widget is scrollable or not.
     switch -- $::ms::current($w,scrollable) {
         false { set address [list interp invokehidden {} $w] }
