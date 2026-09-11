aeeae89a1886632f4e5ed3d45bfbde400187f2eb
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-11 11:48:27 +0200

    scrollbar: put the note in the 'Touchpad' procedure description

--- widgets/scrollbar.tcl
+++ widgets/scrollbar.tcl
@@ -2687,6 +2687,10 @@ proc ::ms::scrollbar::Scroll { w amount { what units } } {
 #
 # Move the address linked to the widget along the X or Y axis (depending on the widget orientation axis).
 #
+# Note: This code is taken (and adapted) from the 'Recent improvements
+#       on Tk 9' pdf paper by 'Csaba Nemethi'.
+#       All credits goes to him.
+#
 # Where:
 #
 # w         Should be the scrollable widget real address involved.
@@ -2710,9 +2714,6 @@ proc ::ms::scrollbar::Scroll { w amount { what units } } {
 #
 # It doesn't return anything.
 proc ::ms::scrollbar::Touchpad { w counter amount { what units } } {
-    # Acknowledgment: This code is taken (and adapted) from the 'Recent improvements
-    #                 on Tk 9' pdf paper by 'Csaba Nemethi'.
-
     # <TouchpadScroll> events can be generated about 60 times per second
     # during a two-finger gesture.
     # This code allows the binding script to respond to every 5th <TouchpadScroll> event
