58c94bab9b609c83da41e641875814ce09856a8f
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-11 12:26:24 +0200

    treeview: refactor the 'ButtonRelease' procedure

--- widgets/treeview.tcl
+++ widgets/treeview.tcl
@@ -6251,6 +6251,11 @@ proc ::ms::treeview::ButtonRelease { w x y } {
         disabled { return "" }
     }
 
+    # Check if the widget is focussable or not.
+    switch -- [::ms::Is_Focussable $::ms::addr($w,widget)] {
+        0   { return "" }
+    }
+
     # Check if the widget is scrollable or not.
     switch -- $::ms::current($w,scrollable) {
         false { set address [list interp invokehidden {} $w] }
