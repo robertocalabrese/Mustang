591292293146a58d66b013ee05994b3cfe6b2876
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-11 11:48:52 +0200

    spinbox: put the note in the 'Touchpad' procedure description

--- widgets/spinbox.tcl
+++ widgets/spinbox.tcl
@@ -6577,6 +6577,10 @@ proc ::ms::spinbox::Shift_MouseWheel { w amount } {
 #   2 - If the listbox can scroll vertically, scroll it by units (**TouchpadScroll**) or by pages
 #       (**Control-TouchpadScroll**), otherwise don't do any movement on the vertical axis.
 #
+# Note: This code is taken (and adapted) from the 'Recent improvements
+#       on Tk 9' pdf paper by 'Csaba Nemethi'.
+#       All credits goes to him.
+#
 # Where:
 #
 # w         Should be the scrollable widget real address involved.
@@ -6603,9 +6607,6 @@ proc ::ms::spinbox::Shift_MouseWheel { w amount } {
 #
 # It doesn't return anything.
 proc ::ms::spinbox::Touchpad { w counter amount } {
-    # Acknowledgment: This code is taken (and adapted) from the 'Recent improvements
-    #                 on Tk 9' pdf paper by 'Csaba Nemethi'.
-
     # **TouchpadScroll** events can be generated about 60 times per second
     # during a two-finger gesture.
     # This allow the binding script to respond to every 5th **TouchpadScroll** event
