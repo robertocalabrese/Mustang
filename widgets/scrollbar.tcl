9f87c93f4782d44207ebfa5df25b838911f8b0b2
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-13 16:00:33 +0200

    scrollbar: remove the 'Scroll' procedure

--- widgets/scrollbar.tcl
+++ widgets/scrollbar.tcl
@@ -763,16 +763,16 @@ _bind _Scrollbar <FocusOut> { interp invokehidden %W state [list !focus]; break 
 # Mousewheel and Touchpad
 
 # Move the widget's content address by one unit up or down (depending on the mousewheel direction).
-_bind _Scrollbar <MouseWheel> { ::ms::scrollbar::Scroll %W %D units; break }
+_bind _Scrollbar <MouseWheel> { ::ms::scrollbar::Pathname_Cmd %W scroll %D units; break }
 
 # Move the widget's content address by one unit left or right (depending on the mousewheel direction).
-_bind _Scrollbar <Shift-MouseWheel> { ::ms::scrollbar::Scroll %W %D units; break }
+_bind _Scrollbar <Shift-MouseWheel> { ::ms::scrollbar::Pathname_Cmd %W scroll %D units; break }
 
 # Move the widget's content address by one page up or down (depending on the mousewheel direction).
-_bind _Scrollbar <Control-MouseWheel> { ::ms::scrollbar::Scroll %W %D pages; break }
+_bind _Scrollbar <Control-MouseWheel> { ::ms::scrollbar::Pathname_Cmd %W scroll %D pages; break }
 
 # Move the widget's content address by one page left or right (depending on the mousewheel direction).
-_bind _Scrollbar <Control-Shift-MouseWheel> { ::ms::scrollbar::Scroll %W %D pages; break }
+_bind _Scrollbar <Control-Shift-MouseWheel> { ::ms::scrollbar::Pathname_Cmd %W scroll %D pages; break }
 
 # Note: **TouchpadScroll** and **Control-TouchpadScroll** only works on Windows and macOS.
 #       On Linux they will be ignored and touchpads movements will be processed as mousewheel events.
@@ -2646,57 +2646,6 @@ proc ::ms::scrollbar::Drag { w x y } {
     return ""
 }
 
-## Scroll
-#
-# Scrolls the address linked to the widget along the X or Y axis (depending on the widget orientation axis).
-#
-# Where:
-#
-# w        Should be the widget real address involved.
-#
-# amount   Should be the delta value of a **MouseWheel** event.
-#          The delta value represents the rotation units the mouse wheel has been moved.
-#          The sign of the value represents the direction the mouse wheel was scrolled.
-#          *Amount* is normally delivered by the **MouseWheel** event with a value of
-#          **+120.0** or **-120.0**, depending on the scroll direction.
-#
-#          If the value provided as *amount* is not an integer or a float,
-#          defaults to **+120.0**.
-#
-#          Note: **0** is not allowed. If provided, it will be changed to **+120.0**.
-#
-# what     Should be a string that specifies the unit type.
-#          Allowed values are the word **units** or **pages**.
-#          If not provided, defaults to **units**.
-#
-# Note: 1.0/120.0 = 0.008333333333333333
-#
-# It doesn't return anything.
-proc ::ms::scrollbar::Scroll { w amount { what units } } {
-    # Check that 'amount' is an integer or a float.
-    switch -- [string is double -strict $amount] {
-        0   { set amount 120.0 }
-        1   {
-            if { $amount == 0 } {
-                set amount 120
-            } else {
-                set amount [expr { $amount*1.0 }]
-            }
-        }
-    }
-
-    # Check the scrollmode.
-    switch -- $::ms::scrollmode {
-        natural { set amount [expr { -1.0*$amount }] }
-    }
-    set amount [expr { -$amount*0.008333333333333333 }]
-
-    # Scroll the scrollbar by 'amount' of 'what'.
-    ::ms::scrollbar::Pathname_Cmd $w scroll $amount $what
-
-    return ""
-}
-
 ## Touchpad
 #
 # Move the address linked to the widget along the X or Y axis (depending on the widget orientation axis).
@@ -2740,41 +2689,22 @@ proc ::ms::scrollbar::Touchpad { w counter amount { what units } } {
     # Translate 'amount' in 'delta_x' and 'delta_y'.
     lassign [::tk::PreciseScrollDeltas $amount] delta_x delta_y
 
-    # Check the 'what' provided.
-    switch -- $what {
-        units {
-            # Adjust 'delta_x' and 'delta_y' values, or the movement will be too slow.
-            set delta_x [expr { $delta_x*30 }]
-            set delta_y [expr { $delta_y*30 }]
-        }
-    }
-
-    # Set the right delta relative to the widget orientation axis.
+    # Check the widget orientation axis.
     switch -- $::ms::current($w,orient) {
         horizontal {
-            # Check if there was a movement along the X axis.
-            if { $delta_x != 0 } {
-                # Move the horizontal scrollbar by 'delta_x' of 'what'.
-                ::ms::scrollbar::Scroll $w $delta_x $what
-            }
-
-            # Check if there was a movement along the Y axis.
-            if { $delta_y != 0 } {
-                # Move the parent widget by 'delta_y' of 'what'.
-                ::ms::Scroll_Parent_Y $w $delta_y $what
+            # Scroll the widget if there was a movement along the X axis, otherwise do nothing.
+            if { $delta_x > 0 } {
+                ::ms::scrollbar::Pathname_Cmd $w scroll +1 $what
+            } elseif { $delta_x < 0 } {
+                ::ms::scrollbar::Pathname_Cmd $w scroll -1 $what
             }
         }
         vertical {
-            # Check if there was a movement along the X axis.
-            if { $delta_x != 0 } {
-                # Move the parent widget by 'delta_x' of 'what'.
-                ::ms::Scroll_Parent_X $w $delta_x $what
-            }
-
-            # Check if there was a movement along the Y axis.
-            if { $delta_y != 0 } {
-                # Move the vertical scrollbar by 'delta_y' of 'what'.
-                ::ms::scrollbar::Scroll $w $delta_y $what
+            # Scroll the widget if there was a movement along the Y axis, otherwise do nothing.
+            if { $delta_y > 0 } {
+                ::ms::scrollbar::Pathname_Cmd $w scroll +1 $what
+            } elseif { $delta_y < 0 } {
+                ::ms::scrollbar::Pathname_Cmd $w scroll -1 $what
             }
         }
     }
