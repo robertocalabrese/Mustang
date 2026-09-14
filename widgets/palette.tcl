f5365308c2b1d9fd14c37bcb4177a03daf176e42
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-14 10:29:34 +0200

    palette: refactor the 'Popdown_Shift_MouseWheel' procedure

--- widgets/palette.tcl
+++ widgets/palette.tcl
@@ -6688,46 +6688,59 @@ proc ::ms::palette::Popdown_MouseWheel { w x y amount { what units } } {
 # amount   Should be the delta value of a **Shift-MouseWheel**/**Control-Shift-MouseWheel** event.
 #          The delta value represents the rotation units the mouse wheel has been moved.
 #          The sign of the value represents the direction the mouse wheel was scrolled.
-#          *Amount* is normally delivered by the **Shift-MouseWheel**/**Control-Shift-MouseWheel**
-#          event with a value of **+120.0** or **-120.0**, depending on the scroll direction.
 #
-#          If the value provided as *amount* is not an integer or a float,
-#          defaults to **+120.0**.
+#          If *amount* was provided by a **Shift-MouseWheel** event, its value will be
+#          **+120** (towards left) or **-120** (towards right).
 #
-#          Note: **0** is not allowed. If provided, it will be changed to **+120.0**.
+#          If *amount* was provided by a procedure, its value will be **-1** (towards left)
+#          or **+1** (towards right).
 #
 # what     Should be a string that specifies the unit type.
 #          Allowed values are the word **units** or **pages**.
+#          *Units* are used by the **Shift-MouseWheel** event while *pages* are used
+#          by the **Control-Shift-MouseWheel** event.
 #
 #          If not provided, defaults to **units**.
 #
-# Note: 1.0/120.0 = 0.008333333333333333
-#
 # It doesn't return anything.
 proc ::ms::palette::Popdown_Shift_MouseWheel { w x y amount { what units } } {
-    # Check that 'amount' is an integer or a float.
-    switch -- [string is double -strict $amount] {
-        0   { set amount 120.0 }
-        1   {
-            if { $amount == 0 } {
-                set amount 120
-            } else {
-                set amount [expr { $amount*1.0 }]
-            }
+    # If 'amount' has been provided by a **Shift-MouseWheel** event,
+    # trasform it into **-1** (towards left) or **+1** (towards right).
+    if { ($amount == 120) || ($amount == -120) } {
+         set amount [expr { -$amount/120 }]
+    }
+
+    # Check the 'scrollmode' value ('classic' or 'natural').
+    switch -- $::ms::scrollmode {
+        natural {
+            # Invert the scroll direction.
+            set amount [expr { -1*$amount }]
         }
     }
 
-    # Check the scrollmode.
-    switch -- $::ms::scrollmode {
-        natural { set amount [expr { -1.0*$amount }] }
+    # Scroll the listbox horizontally.
+    $w.popdown.f.lb xview scroll $amount $what
+
+    # Get the index of the current hovered row.
+    set index [$w.popdown.f.lb index @$x,$y]
+
+    # Select and activate the new index.
+    $w.popdown.f.lb activate  $index
+    $w.popdown.f.lb selection clear 0 end
+    $w.popdown.f.lb selection set $index
+
+    # Set the preview color and its bordercolor (black or white).
+    set preview_color [lindex $::ms::data($w,hexadecimals) $index]
+    switch -- [string length $preview_color] {
+        10      { set bordercolor [::ms::palette::Black_Or_White $preview_color 12] }
+        13      { set bordercolor [::ms::palette::Black_Or_White $preview_color 16] }
+        default { set bordercolor [::ms::palette::Black_Or_White $preview_color 8 ] }
     }
 
-    # If possible, scroll the listbox horizontally.
-    try {
-        $w.popdown.f.lb xview scroll [expr { -$amount*0.008333333333333333 }] $what
-    } on error {} {
-        # The popdown listbox cannot scroll horizontally.
-    }
+    # Apply the changes to the preview object.
+    $w.preview configure          -background $preview_color \
+                         -highlightbackground $bordercolor \
+                              -highlightcolor $bordercolor;
 
     return -code break
 }
