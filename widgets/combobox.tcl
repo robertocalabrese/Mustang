a252ae4dc3869f840b355aca1d90fdf9cbf2bbd8
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-14 09:38:42 +0200

    combobox: refactor the 'Popdown_Scrollbar_MouseWheel' procedure

--- widgets/combobox.tcl
+++ widgets/combobox.tcl
@@ -7065,46 +7065,33 @@ proc ::ms::combobox::Popdown_PageUp { w } {
 # amount   Should be the delta value of a **MouseWheel**/**Control-MouseWheel** event.
 #          The delta value represents the rotation units the mouse wheel has been moved.
 #          The sign of the value represents the direction the mouse wheel was scrolled.
-#          *Amount* is normally delivered by the **MouseWheel**/**Control-MouseWheel**
-#          event with a value of **+120.0** or **-120.0**, depending on the scroll direction.
 #
-#          If the value provided as *amount* is not an integer or a float,
-#          defaults to **+120.0**.
-#
-#          Note: **0** is not allowed. If provided, it will be changed to **+120.0**.
+#          *Amount* will be **+120** (towards top) or **-120** (towards bottom).
 #
 # what     Should be a string that specifies the unit type.
 #          Allowed values are the word **units** or **pages**.
+#          *Units* are used by the **MouseWheel** event while *pages* are used
+#          by the **Control-MouseWheel** event.
 #
 #          If not provided, defaults to **units**.
 #
-# Note: 1.0/120.0 = 0.008333333333333333
-#
 # It doesn't return anything.
 proc ::ms::combobox::Popdown_Scrollbar_MouseWheel { w amount what } {
-    # Check that 'amount' is an integer or a float.
-    switch -- [string is double -strict $amount] {
-        0   { set amount 120.0 }
-        1   {
-            if { $amount == 0 } {
-                set amount 120
-            } else {
-                set amount [expr { $amount*1.0 }]
-            }
-        }
+    # Trasform 'amount' into **-1** (towards top) or **+1** (towards bottom).
+    if { ($amount == 120) || ($amount == -120) } {
+         set amount [expr { -$amount/120 }]
     }
 
     # Check the 'scrollmode' value ('classic' or 'natural').
     switch -- $::ms::scrollmode {
-        natural { set amount [expr { -1.0*$amount }] }
+        natural {
+            # Invert the scroll direction.
+            set amount [expr { -1*$amount }]
+        }
     }
 
-    # If possible, scroll the popdown listbox vertically.
-    try {
-        $w.popdown.f.lb yview scroll [expr { -$amount*0.008333333333333333 }] $what
-    } on error {} {
-        # The popdown listbox cannot scroll vertically.
-    }
+    # Scroll the popdown listbox vertically.
+    $w.popdown.f.lb yview scroll $amount $what
 
     return ""
 }
