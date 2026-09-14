37b9a0eba253a89ebb42c243ffeb5bf376a1a9ee
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-14 10:19:47 +0200

    palette: refactor the 'Shift_MouseWheel' procedure

--- widgets/palette.tcl
+++ widgets/palette.tcl
@@ -6459,16 +6459,15 @@ proc ::ms::palette::MouseWheel { w amount } {
 #
 # w        Should be the widget real address involved.
 #
-# amount   Should be the delta value of a **MouseWheel** event.
+# amount   Should be the delta value of a **Shift-MouseWheel** event.
 #          The delta value represents the rotation units the mousewheel has been moved.
 #          The sign of the value represents the direction the mousewheel was scrolled.
-#          *Amount* is normally delivered by the **MouseWheel** event with a value of
-#          **+120.0** or **-120.0**, depending on the scroll direction.
 #
-#          If the value provided as *amount* is not an integer or a float,
-#          defaults to **+120.0**.
+#          If *amount* was provided by a **Shift-MouseWheel** event, its value will be
+#          **+120** (towards left) or **-120** (towards right).
 #
-#          Note: **0** is not allowed. If provided, it will be changed to **+120.0**.
+#          If *amount* was provided by a procedure, its value will be **-1** (towards left)
+#          or **+1** (towards right).
 #
 # It doesn't return anything.
 proc ::ms::palette::Shift_MouseWheel { w amount } {
@@ -6513,21 +6512,36 @@ proc ::ms::palette::Shift_MouseWheel { w amount } {
                 }
             }
         }
+        1   {
+             # If 'amount' has been provided by a **Shift-MouseWheel** event,
+            # trasform it into **-1** (towards left) or **+1** (towards right).
+            if { ($amount == 120) || ($amount == -120) } {
+                 set amount [expr { -$amount/120 }]
+            }
+
+            # Check the 'scrollmode' value ('classic' or 'natural').
+            switch -- $::ms::scrollmode {
+                natural {
+                    # Invert the scroll direction.
+                    set amount [expr { -1*$amount }]
+                }
+            }
+
+            # Get the current cursor position
+            set index [$w.combobox index insert]
+
+            # Move the cursor by one character to the left or to the right (depending
+            # on the mousewheel direction).
+            if { $amount > 0 } {
+                $w.combobox icursor $index+1
+            } else {
+                $w.combobox icursor $index-1
+            }
+
+            # Make the index character visible.
+            ::ttk::entry::See $w.combobox $index
+        }
     }
-
-    # Get the current cursor position
-    set index [$w.combobox index insert]
-
-    # Move the cursor by one character to the left or to the right (depending
-    # on the mousewheel direction).
-    if { $amount > 0 } {
-        $w.combobox icursor $index+1
-    } else {
-        $w.combobox icursor $index-1
-    }
-
-    # Make the index character visible.
-    ::ttk::entry::See $w.combobox $index
 
     return ""
 }
