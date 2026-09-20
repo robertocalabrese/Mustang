5d4ebaf60d9731c5cba27eb2e7d17bff001d9798
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-20 16:17:44 +0200

    scale: fix the initial bindings

--- widgets/scale.tcl
+++ widgets/scale.tcl
@@ -848,8 +848,8 @@ package provide ::ms::scale 0.1
 #############################
 
 # Activate/Deactivate
-_bind _Scale <Activate>   { interp invokehidden {} %W [list !background]; break }
-_bind _Scale <Deactivate> { interp invokehidden {} %W [list  background]; break }
+_bind _Scale <Activate>   { interp invokehidden {} %W state [list !background]; break }
+_bind _Scale <Deactivate> { interp invokehidden {} %W state [list  background]; break }
 
 # Buttonpress-1
 _bind _Scale <ButtonPress-1> { ::ms::scale::ButtonPress %W %x %y; break }
@@ -861,11 +861,11 @@ _bind _Scale <<ContextMenu>> { ::ms::Show_ContextMenu %W %X %Y cmenu; break }
 _bind _Scale <Destroy> { ::ms::scale::Destroy %W; break }
 
 # Enter/Leave
-_bind _Scale <Enter> { interp invokehidden {} %W [list  hover]; break }
-_bind _Scale <Leave> { interp invokehidden {} %W [list !hover]; break }
+_bind _Scale <Enter> { interp invokehidden {} %W state [list  hover]; break }
+_bind _Scale <Leave> { interp invokehidden {} %W state [list !hover]; break }
 
 # FocusIn/FocusOut
-_bind _Scale <FocusIn>  { interp invokehidden {} %W [list focus]; break }
+_bind _Scale <FocusIn>  { interp invokehidden {} %W state [list focus]; break }
 _bind _Scale <FocusOut> { ::ms::scale::FocusOut %W; break }
 
 # PrevChar/NextChar
