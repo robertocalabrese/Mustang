484beddb3ad808900af10a73480346f8802302cb
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-20 16:14:17 +0200

    checkbutton: fix the automatic translation check

--- widgets/checkbutton.tcl
+++ widgets/checkbutton.tcl
@@ -1655,11 +1655,18 @@ proc ::ms::checkbutton::Command { window { args "" } } {
             switch -- $::ms::current($w,textvariable) {
                 ""  {
                     # Automatic translation: ON
-                    set ::ms::data($w,translated_text) [::msgcat::mc {*}$::ms::current($w,text)]
+
+                    # Check the current text provided.
+                    switch -- [string trim $::ms::current($w,text)] {
+                        ""      { set ::ms::data($w,translated_text) $::ms::current($w,text) }
+                        default { set ::ms::data($w,translated_text) [::msgcat::mc {*}$::ms::current($w,text)] }
+                    }
+
                     set text_variable ::ms::data($w,translated_text)
                 }
                 default {
                     # Automatic translation: OFF
+
                     set text_variable $::ms::current($w,textvariable)
                 }
             }
@@ -2452,11 +2459,18 @@ proc ::ms::checkbutton::Pathname_Cmd { w cmd args } {
                             switch -- $::ms::current($w,textvariable) {
                                 ""  {
                                     # Automatic translation: ON
-                                    set ::ms::data($w,translated_text) [::msgcat::mc {*}$::ms::current($w,text)]
+
+                                    # Check the current text provided.
+                                    switch -- [string trim $::ms::current($w,text)] {
+                                        ""      { set ::ms::data($w,translated_text) $::ms::current($w,text) }
+                                        default { set ::ms::data($w,translated_text) [::msgcat::mc {*}$::ms::current($w,text)] }
+                                    }
+
                                     set text_variable ::ms::data($w,translated_text)
                                 }
                                 default {
                                     # Automatic translation: OFF
+
                                     set text_variable $::ms::current($w,textvariable)
                                 }
                             }
