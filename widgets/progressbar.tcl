bc7ac6511b9cb070795201074f4672541f7a988f
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-20 16:15:49 +0200

    progressbar: fix the automatic translation check

--- widgets/progressbar.tcl
+++ widgets/progressbar.tcl
@@ -1389,12 +1389,19 @@ proc ::ms::progressbar::Command { window { args "" } } {
             switch -- $::ms::current($w,textvariable) {
                 ""  {
                     # Automatic translation: ON
-                    set ::ms::data($w,translated_text) [::msgcat::mc {*}$::ms::current($w,text)]
-                    set text_variable $::ms::data($w,translated_text)
+
+                    # Check the current text provided.
+                    switch -- [string trim $::ms::current($w,text)] {
+                        ""      { set ::ms::data($w,translated_text) $::ms::current($w,text) }
+                        default { set ::ms::data($w,translated_text) [::msgcat::mc {*}$::ms::current($w,text)] }
+                    }
+
+                    set text_variable ::ms::data($w,translated_text)
                 }
                 default {
                     # Automatic translation: OFF
-                    set text_variable [set $::ms::current($w,textvariable)]
+
+                    set text_variable $::ms::current($w,textvariable)
                 }
             }
 
@@ -1972,11 +1979,18 @@ proc ::ms::progressbar::Pathname_Cmd { w cmd args } {
                             switch -- $::ms::current($w,textvariable) {
                                 ""  {
                                     # Automatic translation: ON
-                                    set ::ms::data($w,translated_text) [::msgcat::mc {*}$::ms::current($w,text)]
+
+                                    # Check the current text provided.
+                                    switch -- [string trim $::ms::current($w,text)] {
+                                        ""      { set ::ms::data($w,translated_text) $::ms::current($w,text) }
+                                        default { set ::ms::data($w,translated_text) [::msgcat::mc {*}$::ms::current($w,text)] }
+                                    }
+
                                     set text_variable ::ms::data($w,translated_text)
                                 }
                                 default {
                                     # Automatic translation: OFF
+
                                     set text_variable $::ms::current($w,textvariable)
                                 }
                             }
