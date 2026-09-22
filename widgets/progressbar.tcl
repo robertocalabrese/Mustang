bb138d7dc1919bc0350a965ef382c6df509f4ad5
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2026-09-22 18:09:16 +0200

    progressbar: fix the initial bindings

--- widgets/progressbar.tcl
+++ widgets/progressbar.tcl
@@ -930,6 +930,22 @@ _bind _Progressbar <Leave> { interp invokehidden {} %W state [list !hover]; brea
 _bind _Progressbar <FocusIn>  { interp invokehidden {} %W state [list focus]; break }
 _bind _Progressbar <FocusOut> { ::ms::progressbar::FocusOut %W; break }
 
+# Tab/Shift-Tab keys
+_bind _Progressbar <KeyPress-Tab> { ::tk::TabToWindow [tk_focusNext %W]; break }
+
+switch -- [_tk windowingsystem] {
+    win32   { _bind _Progressbar <Shift-Tab> { ::tk::TabToWindow [tk_focusPrev %W]; break } }
+    default {
+        _bind _Progressbar <ISO_Left_Tab>    { ::tk::TabToWindow [tk_focusPrev %W]; break }
+
+        try {
+            _bind _Progressbar <hpBackTab>   { ::tk::TabToWindow [tk_focusPrev %W]; break }
+        } on error {} {
+            # Do nothing.
+        }
+    }
+}
+
 # Mousewheel and Touchpad
 
 # Try to find the innermost widget's scrollable parent with an active vertical scrollbar
